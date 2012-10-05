4f96ab78f4b3e9b0f8f00a331aec7b1fdc28d709
Andrew Beveridge <ab441@hw.ac.uk>
2012-10-05 15:58:27 +0100

    deletedt redundant code

--- app/views/pages/check.php
+++
@@ -1,4 +0,0 @@
-<?php 
-	echo 'var check = {"code": '.$code.',"message": "'.addslashes($message).'"}';
-	//if(isset($debug)) echo "var debug = '".addslashes($debug)."';";
-?>
--- app/views/pages/map.php
+++
@@ -1,6 +0,0 @@
-	<div id="MapContainer"><div id="Map"></div></div>
-	<div id="ButtonZoomIn" class="button roundTop" onclick="zoomIn()"><p>+</p></div>
-	<div id="ButtonZoomOut" class="button roundBottom" onclick="zoomOut()"><p>&#8211;</p></div>
-	<div id="ButtonLocation" class="button inactive" onclick="toggleLocation()"><p><img src="/img/location.png" width="22" alt="Geolocation" title="Geolocation"/></p></div>
-	<a onclick="window.location.href='/help'"><div id="ButtonHelp" class="button"><p>?</p></div></a>
-	<a onclick="buttonSelect();"><div id="ButtonSelect" class="button"><p>Select Recyclables...</p></div></a>
