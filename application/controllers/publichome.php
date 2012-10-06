a230d20be20ba6996b9f1e0ed8607eaedc50d51a
Andrew Beveridge <ab441@hw.ac.uk>
2012-10-06 06:26:39 +0100

    added home view for admin user, changed auth system to use custom controllers

---
+++ application/controllers/publichome.php
@@ -0,0 +1,60 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class PublicHome extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+    /**
+     * Global Login function to log user in and direct to proper area
+     *
+     * @return void
+     * @author Jonathan Johnson
+     **/
+    function login() {
+
+        if($_POST) {   //clean public facing app input
+            $identity = $this->input->post('identity', true);
+            $password = $this->input->post('password', true);
+
+            if($this->ion_auth->login($identity,$password)) {
+
+                $user = $this->ion_auth->user()->row();
+				if($this->ion_auth->is_admin()) {
+					redirect('admin/home');
+				} else {
+					redirect('user/home');
+				}
+				
+            }
+            else {
+
+                // set error flashdata
+                $this->session->set_flashdata(
+                    'error',
+                    'Your login attempt failed.'
+                );
+
+                redirect('/');
+            }
+        }
+        $this->load->view('login_form');
+    }
+
+    /**
+     * Global logout function to destroy user session
+     *
+     * @return void
+     * @author Jonathan Johnson
+     **/
+    function logout() {   //Basic Ion_Auth Logout function
+        $this->ion_auth->logout();
+        redirect('/');
+    }
+	
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
