33ae04f5e3bc28ae4589ebdd7b8b97379834dc38
Andrew Beveridge <ab441@hw.ac.uk>
2012-10-06 04:12:11 +0100

    moved old CI code to /old, started fresh with ion_auth and uhoh for debugging

--- old/index.php
+++ old/index.php
@@ -34,6 +34,7 @@ if (defined('ENVIRONMENT'))
 	{
 		case 'development':
 			error_reporting(E_ALL);
+			ini_set('display_errors', 1);
 		break;
 	
 		case 'testing':
@@ -56,7 +57,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'system';
+	$system_path = 'sys';
 
 /*
  *---------------------------------------------------------------
@@ -72,7 +73,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'application';
+	$application_folder = 'app';
 
 /*
  * --------------------------------------------------------------------
