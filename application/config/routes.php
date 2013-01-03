2c67b1f76e8d23407325e18e7e4ea2aa19d33ccb
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-03 19:52:16 +0000

    rearranged everything, renamed controllers et

--- application/config/routes.php
+++ application/config/routes.php
@@ -37,6 +37,7 @@
 | in the URL cannot be matched to a valid route.
 |
 */
+$slug = '';
 if ($_SERVER['HTTP_HOST']=="hwsports.co.uk") {
     $slug = "hwsports";
 	$config['base_url'] = 'http://hwsports.co.uk/';
