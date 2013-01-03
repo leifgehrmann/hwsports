b1baccc5db53d3ade3552c75e4c946d65f84a15b
lg90 <lg90@hw.ac.uk>
2013-01-03 20:05:51 +0000

    added some poo

---
+++ application/views/product/footer.php
@@ -0,0 +1,8 @@
+			</div>
+			<div id="footer">
+				<div class="left legal">&copy; Infusion Systems 2012</div>
+				<div class="right"><a href="http://www.infusionsystems.co.uk"><div class="company-logo"></div></a></div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ application/views/product/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="">
+        <title>Infusion Sports</title>
+        <link href="../../css/normalize.min.css" rel="stylesheet" type="text/css">
+        <link href="../../css/main.css" rel="stylesheet" type="text/css">
+        <link href="../../css/sis.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+        <!--
+            body {
+                background:#ededed;
+            }
+            #header {
+                background:#1E303A;
+            }
+            #content h1, #content h2, #content h3, #content a{
+                color:#1E303A;
+            }
+        -->
+        </style>
+    </head>
+    <!--[if lt IE 7]>      <body class="page-<?=$_ci_view?> lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+    <!--[if IE 7]>         <body class="page-<?=$_ci_view?> lt-ie9 lt-ie8"> <![endif]-->
+    <!--[if IE 8]>         <body class="page-<?=$_ci_view?> lt-ie9"> <![endif]-->
+    <!--[if gt IE 8]><!--> <body class="page-<?=$_ci_view?>"> <!--<![endif]-->
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+    <![endif]-->
+    <body>
+        <div id="container">
+            <a href="/"><div id="header"></div></a>
+            <div id="menu">
+                <ul>
+                    <a href=""><li><img src="../../img/icons/home.14.png"/>Homepage</li></a>
+                    <a href=""><li><img src="../../img/icons/star.14.png"/>Signup</li></a>
+                </ul>
+            </div>
+            <div id="content">
