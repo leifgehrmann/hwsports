e9ab6a46231c9e25bea9f2e56290d62b8d06d207
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-03 20:31:27 +0000

    rearranged everything, renamed controllers et

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -75,14 +75,14 @@ class Auth extends CI_Controller {
 				//if the login is successful
 				//redirect them back to the home page
 				$this->session->set_flashdata('message', $this->ion_auth->messages());
-				redirect('/', 'refresh');
+				redirect('/loginsuccess', 'refresh');
 			}
 			else
 			{
 				//if the login was un-successful
 				//redirect them back to the login page
 				$this->session->set_flashdata('message', $this->ion_auth->errors());
-				redirect('auth/login', 'refresh'); //use redirects instead of loading views for compatibility with MY_Controller libraries
+				redirect('/loginfail', 'refresh'); //use redirects instead of loading views for compatibility with MY_Controller libraries
 			}
 		}
 		else
