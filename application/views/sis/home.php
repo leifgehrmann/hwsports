e30fee0e5e8947c66299b4bd400d4d1200e016ae
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-03 22:03:56 +0000

    rearranged everything, renamed controllers et

--- application/views/sis/home.php
+++ application/views/sis/home.php
@@ -3,7 +3,7 @@
 <div id="infoMessage"><?php echo $message;?></div>
 
 Current User ID: <?=$currentUser->id?><br />
-Current User Email: <?=$currentUser->id?><br />
+Current User Email: <?=$currentUser->email?><br />
 Current User Data: <?print_r($currentUserData)?><br />
 
 <a href="<?= base_url('auth/login') ?>">Login</a>
