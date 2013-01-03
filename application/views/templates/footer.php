29b3f9a465aea482cc99aad31939c9dd3bebdcae
lg90 <lg90@hw.ac.uk>
2013-01-03 17:51:34 +0000

    updated header & footer with some style

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,9 @@
+		</div>
+		<div id="footer">
+			<div class="left"><a href="http://www.infusionsystems.co.uk"><div class="company-logo"></div></a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="">Feedback</a></diV>
+			<div class="right"><span style="font-weight:bold;">Tournament Management System 1.0</span>
+			</diV>
+		</div>
 	
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.8.1.min.js"><\/script>')</script>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,11 +14,72 @@
 
         <script src="/js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
     </head>
-<!--[if lt IE 7]>      <body class="page-<?=$page?> lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <body class="page-<?=$page?> lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <body class="page-<?=$page?> lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <body class="page-<?=$page?>"> <!--<![endif]-->
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
-        <![endif]-->
 	
+
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="">
+        <title><?=$title?></title>
+        <link rel="icon" type="image/png" href="/img/logo/tms.fav.png" />
+        <link href="/css/normalize.min.css" rel="stylesheet" type="text/css">
+        <link href="/css/main.css" rel="stylesheet" type="text/css">
+        <link href="/css/tms.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+        <!--
+            #header {
+                background:#00468C;
+                /*background:#222222;*/
+            }
+            #content h1, #content h2, #content h3, #content a{
+                color:#00468C;
+                /*color:#222222;*/
+            }
+        -->
+        </style>
+    </head>
+    <!--[if lt IE 7]>      <body class="page-<?=$page?> lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+    <!--[if IE 7]>         <body class="page-<?=$page?> lt-ie9 lt-ie8"> <![endif]-->
+    <!--[if IE 8]>         <body class="page-<?=$page?> lt-ie9"> <![endif]-->
+    <!--[if gt IE 8]><!--> <body class="page-<?=$page?>"> <!--<![endif]-->
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+    <![endif]-->
+        <div id="header">
+            <div id="title-bar" class="left">
+                <div class="logo"></div>
+                <div class="name">Heriot Watt University</div>
+                <div class="system">Tournaments System</div>
+            </div>
+            <div id="user-bar" class="right">
+                <span class="username">Leif Gehrmann</span>
+                <span class="sep">|</span>
+                <a href=""><span class="role">Switch Account</span></a>
+                <span class="sep">|</span>
+                <a href=""><span class="logout">Logout</span></a>
+            </div>
+        </div>
+        <div id="menu">
+            <ul>
+                <li class="selected"><img src="/img/icons/home.14.png"/>Dashboard</li>
+            </ul>
+            <ul>
+                <li><a href=""><img src="/img/icons/tournament.14.png"/>Tournaments</a></li>
+                <li><img src="/img/icons/jellyfish.14.png"/>Venues</li>
+                <li><img src="/img/icons/sport.14.png"/>Sports</li>
+                <li><img src="/img/icons/match.14.png"/>Matches</li>
+                <li ><img src="/img/icons/group.14.png"/>Groups</li>
+                <li><img src="/img/icons/user.14.png"/>Users</li>
+                <li><img src="/img/icons/news.14.png"/>News</li>
+                <li><img src="/img/icons/money.14.png"/>Tickets</li>
+                <li><img src="/img/icons/report.14.png"/>Reports</li>
+            </ul>
+            <ul>
+                <li><img src="/img/icons/appearence.14.png"/>Appearence</li>
+                <li><img src="/img/icons/cogwheel.14.png"/>Settings</li>
+                <li><img src="/img/icons/cogwheel.14.png"/>Personal Settings</li>
+            </ul>
+        </div>
+        <div id="content">
