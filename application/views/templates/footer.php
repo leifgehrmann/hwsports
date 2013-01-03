8e523d1329b4bdece6d8358feef3d3650d14c0ff
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-03 18:05:08 +0000

    added auth controller

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -8,6 +8,6 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.8.1.min.js"><\/script>')</script>
 
-        <script src="/js/<?=$page?>"></script>
+        <script src="/js/<?=$_ci_view?>"></script>
     </body>
 </html>
