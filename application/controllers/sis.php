9b325d9b37cab8d7d60057430c1f6b411bec00a9
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-03 21:18:09 +0000

    rearranged everything, renamed controllers et

--- application/controllers/sis.php
+++ application/controllers/sis.php
@@ -5,7 +5,12 @@ class Sis extends CI_Controller {
 	public function index($slug)
 	{
 		// Page title
-		$this->data['title'] = $slug;
+		$this->data['title'] = "Home";
+		
+		$this->session->set_userdata('slug', $slug);
+		$query = $this->db->query("SELECT `centreID` FROM `centreData` WHERE `key` = 'slug' AND `value` = '$slug' LIMIT 1");
+		$row = $query->row_array();
+		$this->session->set_userdata('centreID', $row['centreID']);
 		
 		//set the flash data error message if there is one
 		$this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
