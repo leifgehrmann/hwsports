cb64dd4d34901cd71022e0e1ba6ae9c5870d562e
Andrew Beveridge <github@andrewbeveridge.co.uk>
2013-01-03 15:12:54 +0000

    fix red

--- application/controllers/admin/home.php
+++ application/controllers/admin/home.php
@@ -6,7 +6,19 @@ class Home extends MY_Controller {
     }
 
     function index() {
-		$this->load->view('admin_view');
+	
+
+		//set the flash data error message if there is one
+		$this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
+
+		//list the users
+		$this->data['users'] = $this->ion_auth->users()->result();
+		foreach ($this->data['users'] as $k => $user)
+		{
+			$this->data['users'][$k]->groups = $this->ion_auth->get_users_groups($user->id)->result();
+		}
+
+		$this->load->view('admin_view',$this->data);
     }
 
 }
