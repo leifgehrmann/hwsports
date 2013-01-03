601df2647445c9742d71124e9f3c7c816674ee90
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-03 17:59:03 +0000

    added auth controller

--- application/views/admin_view.php
+++ application/views/admin_view.php
@@ -1,6 +1,6 @@
-<h1>Admin View!</h1>
+<h1>Admin Portal</h1>
 
-<h4>User info: <?= print_r($the_user,1) ?></h4>
+<h4>Hello, <?=$the_user[username]?>!</h4>
 
 <h1>Create User</h1>
 <p>Please enter the users information below.</p>
@@ -8,6 +8,41 @@
 <div id="infoMessage"><?php echo $message;?></div>
 
 <?php echo form_open("auth/create_user"); ?>
+
+      <p>
+            First Name: <br />
+            <?php echo form_input($first_name);?>
+      </p>
+
+      <p>
+            Last Name: <br />
+            <?php echo form_input($last_name);?>
+      </p>
+
+      <p>
+            Company Name: <br />
+            <?php echo form_input($company);?>
+      </p>
+
+      <p>
+            Email: <br />
+            <?php echo form_input($email);?>
+      </p>
+
+      <p>
+            Phone: <br />
+            <?php echo form_input($phone1);?>-<?php echo form_input($phone2);?>-<?php echo form_input($phone3);?>
+      </p>
+
+      <p>
+            Password: <br />
+            <?php echo form_input($password);?>
+      </p>
+
+      <p>
+            Confirm Password: <br />
+            <?php echo form_input($password_confirm);?>
+      </p>
 
 
       <p><?php echo form_submit('submit', 'Create User');?></p>
