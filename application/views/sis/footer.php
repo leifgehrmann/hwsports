2fa0ba6751f4bbd6552ca4472f14b41b3c1b43cc
lg90 <lg90@hw.ac.uk>
2013-01-03 18:35:55 +0000

    added footer and header content for the SIS

--- application/views/sis/footer.php
+++ application/views/sis/footer.php
@@ -1,13 +1,9 @@
+			</div>
+			<div id="footer">
+				<div class="left legal">&copy; Heriot-Watt University, Edinburgh, Scotland, UK EH14 4AS, Tel: +44 (0) 131 449 5111 
+Scottish registered charity number: SC000278</div>
+				<div class="right"><a href="http://www.infusionsystems.co.uk"><div class="company-logo"></div></a></div>
+			</div>
 		</div>
-		<div id="footer">
-			<div class="left"><a href="http://www.infusionsystems.co.uk"><div class="company-logo"></div></a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="">Feedback</a></diV>
-			<div class="right"><span style="font-weight:bold;">Tournament Management System 1.0</span>
-			</diV>
-		</div>
-	
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.8.1.min.js"><\/script>')</script>
-
-        <script src="/js/<?=$_ci_view?>"></script>
-    </body>
+	</body>
 </html>
--- application/views/sis/header.php
+++ application/views/sis/header.php
@@ -4,11 +4,24 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="description" content="">
-        <title><?=$title?> | Tournament Management System</title>
-        <link rel="icon" type="image/png" href="/img/logo/tms.fav.png" />
-        <link href="/css/normalize.min.css" rel="stylesheet" type="text/css">
-        <link href="/css/main.css" rel="stylesheet" type="text/css">
-        <link href="/css/tms.css" rel="stylesheet" type="text/css">
+        <title><?=$title?> | ***********************************************************</title>
+        <link href="../../css/normalize.min.css" rel="stylesheet" type="text/css">
+        <link href="../../css/main.css" rel="stylesheet" type="text/css">
+        <link href="../../css/sis.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+        <!--
+            body {
+                background:#E7EEF9;
+            }
+            #header {
+                background:#00468C;
+                background-image: url("../../img/banners/hwsports.png");
+            }
+            #content h1, #content h2, #content h3, #content a{
+                color:#00468C;
+            }
+        -->
+        </style>
     </head>
     <!--[if lt IE 7]>      <body class="page-<?=$_ci_view?> lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
     <!--[if IE 7]>         <body class="page-<?=$_ci_view?> lt-ie9 lt-ie8"> <![endif]-->
@@ -17,39 +30,20 @@
     <!--[if lt IE 7]>
         <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
     <![endif]-->
-        <div id="header">
-            <div id="title-bar" class="left">
-                <div class="logo"></div>
-                <div class="name">Heriot Watt University</div>
-                <div class="system">Tournaments System</div>
+    <body>
+        <div id="container">
+            <a href=""><div id="header"></div></a>
+            <div id="menu">
+                <ul>
+                    <a href=""><li><img src="../../img/icons/home.14.png"/>Homepage</li></a>
+                    <a href=""><li><img src="../../img/icons/tournament.14.png"/>Tournaments</li></a>
+                    <a href=""><li class="selected"><img src="../../img/icons/calendar.14.png"/>Calendar</li></a>
+                    <a href=""><li><img src="../../img/icons/match.14.png"/>Matches</li></a>
+
+                    <a href=""><li><img src="../../img/icons/ticket.14.png"/>Tickets</li></a>
+                    <a href=""><li class="special"><img src="../../img/icons/star.14.png"/>Registration</li></a>
+                    <a href=""><li><img src="../../img/icons/key.14.png"/>Login</li></a>
+                    <a href=""><li><img src="../../img/icons/help.14.png"/>Help</li></a>
+                </ul>
             </div>
-            <div id="user-bar" class="right">
-                <span class="username">Leif Gehrmann</span>
-                <span class="sep">|</span>
-                <a href=""><span class="role">Switch Account</span></a>
-                <span class="sep">|</span>
-                <a href=""><span class="logout">Logout</span></a>
-            </div>
-        </div>
-        <div id="menu">
-            <ul>
-                <li class="selected"><img src="/img/icons/home.14.png"/>Dashboard</li>
-            </ul>
-            <ul>
-                <li><a href=""><img src="/img/icons/tournament.14.png"/>Tournaments</a></li>
-                <li><img src="/img/icons/jellyfish.14.png"/>Venues</li>
-                <li><img src="/img/icons/sport.14.png"/>Sports</li>
-                <li><img src="/img/icons/match.14.png"/>Matches</li>
-                <li ><img src="/img/icons/group.14.png"/>Groups</li>
-                <li><img src="/img/icons/user.14.png"/>Users</li>
-                <li><img src="/img/icons/news.14.png"/>News</li>
-                <li><img src="/img/icons/money.14.png"/>Tickets</li>
-                <li><img src="/img/icons/report.14.png"/>Reports</li>
-            </ul>
-            <ul>
-                <li><img src="/img/icons/appearence.14.png"/>Appearence</li>
-                <li><img src="/img/icons/cogwheel.14.png"/>Settings</li>
-                <li><img src="/img/icons/cogwheel.14.png"/>Personal Settings</li>
-            </ul>
-        </div>
-        <div id="content">
+            <div id="content">
