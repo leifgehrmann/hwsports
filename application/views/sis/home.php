5f25d3b58d4657cc2ad060867c639f6c55629c02
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-03 20:29:48 +0000

    rearranged everything, renamed controllers et

--- application/views/sis/home.php
+++ application/views/sis/home.php
@@ -1,6 +1,6 @@
 <h1>Welcome to HW Sports! </h1>
 
-<div id="infoMessage"><?php @echo $message;?></div>
+<div id="infoMessage"><?php echo (isset($message) ? $message : '');?></div>
 
 <? print_r($currentUser); //$currentUser->username ?>
 <a href="<?= base_url('auth/login') ?>">Login</a>
