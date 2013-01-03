da24ce599503524f0c36af99188ffc7143a26f1a
Andrew Beveridge <github@andrewbeveridge.co.uk>
2013-01-03 14:52:25 +0000

    fix red

--- application/controllers/front.php
+++ application/controllers/front.php
@@ -4,6 +4,7 @@ class Front extends CI_Controller {
 
 	public function index()
 	{
+		$this->page = "landingPage";
 		//$this->load->view('templates/header');
 		//$this->load->view('landingPage');
 		$this->load->view('templates/footer');
