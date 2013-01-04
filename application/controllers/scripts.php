556e9830c02fb990102f08e1942a8b7fa55e1068
lg90 <lg90@hw.ac.uk>
2013-01-04 20:03:42 +0000

    trying to add calendar jquery object thingy

--- application/controllers/scripts.php
+++ application/controllers/scripts.php
@@ -3,7 +3,7 @@ class Scripts extends CI_Controller {
 
 	public function view($file)
 	{				
-		if ( ! file_exists('app/views/scripts/'.$file.'.js.php'))
+		if ( ! file_exists('application/views/scripts/'.$file.'.js.php'))
 		{
 			// Whoops, we don't have a page for that!
 			show_404();
@@ -14,7 +14,7 @@ class Scripts extends CI_Controller {
 	}
 	public function vendor($file)
 	{				
-		if ( ! file_exists('app/views/scripts/vendor/'.$file))
+		if ( ! file_exists('application/views/scripts/vendor/'.$file))
 		{
 			// Whoops, we don't have a page for that!
 			show_404();
