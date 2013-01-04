1508aabd89b7ddc4c61e25c56724376459347b7b
lg90 <lg90@hw.ac.uk>
2013-01-04 20:15:38 +0000

    trying to add calendar jquery object thingy

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,9 +38,6 @@
 |
 */
 
-$route['scripts/vendor/(:any)'] = 'scripts/vendor/$1';
-$route['scripts/(:any)'] = 'scripts/view/$1';
-
 $route['css/(:any)'] = "css/index/$1";
 $route['default_controller'] = ($_SERVER['HTTP_HOST']=="infusionsports.co.uk" ? "product" : "sis");
 $route['404_override'] = '';
