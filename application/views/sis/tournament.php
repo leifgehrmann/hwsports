858a0e7269b7bf470d66fb480537f2df6583dc2e
lg90 <lg90@hw.ac.uk>
2013-01-04 18:32:48 +0000

    changed the tournament view

--- application/views/sis/tournament.php
+++ application/views/sis/tournament.php
@@ -4,6 +4,30 @@
 <?
 	$this->load->library('table');
 
+
+	$tmpl = array (
+                    'table_open'          => '<table border="0" cellpadding="4" cellspacing="0">',
+
+                    'heading_row_start'   => '<tr>',
+                    'heading_row_end'     => '</tr>',
+                    'heading_cell_start'  => '<th>',
+                    'heading_cell_end'    => '</th>',
+
+                    'row_start'           => '<tr>',
+                    'row_end'             => '</tr>',
+                    'cell_start'          => '<td>',
+                    'cell_end'            => '</td>',
+
+                    'row_alt_start'       => '<tr>',
+                    'row_alt_end'         => '</tr>',
+                    'cell_alt_start'      => '<td>',
+                    'cell_alt_end'        => '</td>',
+
+                    'table_close'         => '</table>'
+              );
+
+	$this->table->set_template($tmpl);
+
 	$info = array(
 				array('Name', 'Color'),
 				array('<a href="/sis/game/$gameID">Wattball</a>', 'sign up'),
