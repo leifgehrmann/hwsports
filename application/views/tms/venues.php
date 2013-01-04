7c065510afe36a735dae3cde76aeca537e017086
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-04 18:25:25 +0000

    rearranged everything, renamed controllers et

--- application/views/tms/venues.php
+++ application/views/tms/venues.php
@@ -1,2 +1,31 @@
-<h1>Venue Functions</h1>
+<div id="infoMessage"><?=$message;?></div>
 
+<h1>Venue List</h1>
+
+
+<h1>Create Venue</h1>
+
+<p>Enter details of new venue below.</p>
+
+<?=form_open("tms/venues/create");?>
+
+	<?=form_hidden($createLatLng);?>
+		
+	<p>
+	<label for="name">Name:</label>
+	<?=form_input($createName);?>
+	</p>
+	
+	<p>
+	<label for="description">Description:</label>
+	<?=form_input($createDescription);?>
+	</p>
+	
+	<p>
+	<label for="directions">Directions:</label>
+	<?=form_input($createDirections);?>
+	</p>
+
+	<p><?=form_submit('submit', 'Login');?></p>
+		
+<?=form_close();?>
