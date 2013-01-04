d7b99d6bf515905944a935824d6a99cda1561d05
lg90 <lg90@hw.ac.uk>
2013-01-04 14:04:13 +0000

    modified some php for sis.php

--- application/views/sis/footer.php
+++ application/views/sis/footer.php
@@ -1,6 +1,6 @@
 			</div>
 			<div id="footer">
-				<div class="left legal">************* *********** ************* *********</div>
+				<div class="left legal"><?=$this->session->userdata('centreLegalText')?></div>
 				<div class="right"><a href="http://www.infusionsystems.co.uk"><div class="company-logo"></div></a></div>
 			</div>
 		</div>
