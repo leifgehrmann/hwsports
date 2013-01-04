460c83200b6ad005f256bb2717915729ab90b2fa
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-04 21:46:01 +0000

    rearranged everything, renamed controllers et

--- application/views/tms/footer.php
+++ application/views/tms/footer.php
@@ -8,7 +8,5 @@
 	
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/vendor/jquery-1.8.1.min.js"><\/script>')</script>
-
-        <script src="/js/<?=$_ci_view?>"></script>
     </body>
 </html>
