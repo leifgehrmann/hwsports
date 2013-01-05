3aad8896200c3149a649586f9cda5d155a0a82a6
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-05 02:25:52 +0000

    rearranged everything, renamed controllers et

--- scripts/rico/examples/php/ShipperEdit.php
+++ scripts/rico/examples/php/ShipperEdit.php
@@ -10,44 +10,27 @@ $GLOBALS['sqltext']='.';
 </head>
 <body>
 <?php
-$oDB = new dbClass();
-if (! $oDB->MySqlLogon("sports_northwind", "sports_northwind", "northwind") ) die('MySqlLogon failed');
-$oForm=new TableEditClass();
-$oForm->SetTableName("shippers");
-$oForm->options["XMLprovider"]="ricoQuery.php";
-$oForm->convertCharSet=true;
-$oForm->options["canAdd"]=1;
-$oForm->options["canEdit"]=1;
-$oForm->options["canDelete"]=1;
+	$oDB = new dbClass();
+	if (! $oDB->MySqlLogon("sports_northwind", "sports_northwind", "northwind") ) die('MySqlLogon failed');
+	$oForm=new TableEditClass();
+	$oForm->SetTableName("shippers");
+	$oForm->options["XMLprovider"]="ricoQuery.php";
+	$oForm->convertCharSet=true;
+	$oForm->options["canAdd"]=1;
+	$oForm->options["canEdit"]=1;
+	$oForm->options["canDelete"]=1;
+	$oForm->options["frozenColumns"]=1;
+	$oForm->options["menuEvent"]='click';
+	$oForm->options["highlightElem"]='cursorRow';
+	$oForm->AddEntryFieldW("ShipperID", "ID", "B", "<auto>",50);
+	$oForm->AddEntryFieldW("CompanyName", "Company Name", "B", "", 150);
+	$oForm->ConfirmDeleteColumn();
+	$oForm->SortAsc();
+	$oForm->AddEntryFieldW("Phone", "Phone Number", "B", "", 150);
 
-if ($oForm->action == "table") {
-	DisplayTable();
-} else {
-	DefineFields();
-}
+	$oForm->DisplayPage();
 
-$oDB->dbClose();
-
-function DisplayTable() {
-  global $oForm,$oDB;
-  
-  $oForm->options["frozenColumns"]=1;
-  $oForm->options["menuEvent"]='click';
-  $oForm->options["highlightElem"]='cursorRow';
-  DefineFields();
-}
-
-function DefineFields() {
-  global $oForm,$oDB;
-
-  $oForm->AddEntryFieldW("ShipperID", "ID", "B", "<auto>",50);
-  $oForm->AddEntryFieldW("CompanyName", "Company Name", "B", "", 150);
-  $oForm->ConfirmDeleteColumn();
-  $oForm->SortAsc();
-  $oForm->AddEntryFieldW("Phone", "Phone Number", "B", "", 150);
-
-  $oForm->DisplayPage();
-}
+	$oDB->dbClose();
 ?>
 </body>
 </html>
