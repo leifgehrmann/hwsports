b8ea0d82eddd254351c4068f06f81425184d69ab
lg90 <lg90@hw.ac.uk>
2013-01-05 23:03:11 +0000

    Creating content for pages that will be implemented in the end. Part of the work is deciding how to visualize it. So that is why im adding the content now so we dont have to decide on it later :)

--- application/views/sis/home.php
+++ application/views/sis/home.php
@@ -1,7 +1,16 @@
-<h1>Welcome to HW Sports!</h1>
+<h1>Welcome to Heriot Watt Sports!</h1>
 
 <? if(!empty($message)){ ?>
 	<div id="infoMessage"><?php echo $message;?></div>
 <? } ?>
 
-This is the homepage news stuff for public people.
+<p>This is the homepage news stuff for public people.</p>
+<p>This part could be a welcome message that can be defined in the appearence menu in the TMS. We are also assuming that the title of this page will be changeable as well.</p>
+
+<h2>February 14th, 2013 – Register Now!</h2>
+<p>Want to participate in the Heriot Watt Tournament, well now you can register on this website!</p>
+<p>We are offering the following sports this year.</p>
+<ul>
+	<li>Heriot Hurdling (Men &amp; Womens)</li>
+	<li>Wattball</li>
+</ul>
