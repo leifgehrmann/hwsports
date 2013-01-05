8eee2fd0084cd76856e36130cb967e5eabaa10f9
lg90 <lg90@hw.ac.uk>
2013-01-05 23:21:17 +0000

    Creating content for pages that will be implemented in the end. Part of the work is deciding how to visualize it. So that is why I'm adding the content now so we don't have to decide on it later :)

--- application/views/sis/home.php
+++ application/views/sis/home.php
@@ -5,12 +5,26 @@
 <? } ?>
 
 <p>This is the homepage news stuff for public people.</p>
-<p>This part could be a welcome message that can be defined in the appearence menu in the TMS. We are also assuming that the title of this page will be changeable as well.</p>
+<p>This part could be a welcome message that can be defined in the appearence menu in the
+	TMS. We are also assuming that the title of this page will be changeable as well.</p>
 
-<h2>February 14th, 2013 – Register Now!</h2>
+<h1>News</h1>
+
+<h2>Register Now!</h2>
+<p>Published: February 14th, 2013 | <a href="news/$newsID">permalink</a></p>
 <p>Want to participate in the Heriot Watt Tournament, well now you can register on this website!</p>
 <p>We are offering the following sports this year.</p>
 <ul>
 	<li>Heriot Hurdling (Men &amp; Womens)</li>
 	<li>Wattball</li>
 </ul>
+
+<h2>Opening soon...</h2>
+<p>Published: February 10th, 2013 | <a href="news/$newsID">permalink</a></p>
+<p>The website will work soon hopefully by February... I hope</p>
+
+<h2>Essay: Why Applejack is best pony.</h2>
+<p>Published: January 5th, 2013 | <a href="news/$newsID">permalink</a></p>
+<img src="http://i.minus.com/iE8nEjmu51rGN.gif"/>
+
+<h1>FYI ANDREW, THIS STUFF IS NOT REAL (AKA IN DATABASE)</h1>
