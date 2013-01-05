9ad51d1b94c6c594ad14d2aaa75e273acd189af4
lg90 <lg90@hw.ac.uk>
2013-01-05 20:38:18 +0000

    changing some css for the account page

--- application/views/sis/account.php
+++ application/views/sis/account.php
@@ -1,11 +1,16 @@
 <? if($this->ion_auth->logged_in()){ ?>
 	<h1>Hi <?=$currentUser->firstName?>,</h1>
 
-	<a class="button tickets" href="/sis/tickets"><div class="icon"></div><div class="label">Tickets</div><div class="subtitle">Purchase and view your tickets for tournaments</div></a>
+	<a class="button tickets" href="/sis/tickets"><div class="icon"></div><div class="label">Buy Tickets</div><div class="subtitle">Purchase and view your tickets for tournaments</div></a>
 	<a class="button signup" href="/sis/signup"><div class="icon"></div><div class="label">Sign up</div><div class="subtitle">Sign up for sports tournaments</div></a>
+	<a class="button details" href="/sis/details"><div class="icon"></div><div class="label">Edit My Details</div><div class="subtitle">Change your personal details</div></a>
 	<? if($this->ion_auth->in_group('admin') || $this->ion_auth->in_group('centreadmin')){ ?>
 		<a class="button tms" href="/tms/"><div class="icon"></div><div class="label">Tournament Management System</div><div class="subtitle">Enter the tournament Management System portal</div></a>
 	<? } ?>
+
+	<p> </p>
+	<p> </p>
+	<hr>
 
 	<div id="infoMessage"><?php echo $message;?></div>
 
