be09c2568687c73d4e4086aa3685c653ae5957e0
lg90 <lg90@hw.ac.uk>
2013-01-06 00:11:32 +0000

    Creating content for pages that will be implemented in the end. Part of the work is deciding how to visualize it. So that is why I'm adding the content now so we don't have to decide on it later :)

--- application/views/sis/whatson.php
+++ application/views/sis/whatson.php
@@ -7,32 +7,42 @@ List of Upcoming matches ordered by date increasing in date. Split into days. RE
 	<div class="match-name">3rd heat</div>
 	<div class="match-game">Mens Hurdling</div>
 	<div class="match-tournament">Tournament Name</div>
+	<div class="match-venue">Somewhere</div>
 	<div class="match-time">23:99 - 23:23</div>
+	<div class="match-more">More Info</div>
 </a>
 <a class="match-item">
 	<div class="match-name">Team A vs Team B</div>
 	<div class="match-game">Wattball</div>
 	<div class="match-tournament">Tournament Name</div>
+	<div class="match-venue">Somewhere</div>
 	<div class="match-time">23:99 - 23:23</div>
+	<div class="match-more">More Info</div>
 </a>
 <h2>date 2</h2>
 <a class="match-item">
 	<div class="match-name">Team A vs Team B</div>
 	<div class="match-game">Wattball</div>
 	<div class="match-tournament">Tournament Name</div>
+	<div class="match-venue">Somewhere</div>
 	<div class="match-time">23:99 - 23:23</div>
+	<div class="match-more">More Info</div>
 </a>
 <h2>date 3</h2>
 <a class="match-item">
 	<div class="match-name">Team A vs Team B</div>
 	<div class="match-game">Wattball</div>
 	<div class="match-tournament">Tournament Name</div>
+	<div class="match-venue">Somewhere</div>
 	<div class="match-time">23:99 - 23:23</div>
+	<div class="match-more">More Info</div>
 </a>
 <h2>date 4</h2>
 <a class="match-item">
 	<div class="match-name">Team A vs Team B</div>
 	<div class="match-game">Wattball</div>
 	<div class="match-tournament">Tournament Name</div>
+	<div class="match-venue">Somewhere</div>
 	<div class="match-time">23:99 - 23:23</div>
+	<div class="match-more">More Info</div>
 </a>
