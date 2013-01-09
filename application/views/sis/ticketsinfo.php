3f2049380e1aace256fcab8c228925eb32851591
lg90 <lg90@hw.ac.uk>
2013-01-09 21:54:40 +0000

    modified the structure of the website a bit

---
+++ application/views/sis/ticketsinfo.php
@@ -0,0 +1,23 @@
+<h1>Tickets</h1>
+<p>In order to purchase tickets, you have to first <a href="/auth/register/">register an account with us</a>.</p>
+<h2>How much do tickets cost?</h2>
+<p>We have two different tickets that one can purchase.</p>
+<table>
+	<tr>
+		<td>Ticket Type</td>
+		<td>Cost</td>
+	</tr>
+	<tr>
+		<td>Adult</td>
+		<td>&pound;5</td>
+	</tr>
+	<tr>
+		<td>Concession</td>
+		<td>&pound;3</td>
+	</tr>
+</table>
+<h2>How are tickets used?</h2>
+<p>To enter a venue, you must have a printed ticket to show. This can be checked</p>
+
+<h2>I have a concession ticket, do I need identification?</h2>
+<p>Yes, a student ID will be enough to satisfy identification.</p>
