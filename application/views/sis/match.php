6c57a46b6e95ff840e452a59f74359eed4c06bc2
lg90 <lg90@hw.ac.uk>
2013-01-09 22:03:38 +0000

    modified the structure of the website a bit

--- application/views/sis/match.php
+++ application/views/sis/match.php
@@ -1,25 +1,6 @@
-<h1><a href="">WattBall</a>&gt;Hunters vs. Blue Jays</h1>
+<h1><a href="">WattBall</a> &gt; Hunters vs. Blue Jays</h1>
 <h2>Match Description</h2>
-
-<h2>Match Details</h2>
 <table>
-	<tr>
-		<th>Date</th>
-		<td>14th February 2012</td>
-	</tr>
-	<tr>
-		<th>Time</th>
-		<td>14th February 2012</td>
-	</tr>
-	<tr>
-		<th>Duration</th>
-		<td>14th February 2012</td>
-	</tr>
-	<tr>
-		<th>Venue</th>
-		<td>Pitch A</td>
-		<td><a href="">View on Google Maps</a></td>
-	</tr>
 	<tr>
 		<th>Team A</th>
 		<td>Hunters</td>
@@ -28,8 +9,24 @@
 		<th>Team B</th>
 		<td>Blue Jays</td>
 	</tr>
+</table>
+<h2>Match Details</h2>
+<table>
 	<tr>
-		<th>Team B</th>
-		<td>Blue Jays</td>
+		<th>Date</th>
+		<td>14th February 2012</td>
+	</tr>
+	<tr>
+		<th>Time</th>
+		<td>10:00 AM</td>
+	</tr>
+	<tr>
+		<th>Duration</th>
+		<td>1 hour 30 minutes</td>
+	</tr>
+	<tr>
+		<th>Venue</th>
+		<td>Pitch A</td>
+		<td><a href="">View on Google Maps</a></td>
 	</tr>
 </table>
