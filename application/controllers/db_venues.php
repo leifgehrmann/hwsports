880721bb672e5223b6a24536bd61ee3a60c7a9b6
lg90 <lg90@hw.ac.uk>
2013-01-10 22:58:57 +0000

    trying to create a better version of venues. Go ahead and laugh at how terrible I am at PHP.

--- application/controllers/db_venues.php
+++ application/controllers/db_venues.php
@@ -10,8 +10,14 @@ class Db_venues extends CI_Controller {
 	public function getVenues($centreID)
 	{
 		$output = $this->venues_model->get_venues($centreID);
-
-		echo print_r($output);
+		foreach ($output as $venueID=>$data) {
+			echo "\n<br/><br/>\n$venue[venueID]\n<br/>\n<br/>";
+			foreach ($data as $key=>$value) {
+        		echo "<br/>$key=$value\n<br/>";
+    		}
+			$value = $value * 2;
+		}
+		//echo print_r($output);
 
 	}
 }
