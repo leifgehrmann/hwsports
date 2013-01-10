8fb751a0470f7a807dbffea1a103ef3baa1a68a4
lg90 <lg90@hw.ac.uk>
2013-01-10 23:07:49 +0000

    trying to create a better version of venues. Go ahead and laugh at how terrible I am at PHP.

--- application/models/venues_model.php
+++ application/models/venues_model.php
@@ -1,20 +1,29 @@
 <?php
 class Venues_model extends CI_Model {
 
-	public function get_venues($centreID)
+	/**
+	 * Returns a 2d array of venueID => 
+	 *  
+	 * @return array
+	 **/
+
+	public function get_venues($centreID,$fields=array("name","description","directions","lat","lng"))
 	{
 		$output = array();
 		$queryString = "SELECT venueID FROM venues WHERE centreID = $centreID";
 		$query = $this->db->query($queryString);
 		$array = $query->result_array();
 		foreach($array as $venue) {
-			$dataQueryString = "SELECT " .
-				"MAX(CASE WHEN `key`='name' THEN value END ) AS name, " .
-				"MAX(CASE WHEN `key`='description' THEN value END ) AS description, " .
-				"MAX(CASE WHEN `key`='directions' THEN value END ) AS directions, " .
-				"MAX(CASE WHEN `key`='lat' THEN value END ) AS lat, " .
-				"MAX(CASE WHEN `key`='lng' THEN value END ) AS lng " .
-				"FROM venueData WHERE venueID = {$venue['venueID']}";
+			$dataQueryString = "SELECT ";
+				foreach($fields as $field) {
+					$dataQueryString .= "MAX(CASE WHEN `key`='$field' THEN value END ) AS $field, "
+				}
+				//"MAX(CASE WHEN `key`='name' THEN value END ) AS name, " .
+				//"MAX(CASE WHEN `key`='description' THEN value END ) AS description, " .
+				//"MAX(CASE WHEN `key`='directions' THEN value END ) AS directions, " .
+				//"MAX(CASE WHEN `key`='lat' THEN value END ) AS lat, " .
+				//"MAX(CASE WHEN `key`='lng' THEN value END ) AS lng " .
+				$dataQueryString .= "FROM venueData WHERE venueID = {$venue['venueID']}";
 			$dataQuery = $this->db->query($dataQueryString);
 			$output[$venue['venueID']] = array_merge($venue, $dataQuery->row_array());
 		}
