69829515e5b25850815a0eef16f86847ac41260b
lg90 <lg90@hw.ac.uk>
2013-01-10 09:31:24 +0000

    modified the structure of the website a bit

--- application/views/sis/tournaments.php
+++ application/views/sis/tournaments.php
@@ -4,24 +4,24 @@
 	<h3>2013</h3>
 	<a href="/sis/tournament/2" class="tournament-item">
 		<div class="tournament-name">2013 Heriot Watt Tournament</div>
-		<div class="tournament-start-date">7th January, 2013</div>
-		<div class="tournament-end-date">12th January, 2013</div>
+		<div class="tournament-date-start">7th January, 2013</div>
+		<div class="tournament-date-end">12th January, 2013</div>
 	</a>
 	<h3>2012</h3>
 	<a href="/sis/tournament/2" class="tournament-item">
 		<div class="tournament-name">Wattball Tournament</div>
-		<div class="tournament-start-date">7th January, 2012</div>
-		<div class="tournament-end-date">12th January, 2012</div>
+		<div class="tournament-date-start">7th January, 2012</div>
+		<div class="tournament-date-end">12th January, 2012</div>
 	</a>
 	<a href="/sis/tournament/2" class="tournament-item">
 		<div class="tournament-name">Rugby Tournament</div>
-		<div class="tournament-start-date">7th January, 2012</div>
-		<div class="tournament-end-date">12th January, 2012</div>
+		<div class="tournament-date-start">7th January, 2012</div>
+		<div class="tournament-date-end">12th January, 2012</div>
 	</a>
 	<a href="/sis/tournament/2" class="tournament-item">
 		<div class="tournament-name">Marathon Tournament</div>
-		<div class="tournament-start-date">7th January, 2012</div>
-		<div class="tournament-end-date">12th January, 2012</div>
+		<div class="tournament-date-start">7th January, 2012</div>
+		<div class="tournament-date-end">12th January, 2012</div>
 	</a>
 	<a href="/sis/tournament/2" class="tournament-button-next">Previous Tournaments</a>
 </div>
