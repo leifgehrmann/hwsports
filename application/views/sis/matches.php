631d74d4af52e483590970a0a3774a684d2c2980
lg90 <lg90@hw.ac.uk>
2013-01-10 08:50:34 +0000

    modified the structure of the website a bit

--- application/views/sis/matches.php
+++ application/views/sis/matches.php
@@ -2,63 +2,75 @@
 
 <div class="matches-upcoming">
 	<h2>Upcoming Matches</h2>
-	<h3>14th February, 2012</h3>
-	<a href="" class="match-item">
-		<div class="match-name">3rd heat</div>
+	<h3>11th January, 2013</h3>
+	<a href="/sis/match/2" class="match-item">
+		<div class="match-name">The Red Cows vs. Recoba</div>
 		<div class="match-game">Mens Hurdling</div>
-		<div class="match-tournament">Tournament Name</div>
-		<div class="match-venue">Somewhere</div>
-		<div class="match-time">23:99 - 23:23</div>
+		<div class="match-tournament">2013 Heriot Watt Tournament</div>
+		<div class="match-venue">Pitch A</div>
+		<div class="match-time">10:00 - 11:30</div>
 	</a>
-	<a href="" class="match-item">
-		<div class="match-name">Team A vs Team B</div>
+	<a href="/sis/match/2" class="match-item">
+		<div class="match-name">Greens vs. Hunters</div>
 		<div class="match-game">Wattball</div>
-		<div class="match-tournament">Tournament Name</div>
-		<div class="match-venue">Somewhere</div>
-		<div class="match-time">23:99 - 23:23</div>
+		<div class="match-tournament">2013 Heriot Watt Tournament</div>
+		<div class="match-venue">Pitch B</div>
+		<div class="match-time">10:00 - 11:30</div>
 	</a>
-	<h3>15th February, 2012</h3>
-	<a href="" class="match-item">
-		<div class="match-name">Team A vs Team B</div>
+	<a href="/sis/match/2" class="match-item">
+		<div class="match-name">Blue Jays vs. Wattbulls</div>
 		<div class="match-game">Wattball</div>
-		<div class="match-tournament">Tournament Name</div>
-		<div class="match-venue">Somewhere</div>
-		<div class="match-time">23:99 - 23:23</div>
+		<div class="match-tournament">2013 Heriot Watt Tournament</div>
+		<div class="match-venue">Pitch A</div>
+		<div class="match-time">14:00 - 15:30</div>
 	</a>
-	<h3>16th February</h3>
-	<a href="" class="match-item">
-		<div class="match-name">Team A vs Team B</div>
-		<div class="match-game">Wattball</div>
-		<div class="match-tournament">Tournament Name</div>
-		<div class="match-venue">Somewhere</div>
-		<div class="match-time">23:99 - 23:23</div>
+	<a href="/sis/match/2" class="match-item">
+		<div class="match-name">Round 4</div>
+		<div class="match-game">Hurdling</div>
+		<div class="match-tournament">2013 Heriot Watt Tournament</div>
+		<div class="match-venue">Track 1</div>
+		<div class="match-time">12:00 - 14:00</div>
 	</a>
-	<a href="" class="match-button-next">Full Upcoming List</a>
+	<h3>12th January, 2013</h3>
+	<a href="/sis/match/2" class="match-item">
+		<div class="match-name">Round 5</div>
+		<div class="match-game">Hurdling</div>
+		<div class="match-tournament">2013 Heriot Watt Tournament</div>
+		<div class="match-venue">Track 1</div>
+		<div class="match-time">12:00 - 14:00</div>
+	</a>
+	<a href="/sis/match/2" class="match-button-next">Full Upcoming List</a>
 </div>
 <div class="matches-latest">
 	<h2>Latest Matches</h2>
-	<h3>13th February</h3>
-	<a href="" class="match-item">
-		<div class="match-name">Team A vs Team B</div>
-		<div class="match-game">Wattball</div>
-		<div class="match-tournament">Tournament Name</div>
-		<div class="match-venue">Somewhere</div>
-		<div class="match-time">23:99 - 23:23</div>
+	<h3>10th January, 2013</h3>
+	<a href="/sis/match/2" class="match-item">
+		<div class="match-name">The Red Cows vs. Blue Jays</div>
+		<div class="match-game">Mens Hurdling</div>
+		<div class="match-tournament">2013 Heriot Watt Tournament</div>
+		<div class="match-venue">Pitch A</div>
+		<div class="match-time">10:00 - 11:30</div>
 	</a>
-	<a href="" class="match-item">
-		<div class="match-name">Team A vs Team B</div>
+	<a href="/sis/match/2" class="match-item">
+		<div class="match-name">Greens vs. The Red Cows</div>
 		<div class="match-game">Wattball</div>
-		<div class="match-tournament">Tournament Name</div>
-		<div class="match-venue">Somewhere</div>
-		<div class="match-time">23:99 - 23:23</div>
+		<div class="match-tournament">2013 Heriot Watt Tournament</div>
+		<div class="match-venue">Pitch B</div>
+		<div class="match-time">10:00 - 11:30</div>
 	</a>
-	<h3>12th February</h3>
-	<a href="" class="match-item">
-		<div class="match-name">Team A vs Team B</div>
+	<a href="/sis/match/2" class="match-item">
+		<div class="match-name">Recoba vs. Hunters</div>
 		<div class="match-game">Wattball</div>
-		<div class="match-tournament">Tournament Name</div>
-		<div class="match-venue">Somewhere</div>
-		<div class="match-time">23:99 - 23:23</div>
+		<div class="match-tournament">2013 Heriot Watt Tournament</div>
+		<div class="match-venue">Pitch A</div>
+		<div class="match-time">14:00 - 15:30</div>
 	</a>
-	<a href="" class="match-button-prev">Previous Matches</a>
+	<a href="/sis/match/2" class="match-item">
+		<div class="match-name">Round 3</div>
+		<div class="match-game">Hurdling</div>
+		<div class="match-tournament">2013 Heriot Watt Tournament</div>
+		<div class="match-venue">Track 1</div>
+		<div class="match-time">12:00 - 14:00</div>
+	</a>
+	<a href="/sis/match/2" class="match-button-prev">Previous Matches</a>
 </div>
