3f97ebe6458635118b99117140717681aba1f245
lg90 <lg90@hw.ac.uk>
2013-01-10 23:21:19 +0000

    trying to create a better version of venues. Go ahead and laugh at how terrible I am at PHP.

--- application/models/venues_model.php
+++ application/models/venues_model.php
@@ -2,37 +2,43 @@
 class Venues_model extends CI_Model {
 
 	/**
-	 * Returns a 2d array of venueID => 
+	 * Returns a 2d array of venue data
 	 *  
 	 * @return array
 	 **/
 
-	public function get_venues($centreID,$fields=array("name","description","directions","lat","lng"))
+	public function get_all_venues_data($centreID, $fields=array("name","description","directions","lat","lng"))
 	{
 		$output = array();
 		$queryString = "SELECT venueID FROM venues WHERE centreID = $centreID";
 		$query = $this->db->query($queryString);
 		$array = $query->result_array();
 		foreach($array as $venue) {
-			$dataQueryString = "SELECT ";
-			foreach($fields as $field) {
-				$dataQueryString .= "MAX(CASE WHEN `key`='$field' THEN value END ) AS $field, ";
-			}
-				//"MAX(CASE WHEN `key`='name' THEN value END ) AS name, " .
-				//"MAX(CASE WHEN `key`='description' THEN value END ) AS description, " .
-				//"MAX(CASE WHEN `key`='directions' THEN value END ) AS directions, " .
-				//"MAX(CASE WHEN `key`='lat' THEN value END ) AS lat, " .
-				//"MAX(CASE WHEN `key`='lng' THEN value END ) AS lng " .
-				$dataQueryString .= "FROM venueData WHERE venueID = {$venue['venueID']}";
-			$dataQuery = $this->db->query($dataQueryString);
-			$output[$venue['venueID']] = array_merge($venue, $dataQuery->row_array());
+			$output[] = get_venue_data($venue['venueID'],$fields);
 		}
 		return $output;
 	}
 
-	public function get_venue_data($centreID, $venueID)
+	/**
+	 * Returns an array of data from a specific venue
+	 *  
+	 * @return array
+	 **/
+	public function get_venue_data($venueID, $fields=array("name","description","directions","lat","lng"))
 	{
-
+		$dataQueryString = "SELECT ";
+		$i = 0;
+		$len = count($array);
+		foreach($fields as $field) {
+			$dataQueryString .= "MAX(CASE WHEN `key`='$field' THEN value END ) AS $field";
+			if($i==$len-1)
+				$dataQueryString .= ", ";
+			$i++;
+		}
+		$dataQueryString .= "FROM venueData WHERE venueID = {$venue['venueID']}";
+		$dataQuery = $this->db->query($dataQueryString);
+		$output = array_merge($venue, $dataQuery->row_array());
+		return $output;
 	}
 
 	public function create_venue($centreID, $data)
