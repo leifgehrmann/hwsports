484341ec6227ae3c0434dfa6571757ea48145cbf
lg90 <lg90@hw.ac.uk>
2013-01-11 01:26:17 +0000

    trying to create a better version of venues. Go ahead and laugh at how terrible I am at PHP.

--- application/views/tms/footer.php
+++ application/views/tms/footer.php
@@ -8,6 +8,6 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/vendor/jquery-1.8.1.min.js"><\/script>')</script>
 		<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
-		<script type="text/javascript" src="js/<?=$page?>"></script>
+		<script type="text/javascript" src="js/<?=$page?>.js"></script>
     </body>
 </html>
