e7bcdba8c52b60d5dda8e46348b4fa5c923e5671
lg90 <lg90@hw.ac.uk>
2013-01-11 10:19:21 +0000

    trying to create a better version of venues. Go ahead and laugh at how terrible I am at PHP.

--- application/controllers/db_venues.php
+++ application/controllers/db_venues.php
@@ -29,11 +29,11 @@ class Db_venues extends MY_Controller {
 
 	*/
 
-	// NVM, we don't need this for JSON
+	// 
 	public function get_venues($centreID)
 	{
 		$output = $this->venues_model->get_venues($centreID);
-		$this->data['data'] = $output;
+		$this->data['data'] =  "var data = ".json_encode($output);
 
 		$this->load->view('data', $this->data);
 	}
