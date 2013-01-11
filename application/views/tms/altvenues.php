a06465e45c1139f7db5db891dcb69184ff67b773
lg90 <lg90@hw.ac.uk>
2013-01-11 21:00:42 +0000

    trying to create a better version of venues. Go ahead and laugh at how terrible I am at PHP.

--- application/views/tms/altvenues.php
+++ application/views/tms/altvenues.php
@@ -49,40 +49,8 @@
 	</p>
 
 	<p>Location (drag map center to venue position):</p>
-	<div id="map" style="width: 400px; height: 250px;"></div>
+	<div id="map" class="venues-map"></div>
 
 	<p><?=form_submit('submit', 'Create Venue');?></p>
 		
 <?=form_close();?>
-
-
-<!--<script type="text/javascript">
-	$(document).ready(function() {
-		// a global variable to access the map
-		var map;
-		var centre_marker;
-		var centre_pos  = new google.maps.LatLng( jQuery('input[name="lat"]').val(), jQuery('input[name="lng"]').val() );
-
-		//function zoomIn(){ map.setZoom(map.getZoom()+1);}
-		//function zoomOut(){ map.setZoom(map.getZoom()-1);}
-
-		function initialize(){
-			map = new google.maps.Map(document.getElementById('map'), {
-				zoom: 15,
-				center: centre_pos,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			});
-			
-			centre_marker = new google.maps.Marker({ position: centre_pos, map: map, title: "New Venue Location" });
-
-			google.maps.event.addListener(map, 'center_changed', function() {
-				var newcentre = map.getCenter();
-				centre_marker.setPosition(newcentre);
-				jQuery('input[name=lat]').val(newcentre.lat());
-				jQuery('input[name=lng]').val(newcentre.lng());
-			});
-		}
-
-		google.maps.event.addDomListener(window, 'load', initialize);
-	}
-</script>-->
