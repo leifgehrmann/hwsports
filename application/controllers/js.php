e112e19041bf5fdb7777eb716d1672d0e93f7b1b
lg90 <lg90@hw.ac.uk>
2013-01-11 01:25:33 +0000

    trying to create a better version of venues. Go ahead and laugh at how terrible I am at PHP.

--- application/controllers/js.php
+++ application/controllers/js.php
@@ -4,12 +4,12 @@ class Js extends MY_Controller {
 
 	public function index($file)
 	{				
-		/*if ( ! file_exists('/application/views/js/'.$file))
+		if ( ! file_exists('application/views/js/'.$file))
 		{
 			// Whoops, we don't have a page for that!
 			show_404();
-		}*/
-		
+		}
+				
 		$this->output->set_header("Content-Type: text/javascript"); 
 		$this->load->view('js/'.$file);
 	}
