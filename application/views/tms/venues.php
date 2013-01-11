7d95c90e7e7e01297057ca47e3cce6b794d60c28
lg90 <lg90@hw.ac.uk>
2013-01-11 01:00:14 +0000

    trying to create a better version of venues. Go ahead and laugh at how terrible I am at PHP.

--- application/views/tms/venues.php
+++ application/views/tms/venues.php
@@ -92,30 +92,33 @@ $GLOBALS['oDB']->dbClose();
 <?=form_close();?>
 
 <script type="text/javascript">
-// a global variable to access the map
-var map;
-var centre_marker;
-var centre_pos  = new google.maps.LatLng( jQuery('input[name="lat"]').val(), jQuery('input[name="lng"]').val() );
 
-function zoomIn(){ map.setZoom(map.getZoom()+1);}
-function zoomOut(){ map.setZoom(map.getZoom()-1);}
+	$(document).ready(function() {
+		// a global variable to access the map
+		var map;
+		var centre_marker;
+		var centre_pos  = new google.maps.LatLng( jQuery('input[name="lat"]').val(), jQuery('input[name="lng"]').val() );
 
-function initialize(){
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15,
-		center: centre_pos,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	});
-	
-	centre_marker = new google.maps.Marker({ position: centre_pos, map: map, title: "New Venue Location" });
+		function zoomIn(){ map.setZoom(map.getZoom()+1);}
+		function zoomOut(){ map.setZoom(map.getZoom()-1);}
 
-	google.maps.event.addListener(map, 'dragend', function() {
-		var newcentre = map.getCenter();
-		centre_marker.setPosition(newcentre);
-		jQuery('input[name=lat]').val(newcentre.lat());
-		jQuery('input[name=lng]').val(newcentre.lng());
-	});
-}
+		function initialize(){
 
-google.maps.event.addDomListener(window, 'load', initialize);
+			map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 15,
+				center: centre_pos,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			});
+			
+			centre_marker = new google.maps.Marker({ position: centre_pos, map: map, title: "New Venue Location" });
+
+			google.maps.event.addListener(map, 'dragend', function() {
+				var newcentre = map.getCenter();
+				centre_marker.setPosition(newcentre);
+				jQuery('input[name=lat]').val(newcentre.lat());
+				jQuery('input[name=lng]').val(newcentre.lng());
+			});
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
+	}
 </script>
