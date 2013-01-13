229ab74da3041f8ec435592ebcae0ab30957a8d3
lg90 <lg90@hw.ac.uk>
2013-01-13 14:07:23 +0000

    trying to create a better version of venues. Go ahead and laugh at how terrible I am at PHP.

--- application/controllers/db_venues.php
+++ application/controllers/db_venues.php
@@ -95,7 +95,8 @@ class Db_venues extends MY_Controller {
 
 				$data = array();
 				for ($i = 0; $i < $formLength; $i++) {
-					array_push($formNames[$i]=>$this->input->post($formNames[$i]));
+					insertDataArray[$formNames[$i]] = $this->input->post($formNames[$i]);
+					//array_push($formNames[$i]=>);
 					/*$row = array(
 						'key' => $formNames[$i],
 						'value' => $this->input->post($formNames[$i])
