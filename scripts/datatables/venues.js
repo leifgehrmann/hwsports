957ec0649ce76846d957fac938b9039a503c86df
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-13 22:21:30 +0000

    removed rico, added datatables to venues

--- scripts/datatables/venues.js
+++ scripts/datatables/venues.js
@@ -46,6 +46,7 @@
 				"onOpen": function ( settings, json ) {
 					$('.DTE_Action_Create .DTE_Body_Content').append("<div id='mapcontainer'></div>");
 					$('#mapcontainer').append($('#venuemap'));
+					$('#venuemap').toggle();
 					google.maps.event.trigger(map, 'resize');
 				}
 			}
