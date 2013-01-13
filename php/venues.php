56651606c4fcd1744f711a67f228364995d16a7d
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-13 18:57:32 +0000

    removed rico, added datatables to venues

--- php/venues.php
+++ php/venues.php
@@ -34,17 +34,16 @@ $out = $editor
 // for the 'department' select list and 'access' radio boxes
 if ( !isset($_POST['action']) ) {
 	foreach ( $out['aaData'] as $aaDataID => $user ) {
-	print_r($user['userID']);
+
 	$out['aaData'][$aaDataID]['name'] = $db
 		->query( 'select' )
 		->table( 'users' )
-		->get( 'value as name' )
+		->get( 'value' )
 		->where( 'userID', $user['userID'] )
-		->where( 'key', 'name' )
 		->exec();
 	}
 }
 
 // Send it back to the client
-//echo json_encode( $out );
+echo json_encode( $out );
 
