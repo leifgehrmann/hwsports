3b4f0c1d71d4f363a2e3f9c1a4b4941252d170c4
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-14 22:11:24 +0000

    sports

--- application/views/tms/matches.php
+++ application/views/tms/matches.php
@@ -2,8 +2,8 @@
 <p>Below is a list of upcoming matches and tournaments.</p>
 <ul>
 	<li>Tournaments are coloured as <span style="color:rgb(73, 134, 231);font-weight:bold;">blue</span>.</li>
-	<li>Matches are coloured as <span style="color:rgb(123, 209, 72);font-weight:bold;">green</span>.</li>
-	<li>Registration periods are coloured as <span style="color:rgb(250, 87, 60);font-weight:bold;">red</span>.</li>
+	<li>Tournament matches are coloured as <span style="color:rgb(123, 209, 72);font-weight:bold;">green</span>.</li>
+	<li>Standard bookings are coloured as <span style="color:rgb(123, 209, 72);font-weight:bold;">brown</span>.</li>
 </ul>
 <p>Click the matches/tournaments for more information.</p>
 
