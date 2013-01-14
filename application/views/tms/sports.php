7314cf9693084956ce609b10be89ee8be838f13d
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-14 18:50:57 +0000

    venues

--- application/views/tms/sports.php
+++ application/views/tms/sports.php
@@ -1,1 +1,38 @@
-This is the sports. It is nothing. Nor is your face.
+<h1>Venue List</h1>
+
+<div id="venuemap"></div>
+<div id="centreLat" style="display:none;"><?=$centreLat?></div>
+<div id="centreLng" style="display:none;"><?=$centreLng?></div>
+
+<div id="main">
+
+	<div id="demo">
+		<table cellpadding="0" cellspacing="0" border="0" class="display" id="venues" width="100%">
+			<thead>
+				<tr>
+					<th width="30%">Centre ID</th>
+					<th width="30%">Lat</th>
+					<th width="30%">Lng</th>
+					<th width="30%">Venue ID</th>
+					<th width="30%">Name</th>
+					<th width="30%">Description</th>
+					<th width="30%">Directions</th>
+				</tr>
+			</thead>
+		</table>
+	</div>
+	<div class="spacer"></div>
+	<div id="centreID" style="display:none;"><?=$centre['id']?></div>
+</div><!-- /#main -->
+
+
+<link rel='stylesheet' href='/css/jquery.dataTables.css'>
+<link rel='stylesheet' href='/css/dataTables.tabletools.css'>
+<link rel='stylesheet' href='/css/dataTables.editor.css'>
+<link rel='stylesheet' href='http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.23/themes/eggplant/jquery-ui.css'>
+<script src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
+<script src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.23/jquery-ui.min.js"></script>
+<script src="/scripts/datatables/jquery.dataTables.min.js"></script>
+<script src="/scripts/datatables/dataTables.tabletools.min.js"></script>
+<script src="/scripts/datatables/dataTables.editor.min.js"></script>
+<script src="/scripts/datatables/venues.js"></script>
