788718017e95a8bb846b6ffa505965ed900a7cc9
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-14 22:35:59 +0000

    matches

--- application/controllers/calendar.php
+++ application/controllers/calendar.php
@@ -7,225 +7,211 @@ class Calendar extends MY_Controller {
 		parent::__construct();
 
 	}
-	public function getAllMatches()
-	{
-		$this->data['data'] = <<<EOF
-		[
-
-				// Matches
-
-					// Monday
-					{
-						title: 'Men\'s Hurdling - Preliminary Rounds',
-						start: new Date(y, m, d-3, 12, 0),
-						end:   new Date(y, m, d-3, 14, 0),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/1'
-					},
-					{
-						title: 'WattBall - Recoba vs. WattBulls',
-						start: new Date(y, m, d-3, 10, 0),
-						end:   new Date(y, m, d-3, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'WattBall - The Red Cows vs. Hunters',
-						start: new Date(y, m, d-3, 10, 0),
-						end:   new Date(y, m, d-3, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'WattBall - Blue Jays vs. Greens',
-						start: new Date(y, m, d-3, 14, 0),
-						end:   new Date(y, m, d-3, 15, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
+	public function getAllMatches() {
+		$this->data['data'] = json_encode(array(
+			array(
+				'title' => 'Men\'s Hurdling - Preliminary Rounds',
+				'start' => 2013,
+				'end' => mktime(14, 0, 0, 1, 10-3, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/1'
+			),
+			array(
+				'title' => 'WattBall - Recoba vs. WattBulls',
+				'start' => mktime(10, 0, 0, 1, 10-3, 2013),
+				'end' => mktime(11, 30, 0, 1, 10-3, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'WattBall - The Red Cows vs. Hunters',
+				'start' => mktime(10, 0, 0, 1, 10-3, 2013),
+				'end' => mktime(11, 30, 0, 1, 10-3, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'WattBall - Blue Jays vs. Greens',
+				'start' => mktime(14, 0, 0, 1, 10-3, 2013),
+				'end' => mktime(15, 30, 0, 1, 10-3, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
 					
+			// Tuesday
+			array(
+				'title' => 'Men\'s Hurdling - Round 1',
+				'start' => mktime(12, 0, 0, 1, 10-2, 2013),
+				'end' => mktime(14, 0, 0, 1, 10-2, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/1'
+			),
+			array(
+				'title' => 'WattBall - The Red Cows vs. Greens',
+				'start' => mktime(10, 0, 0, 1, 10-2, 2013),
+				'end' => mktime(11, 30, 0, 1, 10-2, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'WattBall - Blue Jays vs. Recoba',
+				'start' => mktime(10, 0, 0, 1, 10-2, 2013),
+				'end' => mktime(11, 30, 0, 1, 10-2, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'WattBall - Hunters vs. WattBulls',
+				'start' => mktime(14, 0, 0, 1, 10-2, 2013),
+				'end' => mktime(15, 30, 0, 1, 10-2, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
 
-					// Tuesday
-					{
-						title: 'Men\'s Hurdling - Round 1',
-						start: new Date(y, m, d-2, 12, 0),
-						end:   new Date(y, m, d-2, 14, 0),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/1'
-					},
-					{
-						title: 'WattBall - The Red Cows vs. Greens',
-						start: new Date(y, m, d-2, 10, 0),
-						end:   new Date(y, m, d-2, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'WattBall - Blue Jays vs. Recoba',
-						start: new Date(y, m, d-2, 10, 0),
-						end:   new Date(y, m, d-2, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'WattBall - Hunters vs. WattBulls',
-						start: new Date(y, m, d-2, 14, 0),
-						end:   new Date(y, m, d-2, 15, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
+			// Wednesday
+			array(
+				'title' => 'Men\'s Hurdling - Round 2',
+				'start' => mktime(12, 0, 0, 1, 10-1, 2013),
+				'end' => mktime(14, 0, 0, 1, 10-1, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/1'
+			),
+			array(
+				'title' => 'WattBall - The Red Cows vs. WattBulls',
+				'start' => mktime(10, 0, 0, 1, 10-1, 2013),
+				'end' => mktime(11, 30, 0, 1, 10-1, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'WattBall - Recoba vs. Greens',
+				'start' => mktime(14, 0, 0, 1, 10-1, 2013),
+				'end' => mktime(15, 30, 0, 1, 10-1, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'Wattball - Hunters vs. Blue Jays',
+				'start' => mktime(10, 0, 0, 1, 10-1, 2013),
+				'end' => mktime(11, 30, 0, 1, 10-1, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/'
+			),
 
-					// Wednesday
+			// Thursday
+			array(
+				'title' => 'Men\'s Hurdling - Round 3',
+				'start' => mktime(12, 0, 0, 1, 10, 2013),
+				'end' => mktime(14, 0, 0, 1, 10, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/1'
+			),
+			array(
+				'title' => 'WattBall - The Red Cows vs. Blue Jays',
+				'start' => mktime(10, 0, 0, 1, 10, 2013),
+				'end' => mktime(11, 30, 0, 1, 10, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'WattBall - Recoba vs. Hunters',
+				'start' => mktime(14, 0, 0, 1, 10, 2013),
+				'end' => mktime(15, 30, 0, 1, 10, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'Wattball - Greens vs. The Red Cows',
+				'start' => mktime(10, 0, 0, 1, 10, 2013),
+				'end' => mktime(11, 30, 0, 1, 10, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/'
+			),
 
-					{
-						title: 'Men\'s Hurdling - Round 2',
-						start: new Date(y, m, d-1, 12, 0),
-						end:   new Date(y, m, d-1, 14, 0),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/1'
-					},
-					{
-						title: 'WattBall - The Red Cows vs. WattBulls',
-						start: new Date(y, m, d-1, 10, 0),
-						end:   new Date(y, m, d-1, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'WattBall - Recoba vs. Greens',
-						start: new Date(y, m, d-1, 14, 0),
-						end:   new Date(y, m, d-1, 15, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'Wattball - Hunters vs. Blue Jays',
-						start: new Date(y, m, d-1, 10, 0),
-						end:   new Date(y, m, d-1, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/'
-					},
+			// Friday
+			array(
+				'title' => 'Men\'s Hurdling - Round 4',
+				'start' => mktime(12, 0, 0, 1, 10+1, 2013),
+				'end' => mktime(14, 0, 0, 1, 10+1, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/1'
+			),
+			array(
+				'title' => 'WattBall - The Red Cows vs. Recoba',
+				'start' => mktime(10, 0, 0, 1, 10+1, 2013),
+				'end' => mktime(11, 30, 0, 1, 10+1, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'WattBall - Blue Jays vs. WattBulls',
+				'start' => mktime(14, 0, 0, 1, 10+1, 2013),
+				'end' => mktime(15, 30, 0, 1, 10+1, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/2'
+			),
+			array(
+				'title' => 'Wattball - Greens vs. Hunters',
+				'start' => mktime(10, 0, 0, 1, 10+1, 2013),
+				'end' => mktime(11, 30, 0, 1, 10+1, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/'
+			),
 
-					// Thursday
+			// Saturday
+			array(
+				'title' => 'Men\'s Hurdling - Round 5',
+				'start' => mktime(12, 0, 0, 1, 10+2, 2013),
+				'end' => mktime(14, 0, 0, 1, 10+2, 2013),
+				'allDay' => false,
+				'color' => '#2966C7',
+				'url' => '/sis/match/1'
+			),
 
-					{
-						title: 'Men\'s Hurdling - Round 3',
-						start: new Date(y, m, d, 12, 0),
-						end:   new Date(y, m, d, 14, 0),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/1'
-					},
-					{
-						title: 'WattBall - The Red Cows vs. Blue Jays',
-						start: new Date(y, m, d, 10, 0),
-						end:   new Date(y, m, d, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'WattBall - Recoba vs. Hunters',
-						start: new Date(y, m, d, 14, 0),
-						end:   new Date(y, m, d, 15, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'Wattball - Greens vs. The Red Cows',
-						start: new Date(y, m, d, 10, 0),
-						end:   new Date(y, m, d, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/'
-					},
+			// Tournaments
+			array(
+				'title' => 'Heriot Watt Tournament 2013',
+				'start' => mktime(0, 0, 0, 1, 10-3, 2013),
+				'end' => mktime(0, 0, 0, 1, 10+2, 2013),
+				'color' => '#5AB128',
+				'url' => '/sis/tournament/1'
+			),
 
-					// Friday
-					{
-						title: 'Men\'s Hurdling - Round 4',
-						start: new Date(y, m, d+1, 12, 0),
-						end:   new Date(y, m, d+1, 14, 0),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/1'
-					},
-					{
-						title: 'WattBall - The Red Cows vs. Recoba',
-						start: new Date(y, m, d+1, 10, 0),
-						end:   new Date(y, m, d+1, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'WattBall - Blue Jays vs. WattBulls',
-						start: new Date(y, m, d+1, 14, 0),
-						end:   new Date(y, m, d+1, 15, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/2'
-					},
-					{
-						title: 'Wattball - Greens vs. Hunters',
-						start: new Date(y, m, d+1, 10, 0),
-						end:   new Date(y, m, d+1, 11, 30),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/'
-					},
+			// Registration times
 
-					// Saturday
-					{
-						title: 'Men\'s Hurdling - Round 5',
-						start: new Date(y, m, d+2, 12, 0),
-						end:   new Date(y, m, d+2, 14, 0),
-						allDay: false,
-						color: '#2966C7',
-						url: '/sis/match/1'
-					},
-
-
-				// Tournaments
-
-				{
-					title: 'Heriot Watt Tournament 2013',
-					start: new Date(y, m, d-3),
-					end: new Date(y, m, d+2),
-					color: '#5AB128',
-					url: '/sis/tournament/1'
-				},
-
-
-				// Registration times
-
-				{
-					title: 'WattBall Registration Period',
-					start: new Date(y, m, d-30),
-					end: new Date(y, m, d-5),
-					color: '#EA472C'
-				},
-				{
-					title: 'Heriot Hurdling Registration Period',
-					start: new Date(y, m, d-30),
-					end: new Date(y, m, d-7),
-					color: '#EA472C'
-				}
-			]
-		
-EOF;
+			array(
+				'title' => 'WattBall Registration Period',
+				'start' => mktime(0, 0, 0, 1, 10-30, 2013),
+				'end' => mktime(0, 0, 0, 1, 10-5, 2013),
+				'color' => '#EA472C'
+			),
+			array(
+				'title' => 'Heriot Hurdling Registration Period',
+				'start' => mktime(0, 0, 0, 1, 10-30, 2013),
+				'end' => mktime(0, 0, 0, 1, 10-7, 2013),
+				'color' => '#EA472C'
+			)
+		));
 		$this->load->view('data',$this->data);
 	}
 }
