24bbba6636a39f28d48e2d8933d23ed7145c4e7f
lg90 <lg90@hw.ac.uk>
2013-01-17 09:24:23 +0000

    changed product page

--- application/views/product/home.php
+++ application/views/product/home.php
@@ -1,11 +1,9 @@
 <h1>Welcome</h1>
 
-<p>INSERT TEXT HERE CONNOR BOYLE</p>
-
-<ul>
-	<li>Something about how to sign up for bla bla</li>
-	<li>Something about What this product actually is</li>
-	<li>Maybe some pretty pictures of the product?</li>
-</ul>
-
-<p>And to finish it all off, we...</p>
+<p><b>Infusion Sports</b> is sports tournament scheduling software.</p>
+<p>With the recent Olympic games of 2012 and the forthcoming Commonwealth games 
+in Glasgow the sports and leisure industry size is set to increase. In fact, as a whole in March 
+2010 the industry turnover was estimated at over 200 billion pounds in the UK alone. What's more, 
+this means that uptake of sports is set to increase and subsequently sports tournaments.</p>
+<p>Infusion Sports can aid in the hectic organisation of sports events. Why don't you drop us a line at : 
+<a href="mailto:Signup@infusionsports.co.uk">Signup@InfusionSports.co.uk</a> and see what our Infusion Sports can do for your business.</p>
