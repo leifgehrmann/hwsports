725020fa2bf33fac008d2c9cc3787d8acbc579eb
lg90 <lg90@hw.ac.uk>
2013-01-18 23:37:54 +0000

    changed some stuff in the venues view

--- application/views/tms/venues.php
+++ application/views/tms/venues.php
@@ -5,28 +5,25 @@
 <div id="centreLng" style="display:none;"><?=$centreLng?></div>
 
 <div id="main">
-
-	<div id="demo">
-		<table cellpadding="0" cellspacing="0" border="0" class="display" id="venues" width="100%">
-			<thead>
-				<tr>
-					<th width="0%">Centre ID</th>
-					<th width="0%">Lat</th>
-					<th width="0%">Lng</th>
-					<th width="0%">Venue ID</th>
-					<th width="20%">Name</th>
-					<th width="30%">Description</th>
-					<th width="45%">Directions</th>
-					<th width="5%">&nbsp;</th>
-				</tr>
-			</thead>
-		</table>
-	</div>
+	<table cellpadding="0" cellspacing="0" border="0" class="display" id="venues" width="100%">
+		<thead>
+			<tr>
+				<th width="0%">Centre ID</th>
+				<th width="0%">Lat</th>
+				<th width="0%">Lng</th>
+				<th width="0%">Venue ID</th>
+				<th width="20%">Name</th>
+				<th width="30%">Description</th>
+				<th width="45%">Directions</th>
+				<th width="5%">&nbsp;</th>
+			</tr>
+		</thead>
+	</table>
 	<div class="spacer"></div>
 	<div id="centreID" style="display:none;"><?=$centre['id']?></div>
-</div><!-- /#main -->
+</div>
 
-
+<!-- /#main -->
 <link rel='stylesheet' href='/css/jquery.dataTables.css'>
 <link rel='stylesheet' href='/css/dataTables.tabletools.css'>
 <link rel='stylesheet' href='/css/dataTables.editor.css'>
