77f0432c6849c63c382ecc8e40c9e9b9a01e7340
lg90 <lg90@hw.ac.uk>
2013-01-19 20:37:02 +0000

    Changed some tms css

--- application/views/tms/venue.php
+++ application/views/tms/venue.php
@@ -4,14 +4,14 @@
 	$fields = array("name","description","directions");
 	$labels = array("Venue Name","Description","Directions");
 	$types = array("text","textfield","textfield");
-	$widths = array("20%","60%","20%");
+	$widths = array("15%","40%","20%");
 
 	echo "<table>";
 	for($i=0;$i<count($fields);$i++){
 		echo "\t<tr>";
 		echo "\t\t<th style='width:{$widths[0]}'>{$labels[$i]}</th>";
 		if($types[$i]=="text")
-			echo "\t\t<td style='width:{$widths[1]}'><input type='text' oldvalue='' onchange='' value='{$this->data['venue'][$fields[$i]]}'></td>";
+			echo "\t\t<td style='width:{$widths[1]}'><input name='{$labels[$i]}' type='text' oldvalue='' onchange='' value='{$this->data['venue'][$fields[$i]]}'></td>";
 		else if($types[$i]=="textfield")
 			echo "\t\t<td style='width:{$widths[1]}'><textarea onchange=''>{$this->data['venue'][$fields[$i]]}</textarea></td>";
 		echo "\t\t<td style='width:{$widths[2]}'><button>Update</button><button>Cancel</button></td>";
@@ -43,6 +43,9 @@
 <div id='calendar'></div>
 
 <script type='text/javascript'>
+	function toggle(){
+
+	}
 	function toggle(){
 
 	}
