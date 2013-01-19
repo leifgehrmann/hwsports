9e708d09d72120f50159707b03177e9d8d484b49
lg90 <lg90@hw.ac.uk>
2013-01-19 21:26:19 +0000

    Changed some stuff in venue.php

--- application/views/tms/venue.php
+++ application/views/tms/venue.php
@@ -13,7 +13,7 @@
 		if($types[$i]=="text")
 			echo "\t\t<td style='width:{$widths[1]}'><input id='form-{$fields[$i]}' type='text' oldvalue='{$this->data['venue'][$fields[$i]]}' onchange='' value='{$this->data['venue'][$fields[$i]]}'></td>";
 		else if($types[$i]=="textfield")
-			echo "\t\t<td style='width:{$widths[1]}'><textarea onchange='' oldvalue=''>{$this->data['venue'][$fields[$i]]}</textarea></td>";
+			echo "\t\t<td style='width:{$widths[1]}'><textarea id='form-{$fields[$i]}' onchange='' oldvalue='{$this->data['venue'][$fields[$i]]}'>{$this->data['venue'][$fields[$i]]}</textarea></td>";
 		echo "\t\t<td style='width:{$widths[2]}'><button onclick='update(\"{$fields[$i]}\")'>Update</button><button onclick='cancel(\"{$fields[$i]}\")'>Cancel</button></td>";
 		echo "\t</tr>";
 	}
