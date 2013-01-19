c8f3bbe2b5122f5ff3c929399457b22f028b09ab
lg90 <lg90@hw.ac.uk>
2013-01-19 19:52:59 +0000

    Changed some stuff in venue.php

--- application/views/tms/venue.php
+++ application/views/tms/venue.php
@@ -1,6 +1,22 @@
 <h1><a href="/tms/venues/">Venue</a> &#9656; <?=$this->data["venue"]["name"]?></h1>
 
-<table>
+<?php
+	$fields = array("name","description","directions");
+	$labels = array("Venue Name","Description","Directions");
+	$widths = array("20%","60%","20%");
+
+	echo "<table>";
+	for($i=0;$i<count($fields),$i){
+		echo "\t<tr>";
+		echo "\t\t<th style='width:{$widths[0]}'>{$labels[$i]}</th>";
+		echo "\t\t<td style='width:{$widths[1]}'><?=$this->data['venue']['{$fields[$i]}']?></td>";
+		echo "\t\t<td style='width:{$widths[2]}'><button>Edit</button></td>";
+		echo "\t</tr>";
+	}
+	echo "</table>";
+?>
+
+<!--<table>
 	<tr>
 		<th style="width:20%">Venue Name</th>
 		<td style="width:60%"><?=$this->data["venue"]["name"]?></td>
@@ -11,18 +27,21 @@
 		<td style="width:60%"><?=$this->data["venue"]["description"]?></td>
 		<td style="width:20%"><button>Edit</button></td>
 	</tr>
-	<tr>
+	<tr class="">
 		<th style="width:20%">Directions</th>
 		<td style="width:60%"><?=$this->data["venue"]["directions"]?></td>
-		<td style="width:20%"><button>Edit</button></td>
+		<td style="width:20%"><<button>Edit</button></td>
 	</tr>
-</table>
+</table>-->
 
 <h2>Occupied Calendar</h2>
 
 <div id='calendar'></div>
 
 <script type='text/javascript'>
+	function {
+
+	}
 	$(document).ready(function() {
 		$('#calendar').fullCalendar({
 			firstDay: '1',
