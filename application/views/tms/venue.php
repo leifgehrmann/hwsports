a5f9024c7e8629018a9cf14aab29802beaa0d0e5
lg90 <lg90@hw.ac.uk>
2013-01-20 11:02:17 +0000

    changing some javascript in venue.php view to be more user intuitive

--- application/views/tms/venue.php
+++ application/views/tms/venue.php
@@ -11,9 +11,9 @@
 		echo "\t<tr>";
 		echo "\t\t<th style='width:{$widths[0]}'>{$labels[$i]}</th>";
 		if($types[$i]=="text")
-			echo "\t\t<td style='width:{$widths[1]}'><input id='form-{$fields[$i]}' type='text' oldvalue='{$this->data['venue'][$fields[$i]]}' onchange='changed(\"{$fields[$i]}\")' value='{$this->data['venue'][$fields[$i]]}'></td>";
+			echo "\t\t<td style='width:{$widths[1]}'><input id='form-{$fields[$i]}' type='text' oldvalue='{$this->data['venue'][$fields[$i]]}' onkeyup='changed(\"{$fields[$i]}\")' value='{$this->data['venue'][$fields[$i]]}'></td>";
 		else if($types[$i]=="textfield")
-			echo "\t\t<td style='width:{$widths[1]}'><textarea id='form-{$fields[$i]}' onchange='changed(\"{$fields[$i]}\")' oldvalue='{$this->data['venue'][$fields[$i]]}'>{$this->data['venue'][$fields[$i]]}</textarea></td>";
+			echo "\t\t<td style='width:{$widths[1]}'><textarea id='form-{$fields[$i]}' onkeyup='changed(\"{$fields[$i]}\")' oldvalue='{$this->data['venue'][$fields[$i]]}'>{$this->data['venue'][$fields[$i]]}</textarea></td>";
 		echo "\t\t<td id='edit-{$fields[$i]}' style='visibility:hidden;width:{$widths[2]}'><button onclick='update(\"{$fields[$i]}\")'>Update</button><button onclick='cancel(\"{$fields[$i]}\")'>Cancel</button></td>";
 		echo "\t</tr>";
 	}
@@ -26,7 +26,9 @@
 
 <script type='text/javascript'>
 	function changed(fieldname){
-		$("#edit-"+fieldname).css("visibility", "visible");
+		input = $("#form-"+fieldname);
+		if(input.val()!=input.attr('oldvalue'))
+			$("#edit-"+fieldname).css("visibility", "visible");
 	}
 	function cancel(fieldname){
 		input = $("#form-"+fieldname);
