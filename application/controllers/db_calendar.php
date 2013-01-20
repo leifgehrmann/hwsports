855222aed113a44f04941e639c1cef435fa95d95
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-20 17:23:55 +0000

    calendar devel

--- application/controllers/db_calendar.php
+++ application/controllers/db_calendar.php
@@ -13,28 +13,17 @@ class Db_Calendar extends MY_Controller {
 	// Input:
 	//	* 
 	public function getAllMatches() {
-		$output = $this->matches_model->get_matches($this->data['centre']['id']);
-			
-		$this->data['data'] = json_encode($output);
-	
-	
-		// Should return registration periods
-
-		$this->data['test'] = json_encode(array(
-			array(
-				'title' => 'Men\'s Hurdling - Preliminary Rounds',
-				'start' => mktime(12, 0, 0, 1, 10-3, 2013),
-				'end' => mktime(14, 0, 0, 1, 10-3, 2013),
+		$matches = $this->matches_model->get_matches($this->data['centre']['id']);
+		$this->data['data'] = array();		
+		foreach($matches as $match) {
+			$this->data['data'][] = array(
+				'title' => $match['name'],
+				'start' => $match['timestamp'],
+				'end' => $match['timestamp']+7200,
 				'allDay' => false,
 				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'Heriot Hurdling Registration Period',
-				'start' => mktime(0, 0, 0, 1, 10-30, 2013),
-				'end' => mktime(0, 0, 0, 1, 10-7, 2013),
-				'color' => '#EA472C'
 			)
-		));
+		}
 		$this->load->view('data',$this->data);
 	}
 	public function getVenueMatches($venueID){
