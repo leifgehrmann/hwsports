4d30e8148a44e626fabb932e46df2cebf266cc43
lg90 <lg90@hw.ac.uk>
2013-01-20 12:03:41 +0000

    fixed some stupid errors in php

--- application/views/tms/venue.php
+++ application/views/tms/venue.php
@@ -11,17 +11,25 @@
 	for($i=0;$i<count($fields);$i++){
 		echo "\t<tr>";
 		echo "\t\t<th style='width:{$widths[0]}'>{$labels[$i]}</th>";
+		$value = htmlentities($this->data['venue'][$fields[$i]]);
 		if($types[$i]=="text")
 			echo "\t\t<td style='width:{$widths[1]}'>
-						<input 
-							id='form-{$fields[$i]}'
-							type='text'
-							onkeyup='changed(\"{$fields[$i]}\")'
-							oldvalue='{$this->data['venue'][$fields[$i]]}'
-							value='{$this->data['venue'][$fields[$i]]}'>
+							<input 
+								id='form-{$fields[$i]}'
+								type='text'
+								onkeyup='changed(\"{$fields[$i]}\")'
+								oldvalue='{$value}'
+								value='{$value}'>
 						</td>";
 		else if($types[$i]=="textfield")
-			echo "\t\t<td style='width:{$widths[1]}'><textarea id='form-{$fields[$i]}' onkeyup='changed(\"{$fields[$i]}\")' oldvalue='{$this->data['venue'][$fields[$i]]}'>{$this->data['venue'][$fields[$i]]}</textarea></td>";
+			echo "\t\t<td style='width:{$widths[1]}'>
+							<textarea 
+								id='form-{$fields[$i]}'
+								onkeyup='changed(\"{$fields[$i]}\")'
+								oldvalue='{$value}'>
+								{$value}
+							</textarea>
+						</td>";
 		echo "\t\t<td id='edit-{$fields[$i]}' style='visibility:hidden;width:{$widths[2]}'><button onclick='update(\"{$fields[$i]}\")'>Update</button><button onclick='cancel(\"{$fields[$i]}\")'>Cancel</button></td>";
 		echo "\t</tr>";
 	}
