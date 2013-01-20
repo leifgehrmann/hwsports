aea281dd050780618e5db9efa2c2bd8560e40b35
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-20 15:52:11 +0000

    calendar devel

--- application/controllers/db_calendar.php
+++ application/controllers/db_calendar.php
@@ -5,191 +5,29 @@ class Db_Calendar extends MY_Controller {
 	function __construct()
 	{
 		parent::__construct();
-
+		
+		$this->load->library('table');
+		$this->load->model('matches_model');
 	}
 
 	// Input:
 	//	* 
 	public function getAllMatches() {
-
-
-
+		$output = $this->matches_model->get_matches($this->data['centre']['id']);
+			
+		$this->data['data'] = $output;
+	
+	
 		// Should return registration periods
 
-		$this->data['data'] = json_encode(array(
+		$this->data['data'] .= json_encode(array(
 			array(
 				'title' => 'Men\'s Hurdling - Preliminary Rounds',
 				'start' => mktime(12, 0, 0, 1, 10-3, 2013),
 				'end' => mktime(14, 0, 0, 1, 10-3, 2013),
 				'allDay' => false,
 				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - Recoba vs. WattBulls',
-				'start' => mktime(10, 0, 0, 1, 10-3, 2013),
-				'end' => mktime(11, 30, 0, 1, 10-3, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - The Red Cows vs. Hunters',
-				'start' => mktime(10, 0, 0, 1, 10-3, 2013),
-				'end' => mktime(11, 30, 0, 1, 10-3, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - Blue Jays vs. Greens',
-				'start' => mktime(14, 0, 0, 1, 10-3, 2013),
-				'end' => mktime(15, 30, 0, 1, 10-3, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-					
-			// Tuesday
-			array(
-				'title' => 'Men\'s Hurdling - Round 1',
-				'start' => mktime(12, 0, 0, 1, 10-2, 2013),
-				'end' => mktime(14, 0, 0, 1, 10-2, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - The Red Cows vs. Greens',
-				'start' => mktime(10, 0, 0, 1, 10-2, 2013),
-				'end' => mktime(11, 30, 0, 1, 10-2, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - Blue Jays vs. Recoba',
-				'start' => mktime(10, 0, 0, 1, 10-2, 2013),
-				'end' => mktime(11, 30, 0, 1, 10-2, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - Hunters vs. WattBulls',
-				'start' => mktime(14, 0, 0, 1, 10-2, 2013),
-				'end' => mktime(15, 30, 0, 1, 10-2, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-
-			// Wednesday
-			array(
-				'title' => 'Men\'s Hurdling - Round 2',
-				'start' => mktime(12, 0, 0, 1, 10-1, 2013),
-				'end' => mktime(14, 0, 0, 1, 10-1, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - The Red Cows vs. WattBulls',
-				'start' => mktime(10, 0, 0, 1, 10-1, 2013),
-				'end' => mktime(11, 30, 0, 1, 10-1, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - Recoba vs. Greens',
-				'start' => mktime(14, 0, 0, 1, 10-1, 2013),
-				'end' => mktime(15, 30, 0, 1, 10-1, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'Wattball - Hunters vs. Blue Jays',
-				'start' => mktime(10, 0, 0, 1, 10-1, 2013),
-				'end' => mktime(11, 30, 0, 1, 10-1, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-
-			// Thursday
-			array(
-				'title' => 'Men\'s Hurdling - Round 3',
-				'start' => mktime(12, 0, 0, 1, 10, 2013),
-				'end' => mktime(14, 0, 0, 1, 10, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - The Red Cows vs. Blue Jays',
-				'start' => mktime(10, 0, 0, 1, 10, 2013),
-				'end' => mktime(11, 30, 0, 1, 10, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - Recoba vs. Hunters',
-				'start' => mktime(14, 0, 0, 1, 10, 2013),
-				'end' => mktime(15, 30, 0, 1, 10, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'Wattball - Greens vs. The Red Cows',
-				'start' => mktime(10, 0, 0, 1, 10, 2013),
-				'end' => mktime(11, 30, 0, 1, 10, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-
-			// Friday
-			array(
-				'title' => 'Men\'s Hurdling - Round 4',
-				'start' => mktime(12, 0, 0, 1, 10+1, 2013),
-				'end' => mktime(14, 0, 0, 1, 10+1, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - The Red Cows vs. Recoba',
-				'start' => mktime(10, 0, 0, 1, 10+1, 2013),
-				'end' => mktime(11, 30, 0, 1, 10+1, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'WattBall - Blue Jays vs. WattBulls',
-				'start' => mktime(14, 0, 0, 1, 10+1, 2013),
-				'end' => mktime(15, 30, 0, 1, 10+1, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-			array(
-				'title' => 'Wattball - Greens vs. Hunters',
-				'start' => mktime(10, 0, 0, 1, 10+1, 2013),
-				'end' => mktime(11, 30, 0, 1, 10+1, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-
-			// Saturday
-			array(
-				'title' => 'Men\'s Hurdling - Round 5',
-				'start' => mktime(12, 0, 0, 1, 10+2, 2013),
-				'end' => mktime(14, 0, 0, 1, 10+2, 2013),
-				'allDay' => false,
-				'color' => '#2966C7'
-			),
-
-			// Tournaments
-			array(
-				'title' => 'Heriot Watt Tournament 2013',
-				'start' => mktime(0, 0, 0, 1, 10-3, 2013),
-				'end' => mktime(0, 0, 0, 1, 10+2, 2013),
-				'color' => '#5AB128'
-			),
-
-			// Registration times
-			array(
-				'title' => 'WattBall Registration Period',
-				'start' => mktime(0, 0, 0, 1, 10-30, 2013),
-				'end' => mktime(0, 0, 0, 1, 10-5, 2013),
-				'color' => '#EA472C'
-			),
+			)
 			array(
 				'title' => 'Heriot Hurdling Registration Period',
 				'start' => mktime(0, 0, 0, 1, 10-30, 2013),
---
+++ application/models/matches_model.php
@@ -0,0 +1,119 @@
+<?php
+class Matches_model extends CI_Model {
+
+	/**
+	 * $matchID is int(11)
+	 *  
+	 * @return boolean
+	 **/
+
+	public function match_exists($matchID)
+	{
+		$output = array();
+		$queryString = 	"SELECT ".$this->db->escape($matchID)." AS matchID, ".
+						"EXISTS(SELECT 1 FROM matches WHERE matchID = ".$this->db->escape($matchID).") AS `exists`";
+		$queryData = $this->db->query($queryString);
+		$output = $queryData->row_array();
+		return $output;
+	}
+
+	/**
+	 * Returns a 2d array of match data
+	 *  
+	 * @return array
+	 **/
+	public function get_matches($centreID, $fields=array("name","timestamp","description","tournamentID"))
+	{
+		$output = array();
+		$queryString = "SELECT matchID FROM matches WHERE centreID = ".$this->db->escape($centreID);
+		$queryData = $this->db->query($queryString);
+		$data = $queryData->result_array();
+		foreach($data as $match) {
+			$output[] = $this->get_match($match['matchID'],$fields);
+		}
+		return $output;
+	}
+
+	/**
+	 * Returns an array of data from a specific match
+	 *  
+	 * @return array
+	 **/
+	public function get_match($matchID, $fields=array("name","timestamp","description","tournamentID"))
+	{
+		$dataQueryString = "SELECT ";
+		$i = 0;
+		$len = count($fields);
+		foreach($fields as $field) {
+			$dataQueryString .= "MAX(CASE WHEN `key`=".$this->db->escape($field)." THEN value END ) AS ".$this->db->escape($field);
+			if($i<$len-1)
+				$dataQueryString .= ", ";
+			else
+				$dataQueryString .= " ";
+			$i++;
+		}
+		$dataQueryString .= "FROM matchData WHERE matchID = ".$this->db->escape($matchID);
+		$dataQuery = $this->db->query($dataQueryString);
+		$output = array_merge(array("matchID"=>$matchID), $dataQuery->row_array());
+		return $output;
+	}
+
+	/**
+	 * Creates a match with data.
+	 * returns the matchID of the new match if it was
+	 * successful. If not, it should return -1.
+	 *  
+	 * @return int
+	 **/
+	public function insert_match($centreID, $data)
+	{	
+		$this->db->trans_start();
+
+		$this->db->query("INSERT INTO matches (centreID) VALUES (".$this->db->escape($centreID).")");
+		$matchID = $this->db->insert_id();
+
+		$insertDataArray = array();
+		foreach($data as $key=>$value) {
+			$dataArray = array(
+					'matchID' => $this->db->escape($matchID),
+					'key' => $this->db->escape($key),
+					'value' => $this->db->escape($value)
+				);
+			$insertDataArray[] = $dataArray;
+		}
+		if ($this->db->insert_batch('matchData',$insertDataArray)) {
+			// db success
+			$this->db->trans_complete();
+			return $matchID;
+		} else {
+			// db fail
+			return -1;
+		}
+	}
+
+	/**
+	 * Updates a match with data.
+	 *
+	 * @return boolean
+	 **/
+	public function update_match($matchID, $data){
+
+		$this->db->trans_start();
+
+		if($this->match_exists($matchID)){
+			foreach($data as $key=>$value) {
+				$escKey = $this->db->escape($key);
+				$escValue = $this->db->escape($value);
+				$dataQueryString = 	"UPDATE `matchData` ".
+									"SET `value`=$escValue ".
+									"WHERE `key`=$escKey ".
+									"AND `matchID`=$matchID";
+				$this->db->query($dataQueryString);
+			}
+			$this->db->trans_complete();
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
