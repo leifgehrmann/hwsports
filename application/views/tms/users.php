3f133a7052a1f3d605fbf7f29e017285fdb775ab
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-23 08:21:58 +0000

    data dump in users and groups

--- application/views/tms/users.php
+++ application/views/tms/users.php
@@ -1,5 +1,7 @@
 This page is still under development.<br />
 <br />
+<? print_r($users); /*
+
 <? foreach($users as $user) { ?>
 <div>
 	User ID: <? print_r($user) ?> (<a href="auth/delete_user/">Delete?</a>)<br>
@@ -9,4 +11,4 @@ This page is still under development.<br />
 	Phone: <?=$user['phone']?><br>
 	Email: <?=$user['email']?><br>
 </div>
-<? } ?>
+<? } */?>
