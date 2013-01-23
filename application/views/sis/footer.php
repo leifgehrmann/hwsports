e7c7aa76009d3bda96f26f366642c27081573d47
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-01-23 01:35:22 +0000

    added settings page

--- application/views/sis/footer.php
+++ application/views/sis/footer.php
@@ -1,6 +1,6 @@
 			</div>
 			<div id="footer">
-				<div class="legal"><?=print_r($centre,1)?></div>
+				<div class="footerText"><? print_r($centre) ?></div>
 				<div class="powered">Powered By:<a href="http://www.infusionsports.co.uk"><div class="logo"></div></a></div>
 			</div>
 		</div>
