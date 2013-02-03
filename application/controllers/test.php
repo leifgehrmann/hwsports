7a821edb8798fd8b129f25a57c20d0fdc3842ed8
lg90 <lg90@hw.ac.uk>
2013-02-03 16:18:30 +0000

    modified the matches_model

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -6,6 +6,7 @@ class Test extends MY_Controller {
 	    parent::__construct();
 	    $this->load->model('sports_model');
 	    $this->load->model('tournaments_model');
+	    $this->load->model('matches_model');
 	}
 	public function get_sport_category($sportID){
 		$output = $this->sports_model->get_sport_categories();
@@ -23,6 +24,13 @@ class Test extends MY_Controller {
 
 	public function get_tournament($tID){
 		$output = $this->tournaments_model->get_tournament($tID);
+		$this->data['data'] =  print_r($output,1);
+		header('Content-Type: application/json');
+		$this->load->view('data', $this->data);
+	}
+
+	public function get_matches(){
+		$output = $this->matches_model->get_matches($centreID);
 		$this->data['data'] =  print_r($output,1);
 		header('Content-Type: application/json');
 		$this->load->view('data', $this->data);
