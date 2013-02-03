09c74babae99d6cd93d41def24f3f3e4aa3b796d
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-02-03 14:02:58 +0000

    fix tournament

--- application/controllers/tms.php
+++ application/controllers/tms.php
@@ -149,7 +149,7 @@ class Tms extends MY_Controller {
 				//set the flash data error message if there is one
 				$this->data['message'] = (validation_errors() ? validation_errors() : $this->session->flashdata('message') );
 				
-				$this->data['name'] = $tournament['name'];
+				$this->data['tournament']['name'] = $tournament['name'];
 			
 				$this->data['name'] = array(
 					'name'  => 'name',
