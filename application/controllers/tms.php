db22973e636981ca1268179365deb57ff61fd204
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-02-03 16:49:23 +0000

    add sportID to tournaments

--- application/controllers/tms.php
+++ application/controllers/tms.php
@@ -162,7 +162,7 @@ class Tms extends MY_Controller {
 				
 				$this->data['tournament']['name'] = $tournament['name'];
 				$sport = $this->sports_model->get_sport( $tournament['sportID'] );
-				$this->data['tournament']['sport'] = $sport['name'];
+				$this->data['sport'] = $sport['name'];
 			
 				$this->data['name'] = array(
 					'name'  => 'name',
