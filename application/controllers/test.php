e595244aa9926fa6ca54ac3a9c4d0070871df626
lg90 <lg90@hw.ac.uk>
2013-02-03 15:47:54 +0000

    added another test

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -8,7 +8,6 @@ class Test extends MY_Controller {
 	}
 	public function get_sport_category($sportID){
 		$output = $this->sports_model->get_sport_categories();
-		$this->data['data'] =  json_encode($output);
 		$this->data['data'] =  print_r($output,1);
 		header('Content-Type: application/json');
 		$this->load->view('data', $this->data);
@@ -16,7 +15,13 @@ class Test extends MY_Controller {
 
 	public function get_sport($sportID){
 		$output = $this->sports_model->get_sport($sportID);
-		$this->data['data'] =  json_encode($output);
+		$this->data['data'] =  print_r($output,1);
+		header('Content-Type: application/json');
+		$this->load->view('data', $this->data);
+	}
+
+	public function get_tournament($tID){
+		$output = $this->tournaments_model->get_tournament($tID);
 		$this->data['data'] =  print_r($output,1);
 		header('Content-Type: application/json');
 		$this->load->view('data', $this->data);
