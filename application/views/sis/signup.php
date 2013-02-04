b59c71aaf02d9d64f3d6c4a6c5dff94c5b0480c7
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-02-04 03:55:17 +0000

    sis signup

--- application/views/sis/signup.php
+++ application/views/sis/signup.php
@@ -4,21 +4,30 @@
 <? } ?>
 
 	<h2>Sign Up for <?=$tournament['name']?>:</h2>
-	<div id="stage1">
-		Select role: 
-		<select id="role" name="role">
-			<option></option>
-			<option value='1'>Single Player</option>
-			<option value='2'>Team Leader</option>
-			<option value='3'>Umpire</option>
-		</select>
-	</div>	
+	<div id="accordion">
+		<h3>Select role:</h3>
+		<div id="stage1">
+			<select id="role" name="role">
+				<option></option>
+				<option value='1'>Single Player</option>
+				<option value='2'>Team Leader</option>
+				<option value='3'>Umpire</option>
+			</select>
+		</div>
+		<h3>Enter personal details:</h3>
+		<div id="stage2">
+			<textarea id="address" name="address"></textarea>
+		</div>	
+	</div>
+	
 <!-- /#main -->
 
 <script type="text/javascript">
 	$(document).ready(function() {
+		$( "#accordion" ).accordion();
+		
 		$("#role").change(function() {
-			alert("You are a "+$(this).val()+"! Well done!");
+			$('#accordion').accordion('activate', $(this).val() );
 		});
 	});
 </script>
