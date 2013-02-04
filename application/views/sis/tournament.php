8191ac7c0f744f8bc1434ee81459a06d9fa3d865
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-02-04 02:21:27 +0000

    fixed sis tournament registration calculation

--- application/views/sis/tournament.php
+++ application/views/sis/tournament.php
@@ -11,7 +11,7 @@
 
 	if ( $registrationOpen ) { ?>
 		<div class="tournament-signup-button">
-			<a href='/sis/signup/$tournamentID' class='tournamentSignupButton'>Sign Up Now!</a>
+			<a href='/sis/signup/<?=$tournamentID?>' class='tournamentSignupButton'>Sign Up Now!</a>
 		</div>
 	<? } ?>
 <h2>Calendar</h2>
@@ -19,3 +19,55 @@
 
 <div id='calendar'></div>
 
+<script type='text/javascript' src='/scripts/fullcalendar/_loader.js'></script>
+<script type='text/javascript'>
+	$(document).ready(function() {
+		$('#calendar').fullCalendar({
+			firstDay: '1',
+			contentHeight: 590,
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			events: '/db_calendar/getAllMatches/',
+			editable: true,
+			eventResize: function(match,dayDelta,minuteDelta,revertFunc) {
+				//console.log(match);
+				var minutesDelta = ((dayDelta*1440)+minuteDelta)*60;
+				var request = $.ajax({
+					type: "POST",
+					url: '/db_calendar/changeMatchEnd',
+					data: { 'minutesDelta': minutesDelta, 'id': match.data.id }
+				});
+ 
+				request.done(function(msg) {
+				 //alert( msg );
+				});
+				 
+				request.fail(function(jqXHR, textStatus) {
+				  alert( "Request failed: " + textStatus );
+				});
+			},
+			eventDrop: function(match,dayDelta,minuteDelta,allDay,revertFunc) {
+				//console.log(match);
+				var minutesDelta = ((dayDelta*1440)+minuteDelta)*60;
+				//alert(minutesDelta);
+				var request = $.ajax({
+					type: "POST",
+					url: '/db_calendar/changeMatchStart',
+					data: { 'minutesDelta': minutesDelta, 'id': match.data.id }
+				});
+ 
+				request.done(function(msg) {
+				 //alert( msg );
+				});
+				 
+				request.fail(function(jqXHR, textStatus) {
+				  alert( "Request failed: " + textStatus );
+				});
+			}
+		});
+		
+	});
+</script>
