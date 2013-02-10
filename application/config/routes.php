a309d9e71c155767aa4c169e0d298d1a571388aa
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-02-10 03:38:32 +0000

    signup page dev

--- application/config/routes.php
+++ application/config/routes.php
@@ -39,7 +39,7 @@
 */
 
 $route['js/(:any)'] = "js/index/$1";
-$route['css/(:any)'] = "css/index/$1";
+$route['css/(:any)'] = "css/index";
 $route['default_controller'] = ($_SERVER['HTTP_HOST']=="infusionsports.co.uk" ? "product" : "sis");
 $route['404_override'] = '';
 
--- application/controllers/css.php
+++ application/controllers/css.php
@@ -2,10 +2,19 @@
 
 class Css extends MY_Controller {
 
-	public function index($file)
+	public function index()
 	{
-		print_r( $this->uri->rsegment_array() );die;
-		$this->output->set_header("Content-Type: text/css");
-		$this->load->view("css/{$this->data['slug']}/$file",$this->data);
+		// get path segments as interpreted by CI routing
+		$segments = $this->uri->rsegment_array();
+		// get rid of "css" from path
+		unset($segments[0]);
+		$path = implode("/",$segments);
+		
+		if( strpos($path,".css" !== false) ) {
+			$this->output->set_header("Content-Type: text/css");
+			$this->load->view("css/{$this->data['slug']}/$path",$this->data);
+		} else {
+			readfile("{$this->data['slug']}/$path");
+		}
 	}
 }
