812683f6e522456a798d722a5fb537c7435ead2b
lg90 <lg90@hw.ac.uk>
2013-02-10 23:11:15 +0000

    modified the styles a bit more. I'm going to have to do some more css fixes because of jQuery UI stuff, but I'll get there eventually.

---
+++ application/logs/log-2013-02-10.php
@@ -0,0 +1,64 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2013-02-10 22:45:43 --> Config Class Initialized
+DEBUG - 2013-02-10 22:45:43 --> Hooks Class Initialized
+DEBUG - 2013-02-10 22:45:44 --> Utf8 Class Initialized
+DEBUG - 2013-02-10 22:45:44 --> UTF-8 Support Enabled
+DEBUG - 2013-02-10 22:45:44 --> URI Class Initialized
+DEBUG - 2013-02-10 22:45:44 --> Router Class Initialized
+DEBUG - 2013-02-10 22:45:44 --> No URI present. Default controller set.
+DEBUG - 2013-02-10 22:45:44 --> Output Class Initialized
+DEBUG - 2013-02-10 22:45:44 --> Security Class Initialized
+DEBUG - 2013-02-10 22:45:44 --> Input Class Initialized
+DEBUG - 2013-02-10 22:45:44 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-02-10 22:45:44 --> Language Class Initialized
+DEBUG - 2013-02-10 22:45:44 --> Loader Class Initialized
+DEBUG - 2013-02-10 22:45:44 --> Helper loaded: url_helper
+DEBUG - 2013-02-10 22:45:44 --> Helper loaded: form_helper
+DEBUG - 2013-02-10 22:45:44 --> Database Driver Class Initialized
+ERROR - 2013-02-10 22:45:44 --> Unable to connect to the database
+DEBUG - 2013-02-10 22:45:44 --> Language file loaded: language/english/db_lang.php
+ERROR - 2013-02-10 22:45:44 --> ErrorException [ 1 ]: Unable to connect to your database server using the provided settings. / Filename: core/Loader.php / Line Number: 346 ~  [ 0 ]
+DEBUG - 2013-02-10 22:46:10 --> Config Class Initialized
+DEBUG - 2013-02-10 22:46:10 --> Hooks Class Initialized
+DEBUG - 2013-02-10 22:46:10 --> Utf8 Class Initialized
+DEBUG - 2013-02-10 22:46:10 --> UTF-8 Support Enabled
+DEBUG - 2013-02-10 22:46:10 --> URI Class Initialized
+DEBUG - 2013-02-10 22:46:10 --> Router Class Initialized
+ERROR - 2013-02-10 22:46:10 --> 404 Page Not Found --> mock
+ERROR - 2013-02-10 22:46:10 --> ErrorException [ 1 ]: The page you requested was not found. ~  [ 0 ]
+DEBUG - 2013-02-10 22:47:59 --> Config Class Initialized
+DEBUG - 2013-02-10 22:47:59 --> Hooks Class Initialized
+DEBUG - 2013-02-10 22:47:59 --> Utf8 Class Initialized
+DEBUG - 2013-02-10 22:47:59 --> UTF-8 Support Enabled
+DEBUG - 2013-02-10 22:47:59 --> URI Class Initialized
+DEBUG - 2013-02-10 22:47:59 --> Router Class Initialized
+ERROR - 2013-02-10 22:47:59 --> 404 Page Not Found --> pmwiki.php
+ERROR - 2013-02-10 22:47:59 --> ErrorException [ 1 ]: The page you requested was not found. ~  [ 0 ]
+DEBUG - 2013-02-10 23:00:22 --> Config Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> Hooks Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> Utf8 Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> UTF-8 Support Enabled
+DEBUG - 2013-02-10 23:00:23 --> URI Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> Router Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> No URI present. Default controller set.
+DEBUG - 2013-02-10 23:00:23 --> Output Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> Security Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> Input Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-02-10 23:00:23 --> Language Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> Loader Class Initialized
+DEBUG - 2013-02-10 23:00:23 --> Helper loaded: url_helper
+DEBUG - 2013-02-10 23:00:23 --> Helper loaded: form_helper
+DEBUG - 2013-02-10 23:00:23 --> Database Driver Class Initialized
+ERROR - 2013-02-10 23:00:23 --> Unable to connect to the database
+DEBUG - 2013-02-10 23:00:23 --> Language file loaded: language/english/db_lang.php
+ERROR - 2013-02-10 23:00:23 --> ErrorException [ 1 ]: Unable to connect to your database server using the provided settings. / Filename: core/Loader.php / Line Number: 346 ~  [ 0 ]
+DEBUG - 2013-02-10 23:04:36 --> Config Class Initialized
+DEBUG - 2013-02-10 23:04:36 --> Hooks Class Initialized
+DEBUG - 2013-02-10 23:04:36 --> Utf8 Class Initialized
+DEBUG - 2013-02-10 23:04:36 --> UTF-8 Support Enabled
+DEBUG - 2013-02-10 23:04:36 --> URI Class Initialized
+DEBUG - 2013-02-10 23:04:36 --> Router Class Initialized
+ERROR - 2013-02-10 23:04:36 --> 404 Page Not Found --> mock
+ERROR - 2013-02-10 23:04:36 --> ErrorException [ 1 ]: The page you requested was not found. ~  [ 0 ]
