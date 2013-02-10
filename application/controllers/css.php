5cbf0703d6b64b36463a28760825f3d4cf0ad3f5
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-02-10 03:23:19 +0000

    signup page dev

--- application/controllers/css.php
+++ application/controllers/css.php
@@ -4,7 +4,7 @@ class Css extends MY_Controller {
 
 	public function index($file)
 	{
-		print_r($file);die;
+		print_r( $this->uri->rsegment_array() );die;
 		$this->output->set_header("Content-Type: text/css");
 		$this->load->view("css/{$this->data['slug']}/$file",$this->data);
 	}
