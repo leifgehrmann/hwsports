b651098467b46bdb3b3925b37f2c8a6b4d9f096b
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-02-10 06:20:47 +0000

    updated assets controller to support caching

--- application/controllers/assets.php
+++ application/controllers/assets.php
@@ -54,10 +54,10 @@ class Assets extends MY_Controller {
 			//$this->output->cache(60*24); // cache js for 24 hours
 		} elseif( $file_ext == "png" || $file_ext == "jpg" || $file_ext == "jpeg" || $file_ext == "gif" ) {
 			// This is a binary image so read the file directly from the img folder after sending the header - don't load it as a view
-			readBinaryFile("/home/sports/public_html/application/views/img/{$this->data['slug']}/{$path}");
+			$this->readBinaryFile("/home/sports/public_html/application/views/img/{$this->data['slug']}/{$path}");
 		} else {
 			// This isn't js, css or an image based on it's extension so try and load it from a random folder based on it's extension?
-			readBinaryFile("/home/sports/public_html/application/views/{$file_ext}/{$this->data['slug']}/{$path}");
+			$this->readBinaryFile("/home/sports/public_html/application/views/{$file_ext}/{$this->data['slug']}/{$path}");
 		}
 	}
 }
