78553435a3cbb861fc5e6533f589d91cc7800286
lg90 <lg90@hw.ac.uk>
2013-02-14 22:25:31 +0000

    changing the css of sis

--- application/views/sis/footer.php
+++ application/views/sis/footer.php
@@ -4,14 +4,14 @@
 			<div id="footer">
 				<div id="footer-block">
 					<div id="footer-legal">
-						<h3>Contact Us</h3>
+						<h1>Contact Us</h1>
 						<p>&copy; Heriot-Watt University</p>
 						<p>Edinburgh, Scotland, UK EH14 4AS</p>
 						<p>Tel: +44 (0) 131 449 5111</p>
 						<p>Scottish registered charity number: SC000278</p>
 					</div>
 					<div id="footer-links">
-						<h3>Links</h3>
+						<h1>Links</h1>
 						<p><a href="#">Heriot Watt University Homepage</a></p>
 						<p><a href="#">Some other footer nonsense</a></p>
 						<p><a href="#">Hopefully we will fix this</a></p>
@@ -19,7 +19,7 @@
 						<p><a href="#">About InfusionSports</a></p>
 					</div>
 					<div id="footer-powered">
-						<h3>Powered By</h3>
+						<h1>Powered By</h1>
 						<p><a href="http://www.infusionsports.co.uk"><div class="product-logo"></div></a></p>
 						<p><a href="http://www.infusionsystems.co.uk"><div class="company-logo"></div></a></p>
 					</div>
