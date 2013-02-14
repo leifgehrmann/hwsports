a7249ec34b87c152a0868eaf97a853fb4453a604
lg90 <lg90@hw.ac.uk>
2013-02-14 09:10:41 +0000

    modifying tournaments page

--- application/views/sis/tournaments.php
+++ application/views/sis/tournaments.php
@@ -1,18 +1,59 @@
+<? 
+
+// We wish to have all our tournaments sorted by 
+// year and placed into groups of the year. we
+// do this by putting all the tournaments into
+// particular year arrays.
+$yearTournaments = array()
+foreach($tournaments as $tournament) { 
+	$year = DateTime::createFromFormat('Y', $tournament['tournamentStart']);
+	$yearTournaments[$year][] = $tournament;
+}
+foreach($yearTournaments as $year){
+	usort($year, function($a1, $a2) {return strtotime($a["tournamentStart"]) - strtotime($b["tournamentStart"]);});
+}
+
+?>
+
 <h1>Tournaments</h1>
 
 <div class="tournaments-list">
-	<h3>2013</h3>
-	<? foreach($tournaments as $tournament) { 
+	<? foreach($yearTournaments as $year=>$tournaments) { ?>
+		<h2><?=$year?></h2>
+		<div>
+		<? foreach($tournaments as $tournament) { 
+			$registrationStartDate = DateTime::createFromFormat('d/m/Y', $tournament['registrationStart']);
+			$registrationEndDate = DateTime::createFromFormat('d/m/Y', $tournament['registrationEnd']);
+			$today = new DateTime();
+		?>
+			<div class="widget half">
+				<a href="/sis/tournament/<?=$tournament['tournamentID']?>">
+					<div class="widget-title">
+						<div class="widget-title-left icon sport-<?=$sportID?>"></div>
+						<div class="widget-title-centre"><?=$tournament['name']?></div>
+						<div class="widget-title-right icon chevron"></div>
+					</div>
+				</a>
+				<div class="widget-body">
+					<p><b>Duration:</b> <?=$tournament['tournamentStart']?> - <?=$tournament['tournamentEnd']?></p>
+					<p>Running with a ball, sometimes kicking it. This would be the description of the tournament</p>
+					<?=( ($registrationStartDate < $today) && ($today < $registrationEndDate) ? "<a href='/sis/signup/{$tournament['tournamentID']}' class='button green'>Sign up!</a>" : "" )?>
+				</div>
+			</div>
+		<? } ?>
+		</div>
+	<? } ?>
+	<!--<? foreach($tournaments as $tournament) { 
 		$registrationStartDate = DateTime::createFromFormat('d/m/Y', $tournament['registrationStart']);
 		$registrationEndDate = DateTime::createFromFormat('d/m/Y', $tournament['registrationEnd']);
 		$today = new DateTime();
-	?>
-	<a href="/sis/tournament/<?=$tournament['tournamentID']?>" class="tournament-item">
-		<div class="tournament-name"><?=$tournament['name']?></div>
-		<div class="tournament-date-start"><?=$tournament['tournamentStart']?></div>
-		<div class="tournament-date-end"><?=$tournament['tournamentEnd']?></div>
-		<div class="tournament-registration-status"><span style="font-weight: bold">Registration:</span> <?=( ($registrationStartDate < $today) && ($today < $registrationEndDate) ? "<span class='registrationOpen'>Open!</span>" : "<span class='registrationClosed'>Closed</span>" )?></div>
-	</a>
-	<? } ?>
+		?>
+		<a href="/sis/tournament/<?=$tournament['tournamentID']?>" class="tournament-item">
+			<div class="tournament-name"><?=$tournament['name']?></div>
+			<div class="tournament-date-start"><?=$tournament['tournamentStart']?></div>
+			<div class="tournament-date-end"><?=$tournament['tournamentEnd']?></div>
+			<div class="tournament-registration-status"><span style="font-weight: bold">Registration:</span> <?=( ($registrationStartDate < $today) && ($today < $registrationEndDate) ? "<span class='registrationOpen'>Open!</span>" : "<span class='registrationClosed'>Closed</span>" )?></div>
+		</a>
+		<? } ?>-->
 	<a href="/sis/tournaments_history" class="tournament-button-prev">Previous Tournaments</a>
 </div>
