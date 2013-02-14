36afbb457879ccbe181e6338a65e34e515c1d1a4
lg90 <lg90@hw.ac.uk>
2013-02-14 09:35:08 +0000

    modifying tournaments page

--- application/views/sis/matches.php
+++ application/views/sis/matches.php
@@ -4,14 +4,22 @@
 	<h2>Upcoming Matches</h2>
 	
 	<? foreach($matches as $match) { ?>
-	<a href="/sis/match/<?=$match['matchID']?>" class="match-item">
-		<div class="match-name"><?=$match['name']?></div>
-		<div class="match-sport"><?=$match['sport']?></div>
-		<div class="match-tournament"><?=$match['tournament']?></div>
-		<div class="match-venue"><?=$match['venue']?></div>
-		<div class="match-date"><?=$match['date']?></div>
-		<div class="match-time"><?=$match['startTime']?> - <?=$match['endTime']?></div>
-	</a>
+	<div class="widget half">
+		<a href="/sis/match/<?=$match['matchID']?>">
+			<div class="widget-title">
+				<div class="widget-title-left icon sportname-<?=$match['sport']?> sportid-<?=$tournament['sportID']?>"></div>
+				<div class="widget-title-centre"><?=$match['name']?></div>
+				<div class="widget-title-right icon chevron"></div>
+			</div>
+		</a>
+		<div class="widget-body">
+			<p><b>Date:</b> <?=$match['date']?></p>
+			<p><b>Duration:</b> <?=$match['startTime']?> - <?=$match['endTime']?></p>
+			<p><b>Venue:</b> <?=$match['venue']?></p>
+			<p>Running with a ball, sometimes kicking it. This would be the description of the tournament</p>
+			<?=( ($registrationStartDate < $today) && ($today < $registrationEndDate) ? "<a href='/sis/signup/{$tournament['tournamentID']}' class='button right green'>Sign up!</a>" : "" )?>
+		</div>
+	</div>
 	<? } ?>
 	<a href="/sis/all_matches" class="match-button-next">Full Upcoming List</a>
 </div>
