5f1c69b3c17d678a43f55506d38cbae3b5ec8f29
lg90 <lg90@hw.ac.uk>
2013-02-14 02:08:51 +0000

    modifying auth page to test something

--- application/views/sis/account.php
+++ application/views/sis/account.php
@@ -1,9 +1,11 @@
 <? if($this->ion_auth->logged_in()){ ?>
 	<h1>Welcome <?=$currentUser->firstName?> <?=$currentUser->lastName?></h1>
 
-	<? if(!empty($message)){ ?>
-		<div id="infoMessage"><?php echo $message;?></div>
-	<? } ?>
+	<? if(!empty($message)){ ?><div class="message message-information"><div class="icon margin-right"></div><h3>Information</h3><p><?php echo $message;?></p></div><? } ?>
+	<? if(!empty($message_information)){ ?><div class="message message-information"><div class="icon margin-right"></div><h3>Information</h3><p><?php echo $message_information;?></p></div><? } ?>
+	<? if(!empty($message_success)){ ?><div class="message message-success"><div class="icon margin-right"></div><h3>Success</h3><p><?php echo $message_success;?></p></div><? } ?>
+	<? if(!empty($message_error)){ ?><div class="message message-error"><div class="icon margin-right"></div><h3>Error</h3><p><?php echo $message_error;?></p></div><? } ?>
+	<? if(!empty($message_warning)){ ?><div class="message message-warning"><div class="icon margin-right"></div><h3>Warning</h3><p><?php echo $message_warning;?></p></div><? } ?>
 
 	<a class="button tickets" href="/sis/tickets"><div class="icon"></div><div class="label">Buy Tickets</div><div class="subtitle">Purchase and view your tickets for tournaments</div></a>
 	<a class="button signup" href="/sis/tournaments"><div class="icon"></div><div class="label">Sign up</div><div class="subtitle">Sign up for sports tournaments</div></a>
