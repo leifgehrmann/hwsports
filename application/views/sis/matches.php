2498249459d65f2045cd057aafba9f0308c46c7f
lg90 <lg90@hw.ac.uk>
2013-02-15 21:54:09 +0000

    updating the matches page

--- application/views/sis/matches.php
+++ application/views/sis/matches.php
@@ -1,10 +1,87 @@
 <h1>Matches</h1>
-
-
-This should be replaced with a static table of matches, preferably from datatables.
-
+<p>Filter the matches using the form below, or browse the </p>
+<form>
+	<table>
+		<tr>
+			<td>View: </td>
+			<td>
+				<select name="time">
+					<option value="all">All Matches</option>
+					<option value="upcoming">Upcoming Matches</option>
+					<option value="recent">Recent Matches</option>
+				</select>
+			</td>
+			<td>Sport: </td>
+			<td>
+				<select name="sportID">
+					<option value="19">Wattball</option>
+					<option value="23">Hurdling</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>Tournament: </td>
+			<td>
+				<select name="tournamentID">
+					<option value="19">Wattball 2013</option>
+					<option value="23">Men's Hurdling 2013</option>
+					<option value="23">Women's Hurdling 2013</option>
+				</select>
+			</td>
+			<td>Venue: </td>
+			<td>
+				<select name="venueID">
+					<option value="19">Pitch A</option>
+					<option value="23">Pitch B</option>
+					<option value="23">Pitch C</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td>
+				<select name="tournamentID">
+					<option value="19">Wattball 2013</option>
+					<option value="23">Men's Hurdling 2013</option>
+					<option value="23">Women's Hurdling 2013</option>
+				</select>
+			</td>
+			<td></td>
+			<td><input type="submit" value="Filter" class="green" name="Filter"></td>
+		</tr>
+	</table>
+</form>
+<script>
+$(document).ready(function(){
+	$('table.matches').dataTable();
+});
+</script>
+<table class="full matches">
+	<thead>
+		<tr>
+			<th></th>
+			<th>Date</th>
+			<th>Start</th>
+			<th>Title</th>
+			<th>Venue</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<? foreach($matches as $match) { ?>
+		<tr class="match sportname-<?=$match['sport']?> sportid-<?=$match['sportID']?>">
+			<td><div class="icon"></div></td>
+			<td><?=$match['date']?></td>
+			<td><?=$match['startTime']?></td>
+			<td>Information about upcoming tournaments</td>
+			<td><?=$match['venue']?></td>
+			<td><a href="/sis/match/<?=$match['matchID']?>">View Details</a></td>
+		</tr>
+		<? } ?>
+	</tbody>
+</table>
+<!--
 <div class="matches-upcoming">
-	<? foreach($matches as $match) { ?>
 	<div class="widget half match">
 		<a href="/sis/match/<?=$match['matchID']?>">
 			<div class="widget-title">
@@ -22,4 +99,4 @@ This should be replaced with a static table of matches, preferably from datatabl
 	</div>
 	<? } ?>
 	<a href="/sis/all_matches" class="button blue match-button-next">Full Upcoming List</a>
-</div>
+</div>-->
