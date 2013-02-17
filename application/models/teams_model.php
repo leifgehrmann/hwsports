0da3be9b3bf2506f871993e84c1bb9f88fe20bdf
lg90 <lg90@hw.ac.uk>
2013-02-17 16:53:06 +0000

    changed a bit of fancy box css and also changed the models

--- application/models/teams_model.php
+++ application/models/teams_model.php
@@ -7,7 +7,7 @@ class Teams_model extends CI_Model {
 	 * @return boolean
 	 **/
 
-	public function teams_exists($teamID)
+	public function team_exists($teamID)
 	{
 		$output = array();
 		$queryString = 	"SELECT ".$this->db->escape($teamID)." AS teamID, ".
@@ -18,7 +18,25 @@ class Teams_model extends CI_Model {
 	}
 
 	/**
-	 * Returns all team data for every team
+	 * Returns all team data for the teams in a particular tournament
+	 *  
+	 * @return array
+	 **/
+
+	public function get_tournament_teams($tournamentID)
+	{
+		/*$output = array();
+		$queryString = "SELECT sportID FROM sports WHERE centreID = ".$this->db->escape($centreID);
+		$queryData = $this->db->query($queryString);
+		$data = $queryData->result_array();
+		foreach($data as $sport) {
+			$output[] = $this->get_sport($sport['sportID']);
+		}
+		return $output;*/
+	}
+
+	/**
+	 * Returns all team data for the teams in a 
 	 *  
 	 * @return array
 	 **/
@@ -26,11 +44,11 @@ class Teams_model extends CI_Model {
 	public function get_teams($centreID)
 	{
 		$output = array();
-		$queryString = "SELECT sportID FROM sports WHERE centreID = ".$this->db->escape($centreID);
+		$queryString = "SELECT teamID FROM teams WHERE centreID = ".$this->db->escape($centreID);
 		$queryData = $this->db->query($queryString);
 		$data = $queryData->result_array();
-		foreach($data as $sport) {
-			$output[] = $this->get_sport($sport['sportID']);
+		foreach($data as $team) {
+			$output[] = $this->get_sport($team['teamID']);
 		}
 		return $output;
 	}
@@ -63,88 +81,13 @@ class Teams_model extends CI_Model {
 		}
 		$dataQueryString .= "FROM teamData WHERE teamID = ".$this->db->escape($teamID);
 		$dataQuery = $this->db->query($dataQueryString);
-		$output = array_merge(array("teamID"=>$sportID), $dataQuery->row_array());
-		$output['sportCategoryID'] = $relationalResult[0]['sportCategoryID'];
-		$output['sportCategory'] = $this->get_sport_category($relationalResult[0]['sportCategoryID']);
-		return $output;
-	}
 
-	public function get_sport_categories()
-	{
-		$output = array();
-		$queryString = "SELECT DISTINCT sportCategoryID FROM sportCategoryData";
-		$queryData = $this->db->query($queryString);
-		$data = $queryData->result_array();
-		foreach($data as $sportCategory) {
-			$output[$sportCategory['sportCategoryID']] = $this->get_sport_category($sportCategory['sportCategoryID']);
-		}
-		return $output;
-	}
+		// We would also like to get all the users from the team.
+		// So someone should add that in later.
 
-	public function get_sport_category($sportCategoryID)
-	{
-		$fields = array();
-		$fieldsQuery = $this->db->query("SELECT `key` FROM `sportCategoryData` WHERE `sportCategoryID` = ".$this->db->escape($sportCategoryID) );
-		$fieldsResult = $fieldsQuery->result_array();
-		foreach($fieldsResult as $fieldResult) {
-			$fields[] = $fieldResult['key'];
-		}
-
-		$dataQueryString = "SELECT ";
-		$i = 0;
-		$len = count($fields);
-		foreach($fields as $field) {
-			$dataQueryString .= "MAX(CASE WHEN `key`=".$this->db->escape($field)." THEN value END ) AS ".$this->db->escape($field);
-			if($i<$len-1)
-				$dataQueryString .= ", ";
-			else
-				$dataQueryString .= " ";
-			$i++;
-		}
-		$dataQueryString .= "FROM sportCategoryData WHERE sportCategoryID = ".$this->db->escape($sportCategoryID);
-		$dataQuery = $this->db->query($dataQueryString);
-		$output = array_merge($dataQuery->row_array());
-		return $output;
-	}
-	
-	
-	public function get_sport_category_roles($sportCategoryID)
-	{
-		$output = array();
-		// Get roles for this sportCategoryID
-		$rolesQuery = $this->db->query("SELECT * FROM `sportCategoryRoles` WHERE `sportCategoryID` = ".$this->db->escape($sportCategoryID) );
-		$rolesResult = $rolesQuery->result_array();
-		foreach($rolesResult as $roleResult) {
-			// Get sections for this role
-			$roleInputSectionsQuery = $this->db->query("SELECT sportCategoryRoleInputSectionID,label FROM `sportCategoryRoleInputSections` WHERE `sportCategoryRoleID` = ".$this->db->escape( $roleResult['sportCategoryRoleID'] )." ORDER BY position ASC" );
-			$roleInputSectionsResult = $roleInputSectionsQuery->result_array();
-			$sections = array();
-			foreach($roleInputSectionsResult as $roleInputSectionResult) {
-				// Get inputs for this section
-				$roleInputsQuery = $this->db->query("SELECT sportCategoryRoleInputID,keyName,inputType,formLabel FROM `sportCategoryRoleInputs` WHERE `sportCategoryRoleInputSectionID` = ".$this->db->escape( $roleInputSectionResult['sportCategoryRoleInputSectionID'] )." ORDER BY position ASC" );
-				$roleInputsResult = $roleInputsQuery->result_array();
-				$inputs = array();
-				foreach($roleInputsResult as $roleInput) {
-					$inputs[ $roleInput['sportCategoryRoleInputID'] ] = array (
-						'keyName' => $roleInput['keyName'],
-						'inputType' => $roleInput['inputType'],
-						'formLabel' => $roleInput['formLabel']
-					);
-				}
-					
-				$sections[ $roleInputSectionResult['sportCategoryRoleInputSectionID'] ] = array (
-					'label' => $roleInputSectionResult['label'],
-					'inputs' => $inputs
-				);
-			}
-			
-			// Add role to output
-			$output[$roleResult['sportCategoryRoleID']] = array (
-				'name' => $roleResult['sportCategoryRoleName'],
-				'inputSections' => $sections
-			);
-		}
-		
+		$output = array_merge(array("teamID"=>$teamID), $dataQuery->row_array());
+		$output['teamID'] = $relationalResult[0]['teamID'];
+		$output['team'] = $this->get_sport_category($relationalResult[0]['teamID']);
 		return $output;
 	}
 
@@ -155,23 +98,23 @@ class Teams_model extends CI_Model {
 	 *  
 	 * @return int
 	 **/
-	public function insert_sport($centreID, $data)
+	public function insert_team($centreID, $data)
 	{	
 		$this->db->trans_start();
 
-		$this->db->query("INSERT INTO sports (centreID) VALUES (".$this->db->escape($centreID).")");
-		$sportID = $this->db->insert_id();
+		$this->db->query("INSERT INTO teams (teamID) VALUES (".$this->db->escape($teamID).")");
+		$teamID = $this->db->insert_id();
 
 		$insertDataArray = array();
 		foreach($data as $key=>$value) {
 			$dataArray = array(
-					'sportID' => $this->db->escape($sportID),
+					'teamID' => $this->db->escape($sportID),
 					'key' => $this->db->escape($key),
 					'value' => $this->db->escape($value)
 				);
 			$insertDataArray[] = $dataArray;
 		}
-		if ($this->db->insert_batch('venueData',$insertDataArray)) {
+		if ($this->db->insert_batch('teamData',$insertDataArray)) {
 			// db success
 			$this->db->trans_complete();
 			return $sportID;
@@ -182,22 +125,22 @@ class Teams_model extends CI_Model {
 	}
 
 	/**
-	 * Updates a sport with data.
+	 * Updates a team with data.
 	 *
 	 * @return boolean
 	 **/
-	public function update_venue($sportID, $data){
+	public function update_team($teamID, $data){
 
 		$this->db->trans_start();
 
-		if($this->venue_exists($sportID)){
+		if($this->team_exists($teamID)){
 			foreach($data as $key=>$value) {
 				$escKey = $this->db->escape($key);
 				$escValue = $this->db->escape($value);
-				$dataQueryString = 	"UPDATE `venueData` ".
+				$dataQueryString = 	"UPDATE `teamData` ".
 									"SET `value`=$escValue ".
 									"WHERE `key`=$escKey ".
-									"AND `sportID`=$sportID";
+									"AND `teamID`=$teamID";
 				$this->db->query($dataQueryString);
 			}
 			$this->db->trans_complete();
--- application/models/users_model.php
+++ application/models/users_model.php
@@ -1,19 +1,91 @@
 <?php
-class User_model extends CI_Model {
+class Users_model extends CI_Model {
 
-	public function get_all_users($centreID, $fields)
+	public function user_exists($userID)
 	{
-
+		$output = array();
+		$queryString = 	"SELECT ".$this->db->escape($userID)." AS userID, ".
+						"EXISTS(SELECT 1 FROM users WHERE id = ".$this->db->escape($userID).") AS `exists`";
+		$queryData = $this->db->query($queryString);
+		$output = $queryData->row_array();
+		return $output['exists'];
 	}
 
-	public function get_user($userID, $fields)
+	public function get_all_users($centreID)
 	{
-		
+		// To find out if a user is associated with the centre...
+
+		// Tickets > "userID"
+		// Tournaments[teamSport=1] > "teams"    >> TeamsUsers > "userID"
+		// Tournaments[teamSport=0] > "athletes" >> "userID"
+		// UserData[centreID=1] > userID
+
+		$output = array();
+		$queryString = "SELECT userID FROM userData WHERE `key`='centreID' AND `value`=".$this->db->escape($centreID);
+		$queryData = $this->db->query($queryString);
+		$data = $queryData->result_array();
+		foreach($data as $user) {
+			$output[] = $this->get_user($user['userID']);
+		}
+		return $output;
+	}
+
+	public function get_tournament_users($tournamentID)
+	{
+		$output = array();
+		$queryString = "SELECT userID FROM users WHERE centreID = ".$this->db->escape($centreID);
+		$queryData = $this->db->query($queryString);
+		$data = $queryData->result_array();
+		foreach($data as $user) {
+			$output[] = $this->get_user($user['userID']);
+		}
+		return $output;
+	}
+
+	public function get_user($userID)
+	{
+		$fields = array();
+		$fieldsQuery = $this->db->query("SELECT `key` FROM `userData` WHERE `userID` = ".$this->db->escape($userID) );
+		$fieldsResult = $fieldsQuery->result_array();
+		foreach($fieldsResult as $fieldResult) {
+			$fields[] = $fieldResult['key'];
+		}
+
+		$dataQueryString = "SELECT ";
+		$i = 0;
+		$len = count($fields);
+		foreach($fields as $field) {
+			$dataQueryString .= "MAX(CASE WHEN `key`=".$this->db->escape($field)." THEN value END ) AS ".$this->db->escape($field);
+			if($i<$len-1)
+				$dataQueryString .= ", ";
+			else
+				$dataQueryString .= " ";
+			$i++;
+		}q3
+		$dataQueryString .= "FROM userData WHERE userID = ".$this->db->escape($userID);
+		$dataQuery = $this->db->query($dataQueryString);
+		$output = array_merge(array("userID"=>$userID), $dataQuery->row_array());
+		return $output;
 	}
 
 	public function update_user($userID, $data)
 	{
-
+		$this->db->trans_start();
+		if($this->user_exists($userID)){
+			foreach($data as $key=>$value) {
+				$escKey = $this->db->escape($key);
+				$escValue = $this->db->escape($value);
+				$dataQueryString = 	"UPDATE `userData` ".
+									"SET `value`=$escValue ".
+									"WHERE `key`=$escKey ".
+									"AND `userID`=$userID";
+				$this->db->query($dataQueryString);
+			}
+			$this->db->trans_complete();
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
