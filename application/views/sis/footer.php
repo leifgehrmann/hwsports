69f80213e3585389eb7a303bec79408992c380f9
lg90 <lg90@hw.ac.uk>
2013-02-17 13:02:22 +0000

    modified the centre details and added footer contact details

--- application/views/sis/footer.php
+++ application/views/sis/footer.php
@@ -5,18 +5,19 @@
 				<div id="footer-block">
 					<div id="footer-contact">
 						<h1>Contact Us</h1>
-						<p>&copy; Heriot-Watt University</p>
-						<p>Edinburgh, Scotland, UK EH14 4AS</p>
-						<p>Tel: +44 (0) 131 449 5111</p>
-						<p>Scottish registered charity number: SC000278</p>
+						<? 
+						$lines = explode("\n",$centre['publicTitle']);
+						for($lines as $line){?>
+						<p><?=$line?></p>
+						<? } ?>
 					</div>
 					<div id="footer-links">
 						<h1>Links</h1>
-						<p><a href="#">Heriot Watt University Homepage</a></p>
+						<p><a href="http://www.hw.ac.uk">Heriot Watt University Homepage</a></p>
 						<p><a href="#">Some other footer nonsense</a></p>
 						<p><a href="#">Hopefully we will fix this</a></p>
 						<p><a href="#">Privicy Policy</a></p>
-						<p><a href="#">About InfusionSports</a></p>
+						<p><a href="http://www.infusionsports.co.uk">About InfusionSports</a></p>
 					</div>
 					<div id="footer-powered">
 						<h1>Powered By</h1>
@@ -28,12 +29,3 @@
 		</div>
 	</body>
 </html>
-<!--
-			</div>
-			<div id="footer">
-				<div class="footerText"><?=$centre['footerText']?></div>
-				<div class="powered">Powered By:<a href="http://www.infusionsports.co.uk"><div class="logo"></div></a></div>
-			</div>
-		</div>
-	</body>
-</html>-->
