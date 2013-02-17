5f8992daef025cb4b03e08713302bcdd5c8a9a86
lg90 <lg90@hw.ac.uk>
2013-02-17 14:14:44 +0000

    did some things to teams_model and also changed some things in addTeamMember

---
+++ application/models/teams_model.php
@@ -0,0 +1,209 @@
+<?php
+class Teams_model extends CI_Model {
+
+	/**
+	 * $teamID is int(11)
+	 *  
+	 * @return boolean
+	 **/
+
+	public function teams_exists($teamID)
+	{
+		$output = array();
+		$queryString = 	"SELECT ".$this->db->escape($teamID)." AS teamID, ".
+						"EXISTS(SELECT 1 FROM teams WHERE teamID = ".$this->db->escape($teamID).") AS `exists`";
+		$queryData = $this->db->query($queryString);
+		$output = $queryData->row_array();
+		return $output['exists'];
+	}
+
+	/**
+	 * Returns all team data for every team
+	 *  
+	 * @return array
+	 **/
+
+	public function get_teams($centreID)
+	{
+		$output = array();
+		$queryString = "SELECT sportID FROM sports WHERE centreID = ".$this->db->escape($centreID);
+		$queryData = $this->db->query($queryString);
+		$data = $queryData->result_array();
+		foreach($data as $sport) {
+			$output[] = $this->get_sport($sport['sportID']);
+		}
+		return $output;
+	}
+
+	/**
+	 * Returns an array of data for a specific team
+	 * It should also return all team members.
+	 *  
+	 * @return array
+	 **/
+	public function get_team($teamID)
+	{
+		$fields = array();
+		$fieldsQuery = $this->db->query("SELECT `key` FROM `teamData` WHERE `teamID` = ".$this->db->escape($teamID) );
+		$fieldsResult = $fieldsQuery->result_array();
+		foreach($fieldsResult as $fieldResult) {
+			$fields[] = $fieldResult['key'];
+		}
+
+		$dataQueryString = "SELECT ";
+		$i = 0;
+		$len = count($fields);
+		foreach($fields as $field) {
+			$dataQueryString .= "MAX(CASE WHEN `key`=".$this->db->escape($field)." THEN value END ) AS ".$this->db->escape($field);
+			if($i<$len-1)
+				$dataQueryString .= ", ";
+			else
+				$dataQueryString .= " ";
+			$i++;
+		}
+		$dataQueryString .= "FROM teamData WHERE teamID = ".$this->db->escape($teamID);
+		$dataQuery = $this->db->query($dataQueryString);
+		$output = array_merge(array("teamID"=>$sportID), $dataQuery->row_array());
+		$output['sportCategoryID'] = $relationalResult[0]['sportCategoryID'];
+		$output['sportCategory'] = $this->get_sport_category($relationalResult[0]['sportCategoryID']);
+		return $output;
+	}
+
+	public function get_sport_categories()
+	{
+		$output = array();
+		$queryString = "SELECT DISTINCT sportCategoryID FROM sportCategoryData";
+		$queryData = $this->db->query($queryString);
+		$data = $queryData->result_array();
+		foreach($data as $sportCategory) {
+			$output[$sportCategory['sportCategoryID']] = $this->get_sport_category($sportCategory['sportCategoryID']);
+		}
+		return $output;
+	}
+
+	public function get_sport_category($sportCategoryID)
+	{
+		$fields = array();
+		$fieldsQuery = $this->db->query("SELECT `key` FROM `sportCategoryData` WHERE `sportCategoryID` = ".$this->db->escape($sportCategoryID) );
+		$fieldsResult = $fieldsQuery->result_array();
+		foreach($fieldsResult as $fieldResult) {
+			$fields[] = $fieldResult['key'];
+		}
+
+		$dataQueryString = "SELECT ";
+		$i = 0;
+		$len = count($fields);
+		foreach($fields as $field) {
+			$dataQueryString .= "MAX(CASE WHEN `key`=".$this->db->escape($field)." THEN value END ) AS ".$this->db->escape($field);
+			if($i<$len-1)
+				$dataQueryString .= ", ";
+			else
+				$dataQueryString .= " ";
+			$i++;
+		}
+		$dataQueryString .= "FROM sportCategoryData WHERE sportCategoryID = ".$this->db->escape($sportCategoryID);
+		$dataQuery = $this->db->query($dataQueryString);
+		$output = array_merge($dataQuery->row_array());
+		return $output;
+	}
+	
+	
+	public function get_sport_category_roles($sportCategoryID)
+	{
+		$output = array();
+		// Get roles for this sportCategoryID
+		$rolesQuery = $this->db->query("SELECT * FROM `sportCategoryRoles` WHERE `sportCategoryID` = ".$this->db->escape($sportCategoryID) );
+		$rolesResult = $rolesQuery->result_array();
+		foreach($rolesResult as $roleResult) {
+			// Get sections for this role
+			$roleInputSectionsQuery = $this->db->query("SELECT sportCategoryRoleInputSectionID,label FROM `sportCategoryRoleInputSections` WHERE `sportCategoryRoleID` = ".$this->db->escape( $roleResult['sportCategoryRoleID'] )." ORDER BY position ASC" );
+			$roleInputSectionsResult = $roleInputSectionsQuery->result_array();
+			$sections = array();
+			foreach($roleInputSectionsResult as $roleInputSectionResult) {
+				// Get inputs for this section
+				$roleInputsQuery = $this->db->query("SELECT sportCategoryRoleInputID,keyName,inputType,formLabel FROM `sportCategoryRoleInputs` WHERE `sportCategoryRoleInputSectionID` = ".$this->db->escape( $roleInputSectionResult['sportCategoryRoleInputSectionID'] )." ORDER BY position ASC" );
+				$roleInputsResult = $roleInputsQuery->result_array();
+				$inputs = array();
+				foreach($roleInputsResult as $roleInput) {
+					$inputs[ $roleInput['sportCategoryRoleInputID'] ] = array (
+						'keyName' => $roleInput['keyName'],
+						'inputType' => $roleInput['inputType'],
+						'formLabel' => $roleInput['formLabel']
+					);
+				}
+					
+				$sections[ $roleInputSectionResult['sportCategoryRoleInputSectionID'] ] = array (
+					'label' => $roleInputSectionResult['label'],
+					'inputs' => $inputs
+				);
+			}
+			
+			// Add role to output
+			$output[$roleResult['sportCategoryRoleID']] = array (
+				'name' => $roleResult['sportCategoryRoleName'],
+				'inputSections' => $sections
+			);
+		}
+		
+		return $output;
+	}
+
+	/**
+	 * Creates a sport with data.
+	 * returns the sportID of the new sport if it was
+	 * successful. If not, it should return -1.
+	 *  
+	 * @return int
+	 **/
+	public function insert_sport($centreID, $data)
+	{	
+		$this->db->trans_start();
+
+		$this->db->query("INSERT INTO sports (centreID) VALUES (".$this->db->escape($centreID).")");
+		$sportID = $this->db->insert_id();
+
+		$insertDataArray = array();
+		foreach($data as $key=>$value) {
+			$dataArray = array(
+					'sportID' => $this->db->escape($sportID),
+					'key' => $this->db->escape($key),
+					'value' => $this->db->escape($value)
+				);
+			$insertDataArray[] = $dataArray;
+		}
+		if ($this->db->insert_batch('venueData',$insertDataArray)) {
+			// db success
+			$this->db->trans_complete();
+			return $sportID;
+		} else {
+			// db fail
+			return -1;
+		}
+	}
+
+	/**
+	 * Updates a sport with data.
+	 *
+	 * @return boolean
+	 **/
+	public function update_venue($sportID, $data){
+
+		$this->db->trans_start();
+
+		if($this->venue_exists($sportID)){
+			foreach($data as $key=>$value) {
+				$escKey = $this->db->escape($key);
+				$escValue = $this->db->escape($value);
+				$dataQueryString = 	"UPDATE `venueData` ".
+									"SET `value`=$escValue ".
+									"WHERE `key`=$escKey ".
+									"AND `sportID`=$sportID";
+				$this->db->query($dataQueryString);
+			}
+			$this->db->trans_complete();
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
--- application/views/sis/addTeamMember.php
+++ application/views/sis/addTeamMember.php
@@ -1,41 +1,38 @@
 <div class="fancyform">
-  <? if(!empty($success)) { ?>
-		<script type="text/javascript">
-			$('a.addTeamMember').before('<p class="teamMember"><?=$user['firstName'].' '.$user['lastName']?> (ID: <span class="teamMemberID"><?=$user['id']?></span>)</p>');
-			$.fancybox.close();
-		</script>
-  <? } else { ?>
-		<h1>Add New Team Member</h1>
-		<p>Please enter the member's details below.</p>
+	<? if(!empty($success)) { ?>
+	<script type="text/javascript">
+	$('a.addTeamMember').before('<p class="teamMember"><?=$user['firstName'].' '.$user['lastName']?> (ID: <span class="teamMemberID"><?=$user['id']?></span>)</p>');
+	$.fancybox.close();
+	</script>
+	<? } else { ?>
+	<h1>Add New Team Member</h1>
+	<p>Please enter the member's details below.</p>
 
-		<? if(!empty($message)){ ?>
-			  <div id="infoMessage"><?=$message;?></div>
-		<? } ?>
+	<? if(!empty($message)){ ?>
+	<div id="infoMessage"><?=$message;?></div>
+	<? } ?>
 
-		<?php echo form_open("/sis/addTeamMember");?>
+	<?php echo form_open("/sis/addTeamMember");?>
+	<table>
+		<tr>
+			<td>First Name:</td>
+			<td><?php echo form_input($first_name);?></td>
+		</tr>
+		<tr>
+			<td>Last Name: </td>
+			<td><?php echo form_input($last_name);?></td>
+		</tr>
+		<tr>
+			<td>Email: </td>
+			<td><?php echo form_input($email);?></td>
+		</tr>
+		<tr>
+			<td>Phone: </td>>
+			<td><?php echo form_input($phone);?></td>
+		</tr>
+	</table>
+	<p><?php echo form_submit('submit', 'Create Team Member');?></p>
 
-			  <p>
-					First Name: <br />
-					<?php echo form_input($first_name);?>
-			  </p>
-
-			  <p>
-					Last Name: <br />
-					<?php echo form_input($last_name);?>
-			  </p>
-
-			  <p>
-					Email: <br />
-					<?php echo form_input($email);?>
-			  </p>
-
-			  <p>
-					Phone: <br />
-					<?php echo form_input($phone);?>
-			  </p>
-
-			  <p><?php echo form_submit('submit', 'Create Team Member');?></p>
-
-		<?php echo form_close();?>
-   <? } ?>
+	<?php echo form_close();?>
+	<? } ?>
 </div>
