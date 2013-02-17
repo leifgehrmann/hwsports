76c6d6b7ddc73e9f205fc18652a42d0564945c57
lg90 <lg90@hw.ac.uk>
2013-02-17 13:28:05 +0000

    changed some stuff in the database, together with the footer.php

--- application/views/sis/footer.php
+++ application/views/sis/footer.php
@@ -5,16 +5,16 @@
 				<div id="footer-block">
 					<div id="footer-contact">
 						<h1>Contact Us</h1>
-						<? $lines = explode("\\n",$centre['publicFooterContact']);
+						<? $lines = explode("\n",$centre['publicFooterContact']);
 						foreach($lines as $line){?>
 						<p><?=$line?></p>
 						<? } ?>
 					</div>
 					<div id="footer-links">
 						<h1>Links</h1>
-						<? $lines = explode("\\n",$centre['publicFooterLinks']);
-						for($i=0;$i<count($lines)/2;$i++){?>
-						<p><a href="<?=$lines[$i*2]?>"><?=$lines[$i*2+1]?></a></p>
+						<? $lines = explode("\n",$centre['publicFooterLinks']);
+						for($i=0;$i<count($lines)/2;$i+=2){?>
+						<p><a href="<?=$lines[$i*2]?>"><?=$lines[$i/2+1]?></a></p>
 						<? } ?>
 					</div>
 					<div id="footer-powered">
