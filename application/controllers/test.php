123a674d80e5056411b4f6f06bc4b23cf90808a8
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-02-18 23:30:20 +0000

    updated test controller to use better var_export rather than print_r

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -12,53 +12,53 @@ class Test extends MY_Controller {
 	}
 	public function get_all_users($centreID){
 		$output = $this->users_model->get_users($centreID);
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 	public function get_all_teams($centreID){
 		$output = $this->teams_model->get_teams($centreID);
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 	public function get_sport_category_roles($sportID){
 		$output = $this->sports_model->get_sport_category_roles($sportID);
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 
 	public function get_sport_category($sportID){
 		$output = $this->sports_model->get_sport_categories();
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 
 	public function get_sport($sportID){
 		$output = $this->sports_model->get_sport($sportID);
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 
 	public function get_tournament($tID){
 		$output = $this->tournaments_model->get_tournament($tID);
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 	public function get_tournaments($centreID){
 		$output = $this->tournaments_model->get_tournaments($centreID);
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 
 	public function get_matches($centreID){
 		$output = $this->matches_model->get_matches($centreID);
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 	
 	public function user_exists($userID){
 		$output = $this->users_model->user_exists($userID);
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 
 	public function test_constants(){
 		$output = array(APPPATH,SYSDIR,BASEPATH,ENVIRONMENT,SELF,FCPATH,EXT);
-		$this->display(var_export($output, true));
+		$this->display($output);
 	}
 
-	public function display(var_export($output, true)){
-		$this->data['data'] =  print_r($output,1);
+	public function display($output){
+		$this->data['data'] =  var_export($output,true);
 		header('Content-Type: text/plain');
 		$this->load->view('data', $this->data);
 	}
