8fa1c38f9476594567a5dc4c7acfd8751d8da020
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-02-18 01:42:54 +0000

    testing adding team members by logging in

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -67,7 +67,7 @@ class Users_model extends CI_Model {
 		
 		$this->load->model('ion_auth_model');
 		$ionUser = $this->ion_auth_model->user($userID);
-		$output = array_merge(array("userID"=>$userID,"email"=>$ionUser['email']), $dataQuery->row_array());
+		$output = array_merge(array("userID"=>$userID,"email"=>$ionUser->email), $dataQuery->row_array());
 		return $output;
 	}
 
