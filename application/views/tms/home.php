173f0dadfb9e63474c184bd12024557cd80d0863
lg90 <lg90@hw.ac.uk>
2013-03-01 14:52:24 +0000

    updated dashboard page

--- application/views/tms/home.php
+++ application/views/tms/home.php
@@ -9,10 +9,10 @@
 		<? 
 			// foreach($)
 		?>
-		<div class="icon left margin-right"></div><p><a href="/match/sdfkdsbfds">Team A vs Team B</a><br/>20th February &mdash; 10:28</p>
-		<div class="icon left margin-right"></div><p><a href="/match/dsfknds">Team B vs Team C</a>20th February &mdash; 16:30</p>
-		<div class="icon left margin-right"></div><p><a href="/match/sdlkfsd">Heriot Hurdling - Day 1</a>21st February &mdash; 19:20</p>
-		<p><a href="/matches/" class="button right blue">View All Matches</a></p>
+		<div class="match sportCategoryID-6"><div class="icon left margin-right"></div><p><a href="/tms/match/sdfkdsbfds">Team A vs Team B</a><br/>20th February &mdash; 10:28</p></div>
+		<div class="match sportCategoryID-7"><div class="icon left margin-right"></div><p><a href="/tms/match/dsfknds">Team B vs Team C</a><br/>20th February &mdash; 16:30</p></div>
+		<div class="match sportCategoryID-8"><div class="icon left margin-right"></div><p><a href="/tms/match/sdlkfsd">Heriot Hurdling - Day 1</a><br/>21st February &mdash; 19:20</p></div>
+		<p><a href="/tms/matches/" class="button right blue">View All Matches</a></p>
 	</div>
 </div>
 <div class="widget half matches">
@@ -26,12 +26,12 @@
 			// foreach($)
 		?>
 
-		<div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 2</a><br/>21st February &mdash; 00:00</p>
-		<div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 3</a><br/>22nd February &mdash; 00:00</p>
-		<div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 4</a><br/>23rd February &mdash; 00:00</p>
-		<div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 5</a><br/>24th February &mdash; 00:00</p>
-		<div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 6</a><br/>25th February &mdash; 00:00</p>
-		<p><a href="/matches/" class="button right blue">View All Matches</a></p>
+		<div class="match sportCategoryID-1"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 2</a><br/>21st February &mdash; 00:00</p></div>
+		<div class="match sportCategoryID-2"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 3</a><br/>22nd February &mdash; 00:00</p></div>
+		<div class="match sportCategoryID-3"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 4</a><br/>23rd February &mdash; 00:00</p></div>
+		<div class="match sportCategoryID-4"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 5</a><br/>24th February &mdash; 00:00</p></div>
+		<div class="match sportCategoryID-5"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 6</a><br/>25th February &mdash; 00:00</p></div>
+		<p><a href="/tms/matches/" class="button right blue">View All Matches</a></p>
 	</div>
 </div>
 <div class="widget half tournaments">
@@ -44,10 +44,10 @@
 		<? 
 			// foreach($)
 		?>
-		<p><a href="/tournament/dshjf"		>2012 Heriot Watt</a> &mdash; 5th-12th February</p>
-		<p><a href="/tournament/afjb"		>2012 Rugby</a> &mdash; 5th-12th February</p>
-		<p><a href="/tournament/sdlkfjsd"	>2012 Summer WattBall</a> &mdash; 5th-12th February</p>
-		<p><a href="/tournaments/" class="button right blue">View All Tournaments</a></p>
+		<p><a href="/tms/tournament/dshjf"		>2012 Heriot Watt</a> &mdash; 5th-12th February</p>
+		<p><a href="/tms/tournament/afjb"		>2012 Rugby</a> &mdash; 5th-12th February</p>
+		<p><a href="/tms/tournament/sdlkfjsd"	>2012 Summer WattBall</a> &mdash; 5th-12th February</p>
+		<p><a href="/tms/tournaments/" class="button right blue">View All Tournaments</a></p>
 	</div>
 </div>
 <div class="widget half tournaments">
@@ -60,8 +60,8 @@
 		<? 
 			// foreach($)
 		?>
-		<p><a href="/tournament/flksdf">2013 Heriot Watt</a> &mdash; 5th-12th February</p>
-		<p><a href="/tournament/dsflkjsd">2014 Summer WattBall</a> &mdash; 5th-12th February</p>
-		<p><a href="/tournaments/" class="button right blue">View All Tournaments</a></p>
+		<p><a href="/tms/tournament/flksdf">2013 Heriot Watt</a> &mdash; 5th-12th February</p>
+		<p><a href="/tms/tournament/dsflkjsd">2014 Summer WattBall</a> &mdash; 5th-12th February</p>
+		<p><a href="/tms/tournaments/" class="button right blue">View All Tournaments</a></p>
 	</div>
 </div>
