0b6a91c25bbf6899f7e69cc11bfacee0413e3ce3
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-01 17:42:47 +0000

    tournament status model debugging

--- application/config/constants.php
+++ application/config/constants.php
@@ -39,8 +39,8 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 // Herp derp date times
 
-define('DATE_FORMAT' = "yyyy-mm-dd");
-define('DATE_TIME_FORMAT' = DateTime::ISO8601);
+define('DATE_FORMAT', "yyyy-mm-dd");
+define('DATE_TIME_FORMAT', DateTime::ISO8601);
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
