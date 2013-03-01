8fb802fad885f09967ecbd9c14a94eb269357c3d
lg90 <lg90@hw.ac.uk>
2013-03-01 15:05:45 +0000

    updated dashboard page

--- application/views/tms/home.php
+++ application/views/tms/home.php
@@ -26,11 +26,11 @@
 			// foreach($)
 		?>
 
-		<div class="match sportCategoryID-1"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 2</a><br/>21st February &mdash; 00:00</p></div>
-		<div class="match sportCategoryID-2"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 3</a><br/>22nd February &mdash; 00:00</p></div>
-		<div class="match sportCategoryID-3"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 4</a><br/>23rd February &mdash; 00:00</p></div>
-		<div class="match sportCategoryID-4"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 5</a><br/>24th February &mdash; 00:00</p></div>
-		<div class="match sportCategoryID-5"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 6</a><br/>25th February &mdash; 00:00</p></div>
+		<div class="match sportCategoryID-1"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 2</a><br/><span style="display: inline-block;padding-left: 40px;">21st February &mdash; 00:00</span></p></div>
+		<div class="match sportCategoryID-2"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 3</a><br/><span style="display: inline-block;padding-left: 40px;">22nd February &mdash; 00:00</span></p></div>
+		<div class="match sportCategoryID-3"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 4</a><br/><span style="display: inline-block;padding-left: 40px;">23rd February &mdash; 00:00</span></p></div>
+		<div class="match sportCategoryID-4"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 5</a><br/><span style="display: inline-block;padding-left: 40px;">24th February &mdash; 00:00</span></p></div>
+		<div class="match sportCategoryID-5"><div class="icon left margin-right"></div><p><a href="">Heriot Hurdling - Day 6</a><br/><span style="display: inline-block;padding-left: 40px;">25th February &mdash; 00:00</span></p></div>
 		<p><a href="/tms/matches/" class="button right blue">View All Matches</a></p>
 	</div>
 </div>
