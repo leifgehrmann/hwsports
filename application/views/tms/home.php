2af2ac74bcb39b4afe4854c7d16382f0eb248585
lg90 <lg90@hw.ac.uk>
2013-03-01 14:45:22 +0000

    updated dashboard page

--- application/views/tms/home.php
+++ application/views/tms/home.php
@@ -1,8 +1,8 @@
 <h1>Dashboard</h1>
-<div class="widget half tournaments">
+<div class="widget half matches">
 	<div class="widget-title">
 		<div class="widget-title-left icon"></div>
-		<div class="widget-title-centre">Latest Matches / Games</div>
+		<div class="widget-title-centre">Latest Matches</div>
 		<div class="widget-title-right icon"></div>
 	</div>
 	<div class="widget-body">
@@ -12,6 +12,25 @@
 		<div class="icon left margin-right"></div><p><a href="/match/sdfkdsbfds">Team A vs Team B</a><br/>20th February &mdash; 10:28</p>
 		<div class="icon left margin-right"></div><p><a href="/match/dsfknds">Team B vs Team C</a></p><p>20th February &mdash; 16:30</p>
 		<div class="icon left margin-right"></div><p><a href="/match/sdlkfsd">Heriot Hurdling - Day 1</a></p><p>21st February &mdash; 19:20</p>
+		<p><a href="/matches/" class="button right blue">View All Matches</a></p>
+	</div>
+</div>
+<div class="widget half matches">
+	<div class="widget-title">
+		<div class="widget-title-left icon"></div>
+		<div class="widget-title-centre">Upcoming Matches</div>
+		<div class="widget-title-right icon"></div>
+	</div>
+	<div class="widget-body">
+		<? 
+			// foreach($)
+		?>
+
+		<div class="icon left margin-right"><p><a href="">Heriot Hurdling - Day 2</a><br/>21st February &mdash; 00:00</p></div>
+		<div class="icon left margin-right"><p><a href="">Heriot Hurdling - Day 3</a><br/>22nd February &mdash; 00:00</p></div>
+		<div class="icon left margin-right"><p><a href="">Heriot Hurdling - Day 4</a><br/>23rd February &mdash; 00:00</p></div>
+		<div class="icon left margin-right"><p><a href="">Heriot Hurdling - Day 5</a><br/>24th February &mdash; 00:00</p></div>
+		<div class="icon left margin-right"><p><a href="">Heriot Hurdling - Day 6</a><br/>25th February &mdash; 00:00</p></div>
 		<p><a href="/matches/" class="button right blue">View All Matches</a></p>
 	</div>
 </div>
@@ -45,19 +64,4 @@
 		<p><a href="/tournament/dsflkjsd">2014 Summer WattBall</a> &mdash; 5th-12th February</p>
 		<p><a href="/tournaments/" class="button right blue">View All Tournaments</a></p>
 	</div>
-</div>
-
-<div class="widget">
-	<h2>Latest Matches / Games</h2>
-	<div class="row"><p><a href="">Team A vs Team B</a></p><p class="subtitle"><span class="green light bold" style="margin-right:10px;">WattBall</span> 20th February</p></div>
-	<div class="row"><p><a href="">Team B vs Team C</a></p><p class="subtitle"><span style="color:#468C00;font-weight:bold;margin-right:10px;">WattBall</span> 21st February</p></div>
-	<div class="row"><p><a href="">Heriot Hurdling - Day 1</a></p><p class="subtitle"><span style="color:#B25900;font-weight:bold;margin-right:10px;">Heriot Hurdling Mens</span> 20th February</p></div>
-</div>
-<div class="widget">
-	<h2>Upcoming Matches / Games</h2>
-	<div class="row"><p><a href="">Heriot Hurdling - Day 2</a></p><p class="subtitle"><span style="color:#B25900;font-weight:bold;margin-right:10px;">Heriot Hurdling Womens</span> 21st February</p></div>
-	<div class="row"><p><a href="">Heriot Hurdling - Day 3</a></p><p class="subtitle"><span style="color:#B25900;font-weight:bold;margin-right:10px;">Heriot Hurdling Mens</span> 22nd February</p></div>
-	<div class="row"><p><a href="">Heriot Hurdling - Day 4</a></p><p class="subtitle"><span style="color:#B25900;font-weight:bold;margin-right:10px;">Heriot Hurdling Womens</span> 23rd February</p></div>
-	<div class="row"><p><a href="">Heriot Hurdling - Day 5</a></p><p class="subtitle"><span style="color:#B25900;font-weight:bold;margin-right:10px;">Heriot Hurdling Mens</span> 24th February</p></div>
-	<div class="row"><p><a href="">Heriot Hurdling - Day 6</a></p><p class="subtitle"><span style="color:#B25900;font-weight:bold;margin-right:10px;">Heriot Hurdling Womens</span> 25th February</p></div>
 </div>
