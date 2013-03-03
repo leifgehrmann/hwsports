0b8e108515d34c10644c0631606073b344b4a028
lg90 <lg90@hw.ac.uk>
2013-03-03 16:11:54 +0000

    updated calendar

--- application/models/matches_model.php
+++ application/models/matches_model.php
@@ -48,7 +48,7 @@ class Matches_model extends CI_Model {
 	public function are_valid_dates_in_match($startTime, $endTime, $matchID)
 	{	
 		$match = $this->get_match($matchID);
-		if(array_key_exists('tournamentID',$match))
+		if(!empty($match['tournamentID']))
 			return $this->are_valid_dates_in_tournament($startTime, $endTime, $match['tournamentID']);
 		else
 			return ($startTime<$endTime);
