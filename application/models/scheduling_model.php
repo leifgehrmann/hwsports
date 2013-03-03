b2ddc79dc92193243a1fada8086362124ddd430a
lg90 <lg90@hw.ac.uk>
2013-03-03 11:39:04 +0000

    added scheduling model

---
+++ application/models/scheduling_model.php
@@ -0,0 +1,45 @@
+<?php
+class Scheduling_model extends CI_Model {
+
+	/**
+	 * $tournamentID is int(11)
+	 * returns true if the scheduling went well.
+	 * an error string if it didn't go well.
+	 * @return boolean 
+	 **/
+
+	public function scheduleFootballFamily($tournamentID)
+	{
+		// Does every team have minimum of players...
+
+		// If tournament is round robin...
+
+		// if team 
+	}
+
+	/**
+	 * $tournamentID is int(11)
+	 * returns true if the scheduling went well.
+	 * an error string if it didn't go well.
+	 * @return boolean 
+	 **/
+
+	public function scheduleRunning($tournamentID)
+	{
+		// For every individual player
+	}
+
+
+	/**
+	 * teams is an array of teamIDs of int(11)
+	 * returns an array of team tuples.
+	 * @return array
+	 **/	
+
+	private function roundrobin($teams)
+	{
+		
+
+	}
+	
+}
