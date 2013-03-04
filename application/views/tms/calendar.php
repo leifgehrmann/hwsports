14c995974c5065e4af3f2d9221215e7a5003c895
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-04 03:03:19 +0000

    added debug output to calendar ajax call

--- application/views/tms/calendar.php
+++ application/views/tms/calendar.php
@@ -43,7 +43,7 @@
 				});
  
 				request.done(function(msg) {
-					//alert( msg );
+					alert( msg );
 				});
 				 
 				request.fail(function(jqXHR, textStatus) {
@@ -62,7 +62,7 @@
 				});
  				
 				request.done(function(msg) {
-					//alert( msg );
+					alert( msg );
 				});
 				 
 				request.fail(function(jqXHR, textStatus) {
