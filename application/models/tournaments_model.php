123975c66dedd8c332cfb50964eea76be5b4ac32
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-04 04:26:56 +0000

    tournament insert was failing

--- application/models/tournaments_model.php
+++ application/models/tournaments_model.php
@@ -147,8 +147,9 @@ class Tournaments_model extends CI_Model {
 		$this->db->trans_start();
 		
 		$this->db->query("INSERT INTO tournaments (centreID,sportID) VALUES ({$this->data['centre']['centreID']},$sportID)");
+		print_r($this->db->last_query()); die();
 		$tournamentID = $this->db->insert_id();
-		if($this->db->affected_rows()>0) {
+		if($tournamentID) {
 			
 			$insertDataArray = array();
 			foreach($data as $key=>$value) {
