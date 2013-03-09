8ac8bf45e57a4f359e8f8e469fe8e0b93c2d5d85
lg90 <lg90@hw.ac.uk>
2013-03-09 14:48:23 +0000

    fixing scheduling model function syntax errors

--- application/models/matches_model.php
+++ application/models/matches_model.php
@@ -72,7 +72,7 @@ class Matches_model extends MY_Model {
 	 * @param endTime 	A dateTime Object
 	 * @return array
 	 **/
-	public function get_venue_matches($venueID,$startTime=NULL,$endTime=NULL)
+	public function get_venue_matches($venueID,$startTimeY=NULL,$endTimeY=NULL)
 	{
 		//$startTime = $this->db->escape(( is_null($startTime) ? "0" : datetime_to_standard($startTime) )); 	// 0 is less than 0000-01-01... Hopefully
 		//$endTime = $this->db->escape(( is_null($endTime) ? ":" : datetime_to_standard($endTime) ));		// : is greater than 9, which is the largest digit so far
