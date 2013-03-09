8b5708f7e8766172271e0757c46161b85b67216d
lg90 <lg90@hw.ac.uk>
2013-03-09 17:07:35 +0000

    testing venue matches

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -48,8 +48,10 @@ class Test extends MY_Controller {
 	/*
 		matches_model
 	*/
-	public function get_matches($centreID){
-		$output = $this->matches_model->get_matches($centreID);
+	public function get_matches($centreID,$start=FALSE,$end=FALSE){
+		$start = urldecode($start);
+		$end   = urldecode($end);
+		$output = $this->matches_model->get_matches($centreID,$start,$end);
 		$this->display($output);
 	}
 	public function get_venue_matches($venueID,$start=FALSE,$end=FALSE){
