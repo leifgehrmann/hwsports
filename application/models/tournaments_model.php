9271b4633fc5aefded26283a2d53425a46ecd84d
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-10 04:19:01 +0000

    testing tournament model code

--- application/models/tournaments_model.php
+++ application/models/tournaments_model.php
@@ -134,6 +134,7 @@ class Tournaments_model extends MY_Model {
 	 * @return int
 	 **/
 	public function insert($data, $relationIDs=array()) {
+		$relationIDs['centreID']=$this->centreID;
 		return $this->insert_object($data, $this->objectIDKey, $this->dataTableName, $this->relationTableName, $relationIDs);
 	}
 
