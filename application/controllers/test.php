9123828bf2ad37141e010fa9a2c490407c9a9483
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-10 02:16:52 +0000

    testing tournament model code

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -24,10 +24,14 @@ class Test extends MY_Controller {
 	// 	
 	public function model($model,$action,$args="") {
 		$args = json_decode(urldecode($args),true);
+		var_export($args);
+		
 		if(is_array($args)) {
 			foreach($args as $arg) {
 				$argstrings[] = var_export($arg,true);
 			}
+			var_export($argstrings);
+			
 			$argstrings = implode(', ', $argstrings);
 			var_export($argstrings); die();
 			
