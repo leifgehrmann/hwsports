b19310d3215198f3fb8c565b8fab1cb00989da58
lg90 <lg90@hw.ac.uk>
2013-03-15 21:44:10 +0000

    working on scheduling

--- application/models/scheduling_model.php
+++ application/models/scheduling_model.php
@@ -207,6 +207,7 @@ class Scheduling_model extends MY_Model {
 					if($key!="teams" && $key!="count")
 						$matchUsageDateTimes[$key] = $value;
 				$weightedDateTimes = $this->fitness_generator($matchUsageDateTimes);
+				var_dump($$matchUsage[$date]);
 				foreach($weightedDateTimes as $dateTimeWeight=>$dateTime)
 				{
 					var_dump("Attempting to add Event at datetime ".$dateTime);
