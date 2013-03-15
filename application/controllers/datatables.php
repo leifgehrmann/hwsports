89c367cf76ff7ad4d8ca61ea771ea3f6934a331a
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-15 16:33:44 +0000

    testing and fixing datatablesserver

--- application/controllers/datatables.php
+++ application/controllers/datatables.php
@@ -28,8 +28,8 @@ class Datatables extends MY_Controller {
 		switch ($action) {
 			case "load":
 				$sports = $this->sports_model->get_all();
-				$out['aaData'] = array("name"=>"hello","description"=>"world","category"=>19);
-				$out['error'] = $sports;
+				$out['aaData'] = array("DT_RowId"=>"row_1","name"=>"hello","description"=>"world","category"=>19);
+				//$out['error'] = $sports;
 			break;
 			case "create":
 				$sports = $this->sports_model->get_all();
