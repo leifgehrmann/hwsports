839759e602aa01393d3d1cfa3a7008880edb7864
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-15 23:54:37 +0000

    testing and fixing datatablesserver

--- application/models/matches_model.php
+++ application/models/matches_model.php
@@ -180,7 +180,7 @@ class Matches_model extends MY_Model {
 	 * @return int
 	 **/
 	public function insert($data, $relationIDs=array()) {
-		return $this->insert_object($data, $this->objectIDKey, $this->dataTableName, $relationIDs);
+		return $this->insert_object($data, $this->objectIDKey, $this->dataTableName, $this->relationTableName, $relationIDs);
 	}
 
 	/**
