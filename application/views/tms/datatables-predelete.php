352860c4946170933b47e27d23a3ce6003803495
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-16 05:08:27 +0000

    testing and fixing datatablesserver

--- application/views/tms/datatables-predelete.php
+++ application/views/tms/datatables-predelete.php
@@ -1,6 +1,6 @@
 <div class="dependencies">
 	<h1>Deletion Dependencies:</h1>
 	<pre><?=$dependencies?></pre>
-	<button id="fancyconfirm_cancel" style="float: left;" class="button red">Cancel</button>
-	<button id="fancyconfirm_ok" style="float: right;" class="button green">Confirm</button>
+	<a href="#" id="fancyconfirm_cancel" style="float: left;" class="button red">Cancel</a>
+	<a href="#" id="fancyconfirm_ok" style="float: right;" class="button green">Confirm</a>
 </div>
