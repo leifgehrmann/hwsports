37779da47d5cfc17aee3c81113fc312b3ed740a3
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-16 13:08:25 +0000

    testing and fixing datatablesserver

--- application/views/tms/delete-confirm.php
+++ application/views/tms/delete-confirm.php
@@ -4,8 +4,8 @@
 	<ul class='deletionList'>
 		<?=$dependencies?>
 	</ul>
-	<br />
 	If this is correct, click 'Confirm'. Otherwise please cancel and edit the above objects first.<br />
+	<br />
 	<a href="#" id="fancycancel" style="float: left;" class="button red">Cancel</a>
 	<a href="#" id="fancyconfirm" style="float: right;" class="button green">Confirm</a>
 </div>
