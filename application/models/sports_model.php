1d81569c1c5dca63c027b37eea62b4d75983f6bd
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-16 00:28:48 +0000

    testing and fixing datatablesserver

--- application/models/sports_model.php
+++ application/models/sports_model.php
@@ -53,6 +53,7 @@ class Sports_model extends MY_Model {
 	 * @return int
 	 **/
 	public function insert($data, $relationIDs=array()) {
+		$relationIDs['centreID'] = $this->centreID;
 		return $this->insert_object($data, $this->objectIDKey, $this->dataTableName, $this->relationTableName, $relationIDs);
 	}
 
