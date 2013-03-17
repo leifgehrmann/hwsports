f99b740ca19a45547afa6aa44f60a7ea32d9e682
lg90 <lg90@hw.ac.uk>
2013-03-17 17:01:47 +0000

    added some startimes logic?

--- application/views/tms/tournament.php
+++ application/views/tms/tournament.php
@@ -220,7 +220,7 @@
 			var startTimes = $('#'+weekdays[x]+'StartTimes');
 			alert("B");
 			alert(startTimes.html());
-			$(startTimes).append('<p><input type="text" name="'+weekdays[x]+'StartTimes[]" value="" placeholder="HH:MM" /><a class="button red removeInputButton" href="#" style="margin-left:20px;top:0px;">Remove</a></p>');
+			startTimes.append('<p><input type="text" name="'+weekdays[x]+'StartTimes[]" value="" placeholder="HH:MM" /><a class="button red removeInputButton" href="#" style="margin-left:20px;top:0px;">Remove</a></p>');
 			alert(startTimes.html());
 			alert("C");
 			return false;
