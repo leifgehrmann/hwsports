37829e6c542bbd3898acef03ef46a465d7722363
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-17 22:33:33 +0000

    fixed tms error

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -23,28 +23,7 @@ class MY_Loader extends CI_Loader {
 
     function model($model, $name = '', $db_conn = FALSE)
     {
-        if (is_array($model))
-        {
-            foreach ($model as $file => $object_name)
-            {
-                // Linear array was passed, be backwards compatible.
-                // CI already allows loading models as arrays, but does
-                // not accept the model name param, just the file name
-                if ( ! is_string($file)) 
-                {
-                    $file = $object_name;
-                    $object_name = NULL;
-                }
-				
-				if($this->is_model_loaded($file)) return;
-				
-				echo "model $file being loaded for the first time"; die();
-				
-                parent::model($file, $object_name);
-            }
-            return;
-        }
-
+		if($this->is_model_loaded($file)) return;
         // Call the default method otherwise
         parent::model($model, $name, $db_conn);
     }
