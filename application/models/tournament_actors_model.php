3147d15458af216bb415257815a0a9212e65a436
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-18 19:51:06 +0000

    testing signup

--- application/models/tournament_actors_model.php
+++ application/models/tournament_actors_model.php
@@ -23,10 +23,11 @@ class Tournament_actors_model extends MY_Model {
 									->join('sportCategoryRoles', 'sportCategoryRoles.sportCategoryRoleID = tournamentActors.roleID')
 									->where('tournamentActorID',$ID)
 									->get()->row_array();
-									echo "get output:\n: "; var_dump($tournamentActor); 
+									echo "tournamentActor:\n: "; var_dump($tournamentActor); 
 		if($tournamentActor===FALSE) return FALSE;
 		// For each actor, get the actual actor data, using the model as defined by the global array in the constructor 
 		$actor = $this->objects_models[$tournamentActor['actorTable']]->get($tournamentActor['actorID']);
+		echo "actor:\n: "; var_dump($actor); 
 		$actor['sportCategoryRoleName'] = $tournamentActor['sportCategoryRoleName'];
 		if(!$actor) return FALSE;
 		// Now get the tournamentActorData using our lovely dry code
