d04f2ec9fb100f935c2c5e5314b8b9e2fa8b2138
lg90 <lg90@hw.ac.uk>
2013-03-18 09:34:55 +0000

    trying to figure out why ci loading of models does not work

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -3,7 +3,7 @@
 class MY_Loader extends CI_Loader {
 
 	function is_model_loaded($model) {
-		echo "is_model_loaded checking for model: $model in array:<br /><pre>";var_dump($this);echo "</pre>"; 
+		//echo "is_model_loaded checking for model: $model in array:<br /><pre>";var_dump($this);echo "</pre>"; 
 		
 		if (in_array($model, $this->_ci_models))
 			return TRUE;
@@ -12,8 +12,12 @@ class MY_Loader extends CI_Loader {
 	}
 
 	function model($model, $name = '', $db_conn = FALSE) {
-		if($name=='test')
-			die();
+
+		$this->_ci_models[] = $model;
+
+		echo "loaded $model into";var_dump($this);echo "</pre>"; 
+		die();
+
 		if($this->is_model_loaded($model)) {
 			//echo "model $model already loaded, skipping<br />\n"; 
 			return;
