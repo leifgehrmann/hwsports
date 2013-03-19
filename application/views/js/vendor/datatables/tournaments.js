5d9c00edfd441771652f03ba78a252c693e8ffbe
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-19 13:34:41 +0000

    adhidden columns in tournaments datatable

--- application/views/js/vendor/datatables/tournaments.js
+++ application/views/js/vendor/datatables/tournaments.js
@@ -78,7 +78,7 @@ $('.DTED_Lightbox_Wrapper').css('visibility','visible');							});
 				{ "mData": "detailsLink" }
 			],
 			"fnRowCallback": function( nRow, aData, iDisplayIndex ) {
-				nRow.className += aData['className'];
+				nRow.className += " "+aData['className'];
 				return nRow;
 			},
 			"aoColumnDefs": [
