317de20912042fc5bb94308a0848279ccc2a67da
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-19 06:23:22 +0000

    implemented deletion of team members on signup form, tidied code. almost all working now

--- application/controllers/datatables.php
+++ application/controllers/datatables.php
@@ -215,7 +215,6 @@ class Datatables extends MY_Controller {
 				// Query the usersGroups table for all users in this team, then add a where clause for each
 				$groupUsersRows = $this->db->get_where('usersGroups',array('groupID' => $groupID))->result_array();
 				$groupUserCount = count($groupUsersRows);
-				var_dump($groupUserCount); die();
 				if($groupUserCount) {
 					for($i=0; $i<$groupUserCount; $i++) {
 						if($i==0) $this->db->where(array('userID' => $groupUsersRows[0]['userID']));
