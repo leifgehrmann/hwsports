5a9a6c2d94523a1a2082da198193e04a70d826b4
lg90 <lg90@hw.ac.uk>
2013-03-22 12:21:25 +0000

    working on scheduling

--- application/views/sis/home.php
+++ application/views/sis/home.php
@@ -11,7 +11,167 @@
 		<a href="sis/help" class="button blue">More Information</a>
 	</div>
 </div>
-<table class="full">
+<div class="widget half matches">
+	<div class="widget-title">
+		<div class="widget-title-left icon"></div>
+		<div class="widget-title-centre">Latest Matches</div>
+		<div class="widget-title-right icon"></div>
+	</div>
+	<div class="widget-body">
+		<? if(count($latestMatches)==0) { ?>
+			<p>There are no recent or current matches.</p>
+		<? } else {
+			foreach($latestMatches as $match){ 
+				$date = datetime_to_public_date($match['startTime']);
+				$time = datetime_to_public_time($match['startTime']);
+			?>
+			<div class="match 
+				matchID-<?=$match['matchID']?> 
+				sportID-<?=$match['sportID']?> 
+				sportCategoryID-<?=$match['sportData']['sportCategoryID']?> 
+				tournamentID-<?=$match['tournamentID']?>
+			">
+				<div class="icon left margin-right"></div>
+				<p>
+					<a href="/tms/match/<?=$match['matchID']?>"><?=$match['name']?></a><br/>
+					<span style="display: inline-block;padding-left: 40px;"><?=$date?> &mdash; <?=$time?></span>
+				</p>
+			</div>
+			<? } ?>
+		<? } ?>
+		<p><a href="/tms/matches/" class="button right blue">View All Matches</a></p>
+	</div>
+</div>
+<div class="widget half matches">
+	<div class="widget-title">
+		<div class="widget-title-left icon"></div>
+		<div class="widget-title-centre">Upcoming Matches</div>
+		<div class="widget-title-right icon"></div>
+	</div>
+	<div class="widget-body">
+		<? if(count($upcomingMatches)==0) { ?>
+			<p>There are no upcoming matches.</p>
+		<? } else {
+			foreach($upcomingMatches as $match){ 
+				$date = datetime_to_public_date($match['startTime']);
+				$time = datetime_to_public_time($match['startTime']);
+			?>
+			<div class="match 
+				matchID-<?=$match['matchID']?> 
+				sportID-<?=$match['sportID']?> 
+				sportCategoryID-<?=$match['sportData']['sportCategoryID']?> 
+				tournamentID-<?=$match['tournamentID']?>
+			">
+				<div class="icon left margin-right"></div>
+				<p>
+					<a href="/tms/match/<?=$match['matchID']?>"><?=$match['name']?></a><br/>
+					<span style="display: inline-block;padding-left: 40px;"><?=$date?> &mdash; <?=$time?></span>
+				</p>
+			</div>
+			<? } ?>
+		<? } ?>
+		<p><a href="/tms/matches/" class="button right blue">View All Matches</a></p>
+	</div>
+</div>
+<div class="widget half tournaments">
+	<div class="widget-title">
+		<div class="widget-title-left icon"></div>
+		<div class="widget-title-centre">Latest Tournaments</div>
+		<div class="widget-title-right icon"></div>
+	</div>
+	<div class="widget-body">
+		<? if(count($latestTournaments)==0) { ?>
+			<p>There are no recent or currently running tournaments.</p>
+		<? } else {
+			foreach($latestTournaments as $tournament){ 
+				$start = datetime_to_public_date($tournament['tournamentStart']);
+				$end = datetime_to_public_date($tournament['tournamentEnd']);
+			?>
+			<div class="tournament 
+				tournamentID-<?=$match['tournamentID']?> 
+				sportID-<?=$match['sportID']?> 
+				sportCategoryID-<?=$match['sportData']['sportCategoryID']?> 
+			">
+				<div class="icon left margin-right"></div>
+				<p>
+					<a href="/tms/tournament/<?=$tournament['tournamentID']?>"><?=$tournament['name']?></a><br/>
+					<span style="display: inline-block;padding-left: 40px;">Starts: <?=$start?></span><br/>
+					<span style="display: inline-block;padding-left: 40px;">Ends: <?=$end?></span>
+				</p>
+			</div>
+			<? } ?>
+		<? } ?>
+		<p><a href="/tms/tournaments/" class="button right blue">View All Tournaments</a></p>
+	</div>
+</div>
+<div class="widget half tournaments">
+	<div class="widget-title">
+		<div class="widget-title-left icon"></div>
+		<div class="widget-title-centre">Upcoming Tournaments</div>
+		<div class="widget-title-right icon"></div>
+	</div>
+	<div class="widget-body">
+		<? if(count($upcomingTournaments)==0) { ?>
+			<p>There are no upcoming tournaments.</p>
+		<? } else {
+			foreach($upcomingTournaments as $tournament){ 
+				$start = datetime_to_public_date($tournament['tournamentStart']);
+				$end = datetime_to_public_date($tournament['tournamentEnd']);
+			?>
+			<div class="tournament 
+				tournamentID-<?=$match['tournamentID']?> 
+				sportID-<?=$match['sportID']?> 
+				sportCategoryID-<?=$match['sportData']['sportCategoryID']?> 
+			">
+				<div class="icon left margin-right"></div>
+				<p>
+					<a href="/tms/tournament/<?=$tournament['tournamentID']?>"><?=$tournament['name']?></a><br/>
+					<span style="display: inline-block;padding-left: 40px;">Starts: <?=$start?></span><br/>
+					<span style="display: inline-block;padding-left: 40px;">Ends: <?=$end?></span>
+				</p>
+			</div>
+			<? } ?>
+		<? } ?>
+		<p><a href="/tms/tournaments/" class="button right blue">View All Tournaments</a></p>
+	</div>
+</div>
+<div class="widget half icons">
+	<div class="widget-title">
+		<div class="widget-title-left icon"></div>
+		<div class="widget-title-centre">Happy small icons</div>
+		<div class="widget-title-right icon"></div>
+	</div>
+	<div class="widget-body">
+		<? for($i=1;$i<=46;$i++) { ?>
+			<div class="match 
+				sportCategoryID-<?=$i?> 
+				tournamentID-<?=$match['tournamentID']?>
+			">
+				<div style="margin-bottom:20px" class="icon left margin-right"></div>
+			</div>
+		<? } ?>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<table class="full">
 	<tr>
 		<td colspan="2"><h2><div class="icon subscribe margin-right"></div>Subscribe to our announcements!</h2></td>
 	</tr>
@@ -26,8 +186,8 @@
 			</form>
 		</td>
 	</tr>
-</table>
-<h1>Announcements (<a href="#announcement-list">Full List</a>)</h1>
+</table>-->
+<!--<h1>Announcements (<a href="#announcement-list">Full List</a>)</h1>
 <div class="widget full announcement">
 	<a href="#newsarticle">
 		<div class="widget-title">
@@ -140,4 +300,4 @@ $(document).ready(function(){
 			<td><a href="#newsarticle">View Announcement</a></td>
 		</tr>
 	</tbody>
-</table>
+</table>-->
