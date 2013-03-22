342176d45cbf7f225f773deea6e6baf88c6f1b0a
lg90 <lg90@hw.ac.uk>
2013-03-22 14:10:46 +0000

    working making sis pages more content filled

--- application/views/sis/tournament.php
+++ application/views/sis/tournament.php
@@ -1,21 +1,19 @@
 <h1><a href="/sis/tournaments">Tournaments</a><div class="icon subsection"></div><?=$tournament['name']?></h1>
 
 <? if ( $tournament['status'] == "inRegistration" ) { ?>
-<table class="full">
+<p><b>Start:</b><?=datetime_to_public_date($tournament['tournamentStart'])?></p>
+<p><b>End:</b><?=datetime_to_public_date($tournament['tournamentEnd'])?></p>
+<p><?=$tournament['description']?></p>
+<table>
 	<tr>
-		<td colspan="2"><h2><div class="icon subscribe margin-right"></div>Sign up for <?=$tournament['name']?>!</h2></td>
+		<td><h2><div class="icon subscribe margin-right"></div>Sign up for <?=$tournament['name']?>!</h2></td>
+		<td rowspan="2"><a href='/sis/signup/<?=$tournament['tournamentID']?>' class='button green'>Sign Up Now!</a></td>
 	</tr>
 	<tr>
-		<td style="width:40%">
-			<p>Registration ends on <?=datetime_to_public_date($tournament['registrationEnd'])?> at <?=datetime_to_public_time($tournament['registrationEnd'])?>.</p>
-		</td>
-		<td style="width:60%">
-			<a href='/sis/signup/<?=$tournament['tournamentID']?>' class='button green'>Sign Up Now!</a>
-		</td>
+		<td><p>Registration ends on <?=datetime_to_public_date($tournament['registrationEnd'])?> at <?=datetime_to_public_time($tournament['registrationEnd'])?>.</p></td>
 	</tr>
 </table>
 <? } ?>
-<p><?=$tournament['description']?></p>
 <h2>Calendar</h2>
 <p>Click the entries for details on individual matches</p>
 
