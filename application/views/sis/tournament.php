203440e585fb00a9582ed5c25b0cf41600ec0a57
lg90 <lg90@hw.ac.uk>
2013-03-22 14:06:14 +0000

    working making sis pages more content filled

--- application/views/sis/tournament.php
+++ application/views/sis/tournament.php
@@ -1,10 +1,17 @@
 <h1><a href="/sis/tournaments">Tournaments</a><div class="icon subsection"></div><?=$tournament['name']?></h1>
 
 <? if ( $tournament['status'] == "inRegistration" ) { ?>
-<table class="">
+<table class="full">
 	<tr>
-		<td><div class="icon subscribe margin-right"></div><p>Registration ends on <?=datetime_to_public_date($tournament['registrationEnd'])?> at <?=datetime_to_public_time($tournament['registrationEnd'])?>.</p></td>
-		<td><a href='/sis/signup/<?=$tournament['tournamentID']?>' class='button green'>Sign Up Now!</a></td>
+		<td colspan="2"><h2><div class="icon subscribe margin-right"></div>Sign up for <?=$tournament['name']?>!</h2></td>
+	</tr>
+	<tr>
+		<td style="width:40%">
+			<p>Registration ends on <?=datetime_to_public_date($tournament['registrationEnd'])?> at <?=datetime_to_public_time($tournament['registrationEnd'])?>.</p>
+		</td>
+		<td style="width:60%">
+			<a href='/sis/signup/<?=$tournament['tournamentID']?>' class='button green'>Sign Up Now!</a>
+		</td>
 	</tr>
 </table>
 <? } ?>
