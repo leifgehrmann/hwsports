309ead3f84eda1a97fb213b00ef10392ed9d5b8d
lg90 <lg90@hw.ac.uk>
2013-03-22 16:39:57 +0000

    fixed match page tms

--- application/views/tms/match.php
+++ application/views/tms/match.php
@@ -9,7 +9,7 @@
 
 	echo "<table>";
 	for($i=0;$i<count($fields);$i++){
-		if(!isset($match[$fields]))
+		if(!isset($this->data['match'][$fields]))
 			continue;
 		echo "\t<tr>";
 		echo "\t\t<th style='width:{$widths[0]}'>{$labels[$i]}</th>";
