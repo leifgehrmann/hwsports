63c039273d9c7994a5b2fca6a945dc037b28c5d1
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-25 18:52:33 +0000

    fixed center settings

--- application/views/tms/header.php
+++ application/views/tms/header.php
@@ -37,15 +37,15 @@
 
 		
 	<!-- Skin CSS file for YUI editor -->
-	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.9.0/build/assets/skins/sam/skin.css">
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.9.0/build/editor/assets/skins/sam/simpleeditor.css" />
 	<!-- Utility Dependencies -->
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.9.0/build/fonts/fonts-min.css" />
 	<script src="http://yui.yahooapis.com/2.9.0/build/yahoo-dom-event/yahoo-dom-event.js"></script> 
 	<script src="http://yui.yahooapis.com/2.9.0/build/element/element-min.js"></script> 
 	<!-- Needed for Menus, Buttons and Overlays used in the Toolbar -->
 	<script src="http://yui.yahooapis.com/2.9.0/build/container/container_core-min.js"></script>
 	<!-- Source file for Rich Text Editor-->
 	<script src="http://yui.yahooapis.com/2.9.0/build/editor/simpleeditor-min.js"></script>
-
 	
 	<script>
 
