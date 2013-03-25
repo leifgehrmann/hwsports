b2064356f53834a77bf6e14d39fa9808829d3f31
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-25 18:45:14 +0000

    fixed center settings

--- application/views/tms/appearance.php
+++ application/views/tms/appearance.php
@@ -1,9 +1,20 @@
+
+<!-- Skin CSS file -->
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.9.0/build/assets/skins/sam/skin.css">
+<!-- Utility Dependencies -->
+<script src="http://yui.yahooapis.com/2.9.0/build/yahoo-dom-event/yahoo-dom-event.js"></script> 
+<script src="http://yui.yahooapis.com/2.9.0/build/element/element-min.js"></script> 
+<!-- Needed for Menus, Buttons and Overlays used in the Toolbar -->
+<script src="http://yui.yahooapis.com/2.9.0/build/container/container_core-min.js"></script>
+<!-- Source file for Rich Text Editor-->
+<script src="http://yui.yahooapis.com/2.9.0/build/editor/simpleeditor-min.js"></script>
+
 <h1>Appearance</h1>
 
 <?php echo form_open("tms/appearance", array('id' => 'settingsForm'));?>
 
 <h2>Main Textual Content</h2>
-<table>
+<table class="yui-skin-sam">
 	<tr>
 		<td><label for="name">Name:</label></td>
 		<td><?php echo form_input($name);?></td>
@@ -38,67 +49,40 @@
 <?php echo form_close();?>
 
 <script type="text/javascript">
-$('input.color').ColorPicker({
-	onSubmit: function(hsb, hex, rgb, el) {
-		$(el).val(hex);
-		$(el).css('background-color','#'+hex);
-		$(el).ColorPickerHide();
-	},
-	onBeforeShow: function () {
-		$(this).ColorPickerSetColor(this.value);
-	}
-}).bind('keyup', function(){
-	$(this).ColorPickerSetColor(this.value);
-});
+	$(document).ready(function() {
+	
+		$('input.color').ColorPicker({
+			onSubmit: function(hsb, hex, rgb, el) {
+				$(el).val(hex);
+				$(el).css('background-color','#'+hex);
+				$(el).ColorPickerHide();
+			},
+			onBeforeShow: function () {
+				$(this).ColorPickerSetColor(this.value);
+			}
+		}).bind('keyup', function(){
+			$(this).ColorPickerSetColor(this.value);
+		});
 
-$('input.time').timepicker();
+		$('input.time').timepicker();
 
+		var yuiEditorLinks = new YAHOO.widget.Editor('publicFooterLinks', {
+			height: '300px',
+			width: '522px',
+			dompath: true, //Turns on the bar at the bottom
+			animate: true //Animates the opening, closing and moving of Editor windows
+		});
+		yuiEditorLinks.render();
+		
+		var yuiEditorContact = new YAHOO.widget.Editor('publicFooterContact', {
+			height: '300px',
+			width: '522px',
+			dompath: true, //Turns on the bar at the bottom
+			animate: true //Animates the opening, closing and moving of Editor windows
+		});
+		yuiEditorContact.render();
+
+	});
 </script>
-<? /*
-<script charset="utf-8" src="/js/vendor/tiny_mce/tiny_mce.js"></script>
-<script type="text/javascript">
 
-tinyMCE.init({
-	// General options
-	mode : "textareas",
-	theme : "advanced",
-	plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-
-	// Theme options
-	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,outdent,indent,|,forecolor,backcolor,|,formatselect",
-	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,undo,redo,|,link,unlink,anchor,|,cleanup,styleprops,code,|,preview,fullscreen",
-	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,charmap,image,iespell,media,advhr,|,help",
-	theme_advanced_buttons4 : "styleprops",
-	theme_advanced_toolbar_location : "top",
-	theme_advanced_toolbar_align : "left",
-	theme_advanced_statusbar_location : "bottom",
-	theme_advanced_resizing : true,
-
-	// height of the editor
-	height : "480",
-
-	// Example content CSS (should be your site CSS)
-	content_css : "css/example.css",
-
-	// Skin options
-	skin : "o2k7",
-	skin_variant : "silver",
-
-	// Drop lists for link/image/media/template dialogs
-	template_external_list_url : "js/template_list.js",
-	external_link_list_url : "js/link_list.js",
-	external_image_list_url : "js/image_list.js",
-	media_external_list_url : "js/media_list.js",
-
-	// Replace values for the template plugin
-	template_replace_values : {
-		username : "Some User",
-		staffid : "991234"
-	}
-});
-</script>
-<form method="post" action="somepage">
-	<textarea name="content" style="width:100%"></textarea>
-</form> 
-*/ ?>
 <!-- /#main -->
