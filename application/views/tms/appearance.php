cccb008b6492e0ccc5c2e477edc2721974448117
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-25 18:55:36 +0000

    fixed center settings

--- application/views/tms/appearance.php
+++ application/views/tms/appearance.php
@@ -59,15 +59,12 @@
 
 
 <script>
-
-(function() {
 	var yuiEditorLinks = new YAHOO.widget.Editor('publicFooterLinks', {
 		height: '300px',
 		width: '522px',
 		dompath: true, //Turns on the bar at the bottom
 		animate: true //Animates the opening, closing and moving of Editor windows
 	});
-})();
 </script>
 
 <!-- /#main -->
