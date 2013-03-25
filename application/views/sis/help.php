0571b86c592d6b9169c25ab35c83e12f15279713
lg90 <lg90@hw.ac.uk>
2013-03-25 19:00:27 +0000

    added some more help to the help page

--- application/views/sis/help.php
+++ application/views/sis/help.php
@@ -1,3 +1,9 @@
-If you have any issues using this web application, please contact us on:<br />
+<h1>Help</h1>
+<p>If you have any issues using this web application, please contact us on:</p>
+<ul>
+	<li>Email: <a href="mailTo:support@infusionsystem.co.uk">support@infusionsystem.co.uk</a></li>
+	<li>Tel: <a href="tel:441314495111">+44 (0) 131 449 5111</a></li>
+</ul>
+
+</p>
 <br />
-Tel: +44 (0) 131 449 5111
