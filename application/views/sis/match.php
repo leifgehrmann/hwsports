7147049ced253b65e2a9aae65d39639a49ced709
lg90 <lg90@hw.ac.uk>
2013-03-25 09:44:59 +0000

    fixed the match page

--- application/views/sis/match.php
+++ application/views/sis/match.php
@@ -3,44 +3,27 @@
 	<div class="widget-title">
 		<div class="widget-title-left icon"></div>
 		<div class="widget-title-centre">Match Details</div>
-		<div class="widget-title-right icon chevron"></div>
+		<div class="widget-title-right icon"></div>
 	</div>
 	<div class="widget-body">
+		<? if(isset($match['description'])) { ?>
 		<p><?=$tournament['description']?></p>
+		<? } ?>
 		<p><b>Date: </b><?=$match['datetime']?></p>
 		<p><b>Duration: </b><?=$match['duration']?></p>
 	</div>
 </div>
-<div class="widget half match sportCategoryID-<?=$match['sportData']['sportCategoryID']?> sportID-<?=$match['sportID']?>">
+<div class="widget half venue?>">
 	<div class="widget-title">
 		<div class="widget-title-left icon"></div>
 		<div class="widget-title-centre">Venue Details</div>
-		<div class="widget-title-right icon chevron"></div>
+		<div class="widget-title-right icon"></div>
 	</div>
 	<div class="widget-body">
 		<p><b>Name: </b><?=$match['venueData']['name']?></p>
-		<p><b>Directions: </b><?=$match['datetime']?></p>
+		<? if(isset($match['description'])) { ?>
+		<p><b>Directions: </b><?=$match['directions']['datetime']?></p>
+		<? } ?>
 		<p><a href="http://maps.google.com/maps?q=<?=$match['venueData']['lat']?>,<?=$match['venueData']['lng']?>">View on Google Maps</a></p>
 	</div>
 </div>
-<table>
-	<tr>
-		<td>Date:</td>
-		<td><?=$match['datetime']?></td>
-	</tr>
-	<tr>
-		<td>Duration:</td>
-		<td><?=$match['duration']?></td>
-	</tr>
-	<? if(isset($match['description'])) { ?>
-	<tr>
-		<td>Description:</td>
-		<td><?=$match['description']?></td>
-	</tr>
-	<? } ?>
-</table>
-
-<tr>
-		<td>venue:</td>
-		<td><?=$match['venueData']['name']?></td>
-	</tr>
