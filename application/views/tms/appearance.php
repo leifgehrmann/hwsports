eb040b0cfe88211d3e9f525f3f31157c8329e6b2
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2013-03-25 18:53:40 +0000

    fixed center settings

--- application/views/tms/appearance.php
+++ application/views/tms/appearance.php
@@ -61,15 +61,12 @@
 <script>
 
 (function() {
-
-		var yuiEditorLinks = new YAHOO.widget.Editor('publicFooterLinks', {
-			height: '300px',
-			width: '522px',
-			dompath: true, //Turns on the bar at the bottom
-			animate: true //Animates the opening, closing and moving of Editor windows
-		});
-		yuiEditorLinks.render();
-
+	var yuiEditorLinks = new YAHOO.widget.Editor('publicFooterLinks', {
+		height: '300px',
+		width: '522px',
+		dompath: true, //Turns on the bar at the bottom
+		animate: true //Animates the opening, closing and moving of Editor windows
+	});
 })();
 </script>
 
